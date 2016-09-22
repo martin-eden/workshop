fa9af0fda5648a154c9703370943b3fe326fe2d1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2016-09-21 21:41:27 -0700

    [.^] [load_from.lua]

--- load_from/lua.lua
+++ load_from/lua.lua
@@ -1,6 +1,6 @@
+local parse = request('generic_loader')
 local syntax = request('^.parse.syntaxes.lua')
 local postprocess_struc = request('lua.formatter_transformer')
-local parse = request('generic_loader')
 
 return
   function(str)
