26948b591c3dd486742a555a6eb8b15b9bd21e14
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2016-09-27 00:48:36 -0700

    [^-] [parse.csv] Dropped slow implementation.

--- parse/syntaxes/csv.lua
+++ parse/syntaxes/csv.lua
@@ -1,8 +1,52 @@
-local implementors =
-  {
-    strict = request('csv.strict'),
-    fast = request('csv.fast'),
-  }
-implementors.default = implementors.fast
+local parser = request('^.parser')
+local handy = parser.handy
 
-return implementors
+local opt_spc = handy.match_pattern('[ \t]*')
+
+local quoted_data =
+  {
+    opt_spc,
+    {
+      name = 'quoted_data',
+      '"',
+      handy.opt(
+        handy.rep(
+          handy.cho1(
+            handy.match_pattern('[^"]+'),
+            '""'
+          )
+        )
+      ),
+      '"',
+    },
+    opt_spc
+  }
+
+local unquoted_data =
+  {
+    name = 'unquoted_data',
+    opt_spc,
+    handy.match_pattern('[^,\r\n]*'),
+  }
+
+local field_sep = {name = 'field_sep', ','}
+local record =
+  {
+    name = 'record',
+    handy.list(
+      handy.opt(
+        handy.cho1(
+          quoted_data,
+          unquoted_data
+        )
+      ),
+      field_sep
+    )
+  }
+
+local record_sep = handy.match_pattern('[\r]?\n')
+local csv_records = handy.list(record, record_sep)
+
+parser.optimize(csv_records)
+
+return csv_records
