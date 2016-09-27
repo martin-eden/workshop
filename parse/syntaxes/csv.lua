fcc3f3c2dcb0c970f313b23b153975f2d02d0080
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2016-09-27 01:48:19 -0700

    [.^] [syntax.csv] Simplifications.

--- parse/syntaxes/csv.lua
+++ parse/syntaxes/csv.lua
@@ -1,6 +1,8 @@
 local parser = request('^.parser')
 local handy = parser.handy
 
+local field_sep_char = ','
+
 local opt_spc = handy.match_pattern('[ \t]*')
 
 local quoted_data =
@@ -23,24 +25,20 @@ local quoted_data =
 local unquoted_data =
   {
     name = 'unquoted_data',
-    handy.match_pattern('[^,\r\n]*'),
+    handy.match_pattern('[^' .. field_sep_char .. '\r\n]*'),
   }
 
-local field_sep = {name = 'field_sep', ','}
-local record =
-  {
-    name = 'record',
-    handy.list(
-      handy.opt(
+return
+  handy.list(
+    {
+      name = 'record',
+      handy.list(
         handy.cho1(
           quoted_data,
           unquoted_data
-        )
-      ),
-      field_sep
-    )
-  }
-
-local record_sep = handy.match_pattern('[\r]?\n')
-
-return handy.list(record, record_sep)
+        ),
+        {name = 'field_sep', field_sep_char}
+      )
+    },
+    handy.match_pattern('[\r]?\n')
+  )
