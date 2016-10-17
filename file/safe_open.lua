5ed5927e738b54a9768779a60de78c0b82c1d84b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2016-10-16 23:43:46 -0700

    [.^] [file.safe_open] Second parameter (open mode) made optional.
    Default open mode is 'rb'.

--- file/safe_open.lua
+++ file/safe_open.lua
@@ -1,6 +1,7 @@
 return
   function(file_name, mode)
     assert_string(file_name)
+    mode = mode or 'rb'
     assert_string(mode)
     local f_object, err_msg = io.open(file_name, mode)
     if not f_object then
