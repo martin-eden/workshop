8b66d24b0231f1046260d36de195f687cec5e87b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-02-06 06:23:36 -0800

    [^] [load_from.generic_loader] Now arbitrary list of post-processers allowed.

--- load_from/generic_loader.lua
+++ load_from/generic_loader.lua
@@ -1,13 +1,16 @@
 local parser = request('^.parse.parser')
 
 return
-  function(str, syntax, struc_transformer)
+  function(str, syntax, ...)
     local result
     local parse_result, finish_pos, data_struc = parser.parse(syntax, str)
     if parse_result then
       result = data_struc
-      if struc_transformer then
-        result = struc_transformer(result)
+      for i = 1, select('#', ...) do
+        local struc_transformer = select(i, ...)
+        if struc_transformer then
+          result = struc_transformer(result)
+        end
       end
     end
     return result
