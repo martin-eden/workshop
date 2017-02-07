61c91e2756816584e5584e6f2f53d7ea4c4a5830
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-02-07 09:28:29 -0800

    [/] [load_from.json] Default JSON loading linked to lexer -> parser code.

--- load_from/json.lua
+++ load_from/json.lua
@@ -1,9 +1,3 @@
+-- return request('json.via_parser')
+return request('json.via_lexer')
 -- return request('json.qd')
-
-local parse = request('generic_loader')
-local syntax = request('^.parse.syntaxes.json')
-local struc_to_lua = request('^.parse.json.struc_to_lua')
-return
-  function(json_str)
-    return parse(json_str, syntax, struc_to_lua)
-  end
