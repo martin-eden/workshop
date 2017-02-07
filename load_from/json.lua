d7dd436b4102cf37bdc7d97422f247c5bb140cf9
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-02-06 19:28:32 -0800

    [.] [load_from.json] Simplified JSON loader code.
    .. by removing already tested heuristics and inlining parser-call code.

--- load_from/json.lua
+++ load_from/json.lua
@@ -1,16 +1,9 @@
-local quick_and_dirty_hack = request('json.qd')
-local slow_and_clean_parser = request('json.via_parser')
+-- return request('json.qd')
 
-local lazy_juggler =
+local parse = request('generic_loader')
+local syntax = request('^.parse.syntaxes.json')
+local struc_to_lua = request('^.parse.json.struc_to_lua')
+return
   function(json_str)
-    local result
-    result =
-      quick_and_dirty_hack(json_str) or
-      slow_and_clean_parser(json_str)
-    return result
+    return parse(json_str, syntax, struc_to_lua)
   end
-
--- return lazy_juggler
-
-return slow_and_clean_parser
--- return quick_and_dirty_hack
--- load_from/json/via_parser.lua
+++
@@ -1,9 +0,0 @@
-local syntax = request('^.^.parse.syntaxes.json')
-local struc_to_lua = request('^.^.parse.json.struc_to_lua')
-
-local parse = request('^.generic_loader')
-
-return
-  function(json_str)
-    return parse(json_str, syntax, struc_to_lua)
-  end
