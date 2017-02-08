ee53363dc05a597a5204372b2a7d3e2c30c25b48
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-02-08 05:15:15 -0800

    [.] [far_dir_list.syntax] Tabs changed to spaces.

--- parse/syntaxes/far_dir_list.lua
+++ parse/syntaxes/far_dir_list.lua
@@ -2,24 +2,24 @@ local parser = request('^.parser')
 local handy = parser.handy
 
 --[[
-	Data sample:
+  Data sample:
 
-	\Documents and Settings\All Users\Documents\My Pictures
-	\Documents and Settings\All Users\Documents\My Pictures\Sample Pictures
-	\Documents and Settings\All Users\Documents\My Videos
+  \Documents and Settings\All Users\Documents\My Pictures
+  \Documents and Settings\All Users\Documents\My Pictures\Sample Pictures
+  \Documents and Settings\All Users\Documents\My Videos
 ]]
 
 return
-	handy.list(
-		{
-			name = 'record',
-			handy.rep(
-				[[\]],
-				{
-					name = 'field',
-					handy.match_pattern('[^%\x5C\n]*')
-				}
-			)
-		},
-		'\n'
-	)
+  handy.list(
+    {
+      name = 'record',
+      handy.rep(
+        [[\]],
+        {
+          name = 'field',
+          handy.match_pattern('[^%\x5C\n]*')
+        }
+      )
+    },
+    '\n'
+  )
