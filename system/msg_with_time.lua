a407060a03ece060b828e211d2f597a42057d9cd
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-03-16 09:17:27 -0700

    [/] [system.msg_with_time] Changed function contract and output.
    Now it is clear that times belongs to previous text:
      Loading from "qcQuest.lua" [4.17MiB]. [9.63ms]
      Parsing. [20.1s]
      Compiling. [25.3s]
      Saving to "out" [3.09MiB]. [3.28ms]
    
    Was
      [59.9ms] Loading from "qcQuest.lua" [4.17MiB].
      [9.63ms] Parsing.
      [20.1s] Compiling.
      [25.3s] Saving to "out" [3.09MiB].
      [3.28ms] Done

--- system/msg_with_time.lua
+++ system/msg_with_time.lua
@@ -1,11 +1,14 @@
 local represent_time = request('^.number.represent_time')
 
-local cur_time = os.clock()
-local last_time = os.clock()
+local last_time
 
 return
   function(s)
-    cur_time = os.clock()
-    io.write(('[%s] %s\n'):format(represent_time(cur_time - last_time), s))
+    local cur_time = os.clock()
+    if last_time then
+      local time_passed = represent_time(cur_time - last_time)
+      io.stderr:write((' [%s]\n'):format(time_passed))
+    end
+    io.stderr:write(s)
     last_time = cur_time
   end
