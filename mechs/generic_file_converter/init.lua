66a0de1814c16525dfcd3d93d7edd0c35c9adb30
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-05-09 09:17:02 -0700

    [.^] [mechs.generic_file_converter] Random shortenings and reformatting.

--- mechs/generic_file_converter/init.lua
+++ mechs/generic_file_converter/init.lua
@@ -2,9 +2,8 @@ local safe_open = request('!.file.safe_open')
 
 return
   function(self)
-    if (self.f_in_name == '') or (self.f_out_name == '') then
-      print('Usage: <f_in> <f_out>')
-    end
+    assert(self.f_in_name)
+    assert(self.f_out_name)
 
     -- Test that <f_out_name> is writable:
     local f_out = safe_open(self.f_out_name, 'w')
