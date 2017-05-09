06069007d86bdc66421ee535ac523f213ed2a984
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-05-09 08:18:52 -0700

    [^+] [mechs.compile] Extended functionality.
    Now [mechs.compile] supports stand-alone handlers which are called
    when node have filled ".type" field.

--- mechs/compile.lua
+++ mechs/compile.lua
@@ -1,8 +1,15 @@
 local unfold = request('^.table.unfold')
 
 return
-  function(t)
+  function(t, node_handlers)
+    if is_string(t) then
+      return t
+    end
+
     assert_table(t)
+    node_handlers = node_handlers or {}
+    assert_table(node_handlers)
+
     local result = {}
 
     local compile
@@ -11,8 +18,14 @@ return
         if is_string(node) then
           result[#result + 1] = node
         elseif is_table(node) then
-          for i = 1, #node do
-            compile(node[i])
+          if node.type then
+            local node_handler = node_handlers[node.type]
+            assert(node_handler, ('No handler found for type "%s".'):format(node.type))
+            result[#result + 1] = node_handler(node)
+          else
+            for i = 1, #node do
+              compile(node[i])
+            end
           end
         end
       end
