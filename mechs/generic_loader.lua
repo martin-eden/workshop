a4cefcaa67c600b52b56a55e55975ceae168ded6
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-05-11 07:35:05 -0700

    [~] [mechs.generic_loader] Relinked with [streams].

--- mechs/generic_loader.lua
+++ mechs/generic_loader.lua
@@ -1,22 +1,28 @@
-local parser = request('parser')
-local c_string_stream = request('!.mechs.streams.readable_string.interface')
+local parse = request('parser.parse')
+local c_string_stream = request('!.mechs.streams.mergeable.string.interface')
 
+-- local profiler = request('!.system.profiler')
 -- local to_str = request('!.formats.lua_table_code.save')
+
 return
   function(str, syntax, ...)
+    -- profiler.start()
     -- print(to_str(syntax))
-    local string_stream = new(c_string_stream)
-    string_stream:init(str)
 
-    local result, data_struc = parser.parse(string_stream, syntax)
-    if result then
+    local input_stream = new(c_string_stream)
+    input_stream:init(str)
+    local result
+
+    local is_processed, data_struc = parser.parse(input_stream, syntax)
+    if is_processed then
       result = data_struc
       for i = 1, select('#', ...) do
         local struc_transformer = select(i, ...)
-        if struc_transformer then
-          result = struc_transformer(string_stream, result)
-        end
+        result = struc_transformer(input_stream, result)
       end
     end
+
+    -- profiler.stop()
+
     return result
   end
