b4d13384ecba20a8e027c43c3c551c84f918f740
Ian <ian@somewhere.com>
2017-07-24 14:53:37 -0700

    [+] [number.represent_frequency] Unit names for frequencies.
    Probably should rework interface. It's redundant.

---
+++ number/represent_frequency.lua
@@ -0,0 +1,16 @@
+local representer = request('representer.interface')
+
+representer =
+  new(
+    representer,
+    {
+      type = 'frequency',
+      digits_display = 3,
+    }
+  )
+representer:init()
+
+return
+  function(n)
+    return representer:represent(n)
+  end
