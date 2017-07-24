4f0add7e78cc380cba175d08e5e6070060053126
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-07-24 14:53:37 -0700

    [+] [number.represent_frequency] Unit names for frequencies.
    Probably should rework interface. It's redundant.

--- number/representer/units/interface.lua
+++ number/representer/units/interface.lua
@@ -1,7 +1,8 @@
 return
   {
-    binary_units = request('binary_units'),
     binary_bytes = request('binary_bytes'),
+    binary_units = request('binary_units'),
+    frequency = request('frequency'),
     general_number = request('general_number'),
     general_time = request('general_time'),
   }
