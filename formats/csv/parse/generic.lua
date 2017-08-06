1abe6f813f82d9d7c54fa306858ea7f3f6a59029
Ian <ian@somewhere.com>
2017-08-06 22:40:29 +0100

    [.] [formats.csv.parse] Typo fix.

--- formats/csv/parse/generic.lua
+++ formats/csv/parse/generic.lua
@@ -1,8 +1,8 @@
 local generic_loader = request('^.^.^.mechs.generic_loader')
 local syntax = request('^.syntax')
-local struc_transormer = request('^.transform.generic.restruc')
+local struc_transformer = request('^.transform.generic.restruc')
 
 return
   function(str)
-    return generic_loader(str, syntax, struc_transormer)
+    return generic_loader(str, syntax, struc_transformer)
   end
