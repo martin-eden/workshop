cee8bbd2d7c4869abaea67442fe4eb23a981fe2a
Ian <ian@somewhere.com>
2017-08-06 22:41:13 +0100

    [.] [formats.url.load] Typo fix.

--- formats/url/load.lua
+++ formats/url/load.lua
@@ -1,8 +1,8 @@
 local generic_loader = request('^.^.mechs.generic_loader')
 local syntax = request('syntax')
-local struc_transormer = nil
+local struc_transformer = nil
 
 return
   function(str)
-    return generic_loader(str, syntax, struc_transormer)
+    return generic_loader(str, syntax, struc_transformer)
   end
