0aef9e1f7a7a94899370b40ff69e108089be02a4
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-08-10 13:14:57 +0100

    [?*] [formats.lua.load.syntax_split_comments] Detected problem and disabled code.

--- formats/lua/load/syntax_split_comments.lua
+++ formats/lua/load/syntax_split_comments.lua
@@ -1,3 +1,16 @@
+--[[
+  Broken!
+
+  It occured to be a bad idea to place unit A in hive that changes
+  another hive's units B.
+
+  Case is when you call both this unit A and code that calls unit B and
+  that relies on it's original behaviour.
+
+  I'm disabling this chunk till normal resolution.
+]]
+
+--[[
 local handy = request('!.mechs.processor.handy')
 local any_char = request('!.mechs.parser.handy').any_char
 
@@ -7,7 +20,7 @@ local opt_rep = handy.opt_rep
 local is_not = handy.is_not
 
 local comment = request('syntax.words.comment')
-comment.name = 'comment'
+comment.name = 'comment'  --here is the entry point for discrepancy
 local type_string = request('syntax.type_string')
 type_string.name = nil
 
@@ -23,3 +36,4 @@ return
       )
     ),
   }
+]]
