dc059e2c30ee5401b1a71810aa7ca09757fa7f3c
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-08-10 09:28:54 +0100

    [.] [-] Removed sketch code from lua modules.

--- mechs/streams/writable/tree/init.lua
+++
@@ -1,7 +0,0 @@
-return
-  function(self)
-    self.levels = {}
-    self.cur_level = 1
-    self.levels[self.cur_level] = new(self.c_sequence)
-    self.levels[self.cur_level]:init()
-  end
--- mechs/streams/writable/tree/move_up.lua
+++
@@ -1,8 +0,0 @@
-return
-  function(self)
-    if (self.levels == 1) then
-      error('Trying to move up from root level.')
-    end
-    self.cur_level = self.cur_level - 1
-    self.levels[self.cur_level]:set_next_position()
-  end
