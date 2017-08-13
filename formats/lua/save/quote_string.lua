bc8b634c776b7150903c183c2d636ac907fb0b8a
Ian <ian@somewhere.com>
2017-08-13 14:04:08 +0100

    [+~] [quote_string.dump] Added and integerated dump string quoting.
    Activeted when length of string fits common binary sizes
    (1, 2, 4, 6, 8, 10, 16 bytes) and string contains at non-ASCII
    character.

--- formats/lua/save/quote_string.lua
+++ formats/lua/save/quote_string.lua
@@ -1,18 +1,33 @@
-local quote_linear = request('quote_string.linear')
-local quote_intact = request('quote_string.intact')
+local quote_escaped = request('quote_string.linear')
+local quote_long = request('quote_string.intact')
+local quote_dump = request('quote_string.dump')
 
-local content_funcs = request('^.^.^.string.content_attributes')
+local content_funcs = request('!.string.content_attributes')
 local has_control_chars = content_funcs.has_control_chars
 local has_backslashes = content_funcs.has_backslashes
 local has_single_quotes = content_funcs.has_single_quotes
 local has_double_quotes = content_funcs.has_double_quotes
+local is_nonascii = content_funcs.is_nonascii
+
+local binary_entities_lengths =
+  {
+    [1] = true,
+    [2] = true,
+    [4] = true,
+    [6] = true,
+    [8] = true,
+    [10] = true,
+    [16] = true,
+  }
 
 return
   function(s)
     assert_string(s)
     local quote_func
-    if has_control_chars(s) then
-      quote_func = quote_linear
+    if binary_entities_lengths[#s] and is_nonascii(s) then
+      quote_func = quote_dump
+    elseif has_control_chars(s) then
+      quote_func = quote_escaped
     elseif
       has_backslashes(s) or
       (
@@ -20,9 +35,9 @@ return
         has_double_quotes(s)
       )
     then
-      quote_func = quote_intact
+      quote_func = quote_long
     else
-      quote_func = quote_linear
+      quote_func = quote_escaped
     end
     local result = quote_func(s)
     return result
