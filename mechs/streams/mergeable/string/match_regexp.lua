63e4b796b186af942c42783984ee47a792b14a3e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-08-13 13:28:48 +0100

    [.^] [streams.string.match_regexp] Documented.

--- mechs/streams/mergeable/string/match_regexp.lua
+++ mechs/streams/mergeable/string/match_regexp.lua
@@ -1,3 +1,12 @@
+--[[
+  Try to find regexp pattern from current position in stream.
+  If found,
+    set position past last character in found pattern,
+    return capture
+  else
+    return nothing
+]]
+
 return
   function(self, pattern)
     -- print(('match_regexp [%d, "%s"]'):format(self:get_position(), pattern))
