9be911989825d9f404ee2713c6de51e4efe355ad
Ian <ian@somewhere.com>
2017-08-13 19:13:02 +0100

    [*] [streams.file.set_position] Off-by-one error fixed.

--- mechs/streams/mergeable/file/set_position.lua
+++ mechs/streams/mergeable/file/set_position.lua
@@ -1,6 +1,6 @@
 return
   function(self, new_position)
     new_position = math.max(new_position, 1)
-    new_position = math.min(new_position, self.f:seek('end'))
+    new_position = math.min(new_position, self.f:seek('end') + 1)
     self.f:seek('set', new_position - 1)
   end
