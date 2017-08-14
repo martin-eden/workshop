896bd4c879f14de3bb51d6fb98230d05f0c41edd
Ian <ian@somewhere.com>
2017-08-14 12:40:42 +0100

    [.] [streams.file] Added possible debug output (disabled).

--- mechs/streams/mergeable/file/match_string.lua
+++ mechs/streams/mergeable/file/match_string.lua
@@ -1,6 +1,6 @@
 return
   function(self, str)
-    -- print(('match [%d, "%s"]'):format(self:get_position(), str))
     local segment = self:block_read(#str)
+    -- print(('match [%d, "%s"]'):format(self:get_position(), str), segment == str)
     return (segment == str)
   end
--- mechs/streams/mergeable/file/read.lua
+++ mechs/streams/mergeable/file/read.lua
@@ -1,5 +1,6 @@
 return
   function(self, num_positions)
+    -- print(('raw_read [%d, %d]'):format(self:get_position(), num_positions))
     local s = self.f:read(num_positions)
     if s then
       return s, #s
