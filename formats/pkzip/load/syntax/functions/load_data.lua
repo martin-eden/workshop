09cb5281604c087d00d2e3167f198ea7ffecc542
Ian <ian@somewhere.com>
2017-08-14 20:57:56 +0100

    [-] [formats.pkzip.old] Dropped replaced code.

--- formats/pkzip/load/syntax/functions/load_data.lua
+++
@@ -1,7 +0,0 @@
-return
-  function(in_stream, out_stream)
-    local int_data = out_stream.int_data
-    local slot_rec = int_data.slots[int_data.slots_pos]
-    in_stream:set_relative_position(slot_rec.length)
-    return true
-  end
--- formats/pkzip/load/syntax/functions/locate_rel.lua
+++
@@ -1,8 +0,0 @@
-return
-  function(name)
-    return
-      function(in_stream, out_stream)
-        in_stream:set_relative_position(out_stream.int_data[name])
-        return true
-      end
-  end
