2027098da91a22412c959a77cb8038f57efb26e6
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-08-14 13:05:32 +0100

    [.] [mechs.generic_loader] Updated var names.

--- mechs/generic_loader.lua
+++ mechs/generic_loader.lua
@@ -1,18 +1,19 @@
 local parse = request('parser.parse')
 
--- local c_string_stream = request('!.mechs.streams.mergeable.file.interface')
-local c_string_stream = request('!.mechs.streams.mergeable.string.interface')
+-- local c_stream = request('!.mechs.streams.mergeable.file.interface')
+local c_stream = request('!.mechs.streams.mergeable.string.interface')
 
 -- local profiler = request('!.system.profiler')
 -- local to_str = request('!.formats.lua_table_code.save')
 
 return
-  function(str, syntax, ...)
+  function(data, syntax, ...)
     -- profiler.start()
     -- print(to_str(syntax))
 
-    local input_stream = new(c_string_stream)
-    input_stream:init(str)
+    local input_stream = new(c_stream)
+    input_stream:init(data)
+
     local result
 
     local is_processed, data_struc = parse(input_stream, syntax)
