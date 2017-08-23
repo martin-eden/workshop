337aaa1415bb77b02f072630fc5b370feb055a81
Ian <ian@somewhere.com>
2017-08-23 08:18:08 +0100

    [^] [bin_ops.xor_strings] Switched to 8-byte chunks, from 4-byte chunks.

--- bare/bin_ops/xor_strings.lua
+++ bare/bin_ops/xor_strings.lua
@@ -6,18 +6,18 @@
 
 return
   function(s1, s2, len)
-    local last_chunk_len = len % 4
-    local num_full_chunks = len // 4
+    local last_chunk_len = len % 8
+    local num_full_chunks = len // 8
     local start_pos = 1
     local s1_int, s2_int, result_int
     local result = {}
 
     for i = 1, num_full_chunks do
-      s1_int = ('<I4'):unpack(s1, start_pos)
-      s2_int = ('<I4'):unpack(s2, start_pos)
+      s1_int = ('<I8'):unpack(s1, start_pos)
+      s2_int = ('<I8'):unpack(s2, start_pos)
       result_int = s1_int ~ s2_int
-      result[#result + 1] = ('<I4'):pack(result_int)
-      start_pos = start_pos + 4
+      result[#result + 1] = ('<I8'):pack(result_int)
+      start_pos = start_pos + 8
     end
 
     for i = 1, last_chunk_len do
