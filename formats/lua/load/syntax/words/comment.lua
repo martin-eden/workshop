899fda50efa7b13b935536c5cf141bad6727b4a2
Ian <ian@somewhere.com>
2017-08-26 14:16:15 +0100

    [/] [syntax.lua.comment] Comments are now named entities.
    Also one-line comment now includes line feed.

--- formats/lua/load/syntax/words/comment.lua
+++ formats/lua/load/syntax/words/comment.lua
@@ -5,9 +5,10 @@ local long_bracket = request('particles.long_bracket')
 
 return
   {
+    name = 'comment',
     '--',
     handy.cho(
       long_bracket,
-      match_regexp('[^\n\r]*')
+      match_regexp('[^\n\r]*[\n\r]?')
     )
   }
