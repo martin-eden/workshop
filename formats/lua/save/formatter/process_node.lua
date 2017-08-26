b614f7f30da6fff42ea272f13bcb89dc54674c4d
Ian <ian@somewhere.com>
2017-08-26 19:02:36 +0100

    [^] [formats.lua.formatter.process_node] Investigated and disabled false alarm.

--- formats/lua/save/formatter/process_node.lua
+++ formats/lua/save/formatter/process_node.lua
@@ -10,14 +10,22 @@ return
         return
       end
     else
-      print('!', 'type(node)', type(node))
-      if is_table(node) then
-        print('node.type', node.type)
-        if node[1] then
-          print('node[1].type', node[1].type)
+      if is_nil(node) then
+        --[[
+          Passed nil. It's ok.
+
+          (For example <local_named_funcion> may have .body = nil)
+        ]]
+      else
+        print('!', 'type(node)', type(node))
+        if is_table(node) then
+          print('node.type', node.type)
+          if node[1] then
+            print('node[1].type', node[1].type)
+          end
         end
+        print('There is some uncovered cases in code formatter.')
       end
-      print('There is some uncovered cases in code formatter.')
     end
     return true
   end
