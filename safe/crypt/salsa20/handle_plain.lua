92dbe56ce678ce9bfa01dca3343082a39fb6afae
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-09-01 21:44:39 +0100

    [^/] [safe.crypt.salsa20] Crydec is object now, not set of routines.

--- safe/crypt/salsa20/handle_plain.lua
+++
@@ -1,7 +0,0 @@
-return
-  function(a_plain)
-    local result
-    assert_string(a_plain, '<plaintext> must be a string.')
-    result = a_plain
-    return result
-  end
---
+++ safe/crypt/salsa20/helpers/handle_input.lua
@@ -0,0 +1,7 @@
+return
+  function(a_input)
+    local result
+    assert_string(a_input)
+    result = a_input
+    return result
+  end
