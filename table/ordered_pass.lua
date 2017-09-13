39d2ebec98f23c65f2f77f208570604bea0a87ec
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-09-13 10:09:47 +0100

    [.] [table.ordered_pass]

--- table/ordered_pass.lua
+++ table/ordered_pass.lua
@@ -1,5 +1,4 @@
 local default_comparator = request('ordered_pass.default_comparator')
-local extract_keys = request('extract_keys')
 local to_key_val = request('to_key_val')
 
 -- Sort <t> and return iterator function to pass that sorted <t>
