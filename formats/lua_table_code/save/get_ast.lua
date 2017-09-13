a8480fd69a33b2882800d06c4654e3b26218e1c9
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-09-13 20:15:49 +0100

    [.^] [formats.lua_table_code.save.get_ast] No more drifting table names.

--- formats/lua_table_code/save/get_ast.lua
+++ formats/lua_table_code/save/get_ast.lua
@@ -31,10 +31,10 @@ local may_print_inline =
   end
 
 local get_assembly_order = request('!.mechs.graph.assembly_order')
-local name_giver = request('!.mechs.name_giver')
 
 return
   function(self, data)
+    local name_giver = new(request('!.mechs.name_giver'))
     local table_serializer = self.table_serializer
     local table_iterator = table_serializer.table_iterator
 
