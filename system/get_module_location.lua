2a798a230eac3f79afa95cd62319ee4d54782fc9
Ian <ian@somewhere.com>
2017-09-18 00:00:29 +0100

    [^] [system.get_module_location] Implemented via standard function.

--- system/get_module_location.lua
+++ system/get_module_location.lua
@@ -1,30 +1,17 @@
 --[[
-  Receives correct module name, require()-ready.
-  Returns file pathname which will be loaded as this module.
+  For correct module name, require()-ready,
+  return file pathname which will be loaded as this module.
 
-  Notes
-    * Uses _G.package.path.
-    * Not all cases can be resolved: module may be loaded but we can't
-      figure file's name. This is possible at least when
-        * direct writing to <package.loaded>
-        * <require> overriden with custom function (in this case record
-          for module in <package.loaded> may be absent)
-        * module is loaded via custom function in <package.searchers>
+  * Not all cases can be resolved: module may be loaded but we can't
+    figure file's name. This is possible at least when
+    * <package.loaded> was directly modified
+    * <require> overriden with custom function (in this case record
+      for module in <package.loaded> may be absent)
+    * module is loaded via custom function in <package.searchers>
 ]]
-
-local split = request('!.string.split')
-local file_exists = request('!.file.exists')
 
 return
   function(module_name)
     assert_string(module_name)
-    module_name = module_name:gsub('%.', '/')
-    local name_patterns = split(package.path, ';')
-    for i = 1, #name_patterns do
-      local possible_file_name = name_patterns[i]
-      possible_file_name = possible_file_name:gsub('%?', module_name)
-      if file_exists(possible_file_name) then
-        return possible_file_name
-      end
-    end
+    return (package.searchpath(module_name, _G.package.path))
   end
