90c0fd3c7cd4b45a47c93cd21d903529ab0aef6f
Ian <ian@somewhere.com>
2017-09-26 00:25:57 +0100

    [~] [mechs.deploy_script] Relinked to now common [strip_updirs] and [strip_self_prefix].

--- mechs/deploy_script/interface.lua
+++ mechs/deploy_script/interface.lua
@@ -1,34 +1,24 @@
 local get_cmd_copy = request('!.bare.file_system.get_cmd_copy')
 local get_cmd_mkdir = request('!.bare.file_system.get_cmd_mkdir')
 
-local directories_created
-
-local get_cmd_mkdir_cached =
-  function(dir_name)
-    if not directories_created[dir_name] then
-      local parent_path = ''
-      for parent_dir in dir_name:gmatch('(.-)/') do
-        parent_path = parent_path .. parent_dir
-        directories_created[parent_path] = true
-        parent_path = parent_path .. '/'
-      end
-      directories_created[dir_name] = true
-      return get_cmd_mkdir(dir_name)
+local mark_directory_created =
+  function(dir_name, directories_created)
+    local parent_path = ''
+    for parent_dir in dir_name:gmatch('(.-)/') do
+      parent_path = parent_path .. parent_dir
+      directories_created[parent_path] = true
+      parent_path = parent_path .. '/'
     end
-  end
-
-local strip_updirs =
-  function(s)
-    return s:gsub('%.%./', '')
+    directories_created[dir_name] = true
   end
 
 local get_modules_dependencies = request('!.system.get_modules_dependencies')
 local get_module_location = request('!.system.get_module_location')
+local strip_updirs = request('!.string.file_name.strip_updirs')
 
 local get_deploy_script =
   function(modules, deploy_dir)
     deploy_dir = (deploy_dir or 'deploy/')
-    directories_created = {}
 
     local files = get_modules_dependencies(modules)
     for i = 1, #files do
@@ -38,11 +28,15 @@ local get_deploy_script =
 
     local result = {}
 
+    local directories_created = {}
     for i = 1, #files do
       local source = files[i]
       local dest = deploy_dir .. strip_updirs(files[i])
       local directory = dest:match('(.+)/')
-      result[#result + 1] = get_cmd_mkdir_cached(directory)
+      if not directories_created[directory] then
+        mark_directory_created(directory, directories_created)
+        result[#result + 1] = get_cmd_mkdir(directory)
+      end
       result[#result + 1] = get_cmd_copy(source, dest)
     end
     result[#result + 1] = ''
