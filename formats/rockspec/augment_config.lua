9c826757d546740200b2038073a97e866cc8b9dd
Ian <ian@somewhere.com>
2017-09-26 04:02:46 +0100

    [^+] [formats.rockspec] Required parameters extended.
    Now there is no need to edit generated rockspec file by hands
    (at least for my case).

--- formats/rockspec/augment_config.lua
+++ formats/rockspec/augment_config.lua
@@ -1,19 +1,42 @@
 local get_loaded_module_files = request('!.system.get_loaded_module_files')
 local strip_lua_postfix = request('strip_lua_postfix')
+local get_modules_dependencies = request('!.system.get_modules_dependencies')
+local get_module_location = request('!.system.get_module_location')
 
 return
   function(cfg)
     assert_string(cfg.project_name)
+    assert_string(cfg.short_desc)
+    assert_string(cfg.description)
+    assert_string(cfg.license)
+    assert_table(cfg.repository)
+    assert_string(cfg.repository.url)
+    assert_string(cfg.repository.branch)
+
     cfg.rockspec_name = ('%s-scm-1.rockspec'):format(cfg.project_name)
-    cfg.used_files = cfg.used_files or get_loaded_module_files()
+
+    assert_table(cfg.used_modules)
 
     if cfg.bash_commands then
       assert_table(cfg.bash_commands)
       for _, rec in ipairs(cfg.bash_commands) do
         assert_string(rec.script)
         assert_string(rec.command)
-        assert_string(rec.lua_module)
-        rec.lua_module = strip_lua_postfix(rec.lua_module)
+        assert_string(rec.lua_script)
+        rec.lua_script = strip_lua_postfix(rec.lua_script)
       end
     end
+
+    local used_modules = get_modules_dependencies(cfg.used_modules)
+    if cfg.bash_commands then
+      for _, rec in ipairs(cfg.bash_commands) do
+        table.insert(used_modules, rec.lua_script)
+      end
+    end
+    local used_files = {}
+    for i = 1, #used_modules do
+      used_files[i] = get_module_location(used_modules[i])
+    end
+    table.sort(used_files)
+    cfg.used_files = used_files
   end
--- formats/rockspec/get_rockspec.lua
+++ formats/rockspec/get_rockspec.lua
@@ -3,6 +3,7 @@ local get_modules_text = request('get_modules_text')
 local get_shell_scripts_text = request('get_shell_scripts_text')
 local rockspec_template = request('rockspec_template')
 local fill_template = request('fill_template')
+local quote = request('!.string.quote')
 
 return
   function(cfg)
@@ -16,9 +17,14 @@ return
     end
     local substitutions =
       {
-        package = cfg.project_name,
-        modules = get_modules_text(modules),
+        package = quote(cfg.project_name),
+        repository_url = quote(cfg.repository.url),
+        repository_branch = quote(cfg.repository.branch),
+        short_desc = quote(cfg.short_desc),
+        description = quote(cfg.description),
+        license = quote(cfg.license),
         shell_scripts = get_shell_scripts_text(shell_scripts),
+        modules = get_modules_text(modules),
       }
     local result = fill_template(rockspec_template, substitutions)
     return result
