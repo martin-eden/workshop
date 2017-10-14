ac5cd713ab41a28cce2f4594ae803fb05e118a61
Ian <ian@somewhere.com>
2017-10-14 02:07:33 +0100

    [-] [mechs.command_line_processor] Dropped parts for printing usage text.

--- mechs/command_line_processor/interface.lua
+++ mechs/command_line_processor/interface.lua
@@ -7,11 +7,7 @@ return
         {name = 'f_out_name', type = 'string'},
       },
     run = request('run'),
-    -- may
-    description = 'Do some file processing.',
-    usage = '<f_in> <f_out>',
     -- implementation
-    print_usage = request('print_usage'),
     parse_args = request('parse_args'),
     classify_item = request('classify_item'),
     get_key_name = request('get_key_name'),
