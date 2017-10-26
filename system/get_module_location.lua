af11395f96bc23470c66f91bc69278989a66092a
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2017-10-26 18:50:47 +0100

    [~] [5.1] [system.get_module_location] "package.searchpath" avoidance.

--- system/get_module_location.lua
+++ system/get_module_location.lua
@@ -11,12 +11,20 @@
 ]]
 
 local normalize_name = request('!.file_system.file.normalize_name')
+local split = request('!.string.split')
+local file_exists = request('!.file_system.file.exists')
 
 return
   function(module_name)
     assert_string(module_name)
-    local result
-    result = package.searchpath(module_name, _G.package.path)
-    result = normalize_name(result)
-    return result
+    module_name = module_name:gsub('%.', '/')
+    local name_patterns = split(package.path, ';')
+    for i = 1, #name_patterns do
+      local possible_file_name = name_patterns[i]
+      possible_file_name = possible_file_name:gsub('%?', module_name)
+      possible_file_name = normalize_name(possible_file_name)
+      if file_exists(possible_file_name) then
+        return possible_file_name
+      end
+    end
   end
