f346a6c46b07fecf105a6708c225a0e0b8d451cc
Ian <ian@somewhere.com>
2017-10-26 18:24:27 +0100

    [~] [5.1] [mechs.text_block.line.init] Use "string.len" instead of "utf8.len".

--- mechs/text_block/line/init.lua
+++ mechs/text_block/line/init.lua
@@ -1,5 +1,5 @@
 return
   function(self)
     self.indents_obj:init()
-    self.chunk_length = utf8.len(self.indents_obj.indent_chunk)
+    self.chunk_length = string.len(self.indents_obj.indent_chunk)
   end
