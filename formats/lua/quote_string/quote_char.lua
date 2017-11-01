1c46783678308c27a4f3bea115803f7302558a31
Ian <ian@somewhere.com>
2017-11-01 03:15:14 +0000

    [~] [5.1] [formats.lua.save.quote_string.quote_char] Switched to \DDD quoting.

--- formats/lua/quote_string/quote_char.lua
+++ formats/lua/quote_string/quote_char.lua
@@ -1,5 +1,5 @@
 return
   function(c)
     --this is going to be called as gsub()'s function, so no arg checks
-    return ([[\x%02X]]):format(c:byte(1, 1))
+    return ([[\%03d]]):format(c:byte(1, 1))
   end
