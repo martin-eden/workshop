4a21b29a0abc2c533794b15e9c34f48af8a98af2
Ian <ian@somewhere.com>
2018-01-25 17:58:52 +0300

    [^] [lua.code.get_ast] Now possible shebang #! part is stripped from file data.

--- lua/code/get_ast.lua
+++ lua/code/get_ast.lua
@@ -2,13 +2,17 @@
   Get AST for string with Lua code.
 
   str -> table
+
+  Handles possible "#!" at start of shell files.
 ]]
 
 local parse = request('!.mechs.generic_loader')
 local syntax = request('!.formats.lua.syntax')
+local parse_sh = request('!.formats.sh.load')
 
 return
   function(s)
     assert_string(s)
-    return parse(s, syntax)
+    local sh = parse_sh(s)
+    return parse(sh.data, syntax)
   end
