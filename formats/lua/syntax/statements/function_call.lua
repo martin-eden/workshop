af42a67d9a86a7a23d1d39085e6d0dc6bc21e207
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-02-02 15:28:25 +0300

    [^] [syntax.lua.function_call] Simplified grammar.
    .. same way as [var_ref].
    
    [.^] ASCII scheme

--- formats/lua/syntax/statements/function_call.lua
+++ formats/lua/syntax/statements/function_call.lua
@@ -1,14 +1,14 @@
 --[[
   Function call grammar.
 
-  Same structure as [var_ref] but must end in <call_args>.
+  Almost same structure as [var_ref] but must end in <call_args>.
 
-  Sample:
+  <function_call>:
 
-    (a).b[c]:d()().e(f)(g){}""
-            ~~~~~~  ~~~~~~~~~~ call_args
-       ~~~~~      ~~           name_continuation
-    ~~~                        name | par_expr
+                        -----------------------------
+                        V                           |
+    ---+- <name> -----+---+- <call_args> ---------+---- <do_ends_in_call_args()> ---
+       +- <par_expr> -+   +- <name_continuation> -+
 ]]
 
 local handy = request('!.mechs.processor.handy')
@@ -22,13 +22,20 @@ local par_expr = request('^.wrappers.par_expr')
 local name_continuation = request('^.qualifiers.name_continuation')
 local call_args = request('^.qualifiers.call_args')
 
+local t2s = request('!.table.as_string')
+
+local do_ends_in_call_args =
+  function(input_stream, output_stream)
+    local result =
+      (output_stream.seq[output_stream.position - 1].type == 'func_args')
+    -- print(t2s(output_stream), output_stream.seq[output_stream.position - 1].type, result)
+    return result
+  end
+
 return
   {
     name = 'function_call',
     cho(name, par_expr),
-    rep(
-      opt_rep(name_continuation),
-      call_args
-    ),
-    is_not(name_continuation),
+    rep(cho(call_args, name_continuation)),
+    do_ends_in_call_args,
   }
