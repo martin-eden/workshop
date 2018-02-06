8f1d5e3edf14ff5a60313e937d57a6c78ecd9380
Ian <ian@somewhere.com>
2018-02-06 15:29:33 +0300

    [*] [formats.sh.load] Added handling empty file case.

--- formats/sh/load.lua
+++ formats/sh/load.lua
@@ -12,17 +12,19 @@ local get_next_line = request('!.string.lines.get_next_line')
 
 return
   function(s)
-    local new_pos, line = get_next_line(s)
     local result =
       {
         tool = nil,
         data = '',
       }
-    if (line:sub(1, 2) == '#!') then
+
+    local new_pos, line = get_next_line(s)
+    if line and (line:sub(1, 2) == '#!') then
       result.tool = line:match('#!%s*(.*)')
       result.data = s:sub(new_pos)
     else
       result.data = s
     end
+
     return result
   end
