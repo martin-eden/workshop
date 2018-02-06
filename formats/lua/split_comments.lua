f14e2debd694ef4d0847aa9394139dc3a21446a4
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-02-06 17:49:32 +0300

    [/] [formats.lua.split_comments] Moved up from "ast_transformer".
    It does not belong to formatter-ast-transformer. But technically it is
    still ast-transformer. Probably I should rename that directory.

--- formats/lua/split_comments.lua
+++ formats/lua/split_comments.lua
@@ -1,3 +1,14 @@
+--[[
+  ABANDONED
+
+  Transform AST to "code" and "comments" parts.
+
+  Currently not used and spoiled. To make this code work custom
+  Lua grammar for AST should be constructed. (It _was_ constructed
+  via loading and changing of full Lua grammar. But guerrilla-patching
+  occurred to be a bad idea.)
+]]
+
 return
   function(data_struc)
     assert_table(data_struc)
