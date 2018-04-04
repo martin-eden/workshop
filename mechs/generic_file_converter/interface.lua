9af5698728fe21fd4640148664417ca80a65d19d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-04-04 16:37:43 +0300

    [^+] [generic_file_converter] Added "tool_name" field.
    .. to visually separate consecutive converter calls.

--- mechs/generic_file_converter/interface.lua
+++ mechs/generic_file_converter/interface.lua
@@ -1,15 +1,16 @@
 return
   {
-    -- core
+    -- parameters
     f_in_name = '',
     f_out_name = '',
-    init = request('init'),
-    run = request('run'),
-    -- adjustable
+    tool_name = '',
     load = request('!.file.text_file_as_string'),
     parse = request('!.formats.lua_table_code.load'),
     compile = request('compile'),
     save = request('!.string.save_to_file'),
+    -- runmes
+    run = request('run'),
     -- implementation
     say = request('say'),
+    init = request('init'),
   }
