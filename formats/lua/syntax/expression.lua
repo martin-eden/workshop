74415b0e0bcde79313c8d6bdaa2a0379ca830a2a
Ian <ian@somewhere.com>
2018-05-23 17:12:16 +0300

    [^<] [syntax.lua.expression] Exported named parts.

--- formats/lua/syntax/expression.lua
+++ formats/lua/syntax/expression.lua
@@ -1,68 +1,23 @@
 --[[
+  Expression grammar.
 
   <expression>:
 
-    ---+--------------+---+- ... -------------+---+----------------------------+---
-       | ------------ |   +- <nil> -----------+   | -------------------------- |
-       | V          | |   +- <boolean>--------+   | V                        | |
-       +-- <un_op> ---+   +- <number> --------+   +-- <bin_op> <expression> ---+
-                          +- <string> --------+
-                          +- <table> ---------+
-                          +- <function> ------+
-                          +- <var_or_call> ---+
+    -+---------------+---+- ... -----------+---+-----------------------------+-
+     | ------------- |   +- <nil> ---------+   | --------------------------- |
+     | V           / |   +- <boolean>------+   | V                         / |
+     +--- <un_op> ---+   +- <number> ------+   +--- <bin_op> <expression> ---+
+                         +- <string> ------+
+                         +- <table> -------+
+                         +- <function> ----+
+                         +- <var_or_call> -+
 ]]
 
 local handy = request('!.mechs.processor.handy')
 local opt_rep = handy.opt_rep
 local cho = handy.cho
-local match_regexp = request('!.mechs.parser.handy').match_regexp
 
-local word = request('words.word')
-local opt_spc = request('words.opt_spc')
-
-local un_op =
-  {
-    name = 'un_op',
-    cho(
-      {opt_spc, match_regexp('[%-%#%~]')},
-      word('not')
-    ),
-  }
-
---[[
-  Order
-
-    <x> must be before <y> because <y> is a prefix of <x>:
-
-    <x> <y>
-
-    //   /
-    ~=   ~
-    <<   <
-    <=   <
-    >>   >
-    >=   >
-]]
-
-local bin_op =
-  {
-    name = 'bin_op',
-    opt_spc,
-    cho(
-      '//',
-      '==',
-      '~=',
-      '<<',
-      '<=',
-      '>>',
-      '>=',
-      '..',
-      match_regexp('[%+%-%*%/%^%%%&%~%|%<%>]'),
-      word('and'),
-      word('or')
-    ),
-  }
-
+local un_op = request('expression.un_op')
 local vararg = request('words.vararg')
 local type_nil = request('type_nil')
 local type_boolean = request('type_boolean')
@@ -71,6 +26,7 @@ local type_string = request('type_string')
 local type_table = request('type_table')
 local type_function = request('type_function')
 local var_or_call = request('qualifiers.var_or_call')
+local bin_op = request('expression.bin_op')
 
 return
   {
