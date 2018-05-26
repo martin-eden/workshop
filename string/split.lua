0cfd77e5c260ebfa58d48bc27fb1bf83971d17b5
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-05-26 17:39:57 +0300

    [^] [string.split] Documented.

--- string/split.lua
+++ string/split.lua
@@ -1,7 +1,25 @@
+--[[
+  Split string in pieces.
+
+  Gets source string with delimiter and delimiter string.
+  Returns table with sequence of pieces, without delimiter string.
+  Delimiter may not be present at end of string. So result for
+  ('a;b', ';') is same as for ('a;b;', ';').
+
+  Input:
+    s: string - source string
+    opt delim: string - delimiter string
+      default: "\n"
+
+  Output:
+    table - list of strings
+]]
+
 return
   function(s, delim)
     assert_string(s)
     local delim = delim or '\n'
+    assert_string(delim)
     local result = {}
     local last_pos = 1
     for line, _last_pos in string.gmatch(s, '(.-)' .. delim .. '()') do
