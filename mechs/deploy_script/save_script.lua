33d721816e93e776888b1e3b7ba92c5a3a5ac72a
Ian <ian@somewhere.com>
2018-06-05 18:44:56 +0300

    [^] [mechs.deploy_script.save_script]

--- mechs/deploy_script/save_script.lua
+++ mechs/deploy_script/save_script.lua
@@ -1,15 +1,15 @@
-local get_script = request('get_script')
+--[[
+  Generate script and save it to file.
 
-local save_script =
-  function(modules, script_name, deploy_dir)
+  - tbl ----+-------------+- -> nil
+   (self)   +---- str ----+
+             (script_name)
+
+  script_name - name of Bash script. Default: "deploy.sh".
+]]
+
+return
+  function(self, script_name)
     script_name = script_name or 'deploy.sh'
-    local f, err_msg = io.open(script_name, 'w+')
-    if not f then
-      error(err_msg)
-    end
-    f:write('#!/bin/bash', '\n')
-    f:write(get_script(modules, deploy_dir))
-    f:close()
+    self.bash_script_writer:save_script(script_name)
   end
-
-return save_script
