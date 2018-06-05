c61313a2f778ec5ae24689c19bd07d70bdb44565
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-06-05 17:02:48 +0300

    [+] [mechs.bash_script.save_script]

---
+++ mechs/bash_script/save_script.lua
@@ -0,0 +1,23 @@
+--[[
+  Generate script and save it to given file.
+
+  - tbl ---+-----------+- -> nil
+   (self)  +--- str ---+
+            (file_name)
+
+  file_name - Bash script file name. Default: "run.sh".
+]]
+
+local safe_open = request('!.file.safe_open')
+
+return
+  function(self, file_name)
+    file_name = file_name or 'run.sh'
+    assert_string(file_name)
+
+    local script_str = self:get_script()
+
+    local f = safe_open(file_name, 'w')
+    f:write(script_str)
+    f:close()
+  end
