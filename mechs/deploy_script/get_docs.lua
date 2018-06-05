f81a168244996f93db8a664bd5d79972634ba130
Ian <ian@somewhere.com>
2018-06-05 18:44:10 +0300

    [+] [mechs.deploy_script.get_docs]

---
+++ mechs/deploy_script/get_docs.lua
@@ -0,0 +1,35 @@
+--[[
+  Get documentation files for deploy script. Internal function.
+
+  Files added:
+
+    <directory> wtf.txt
+    <directory> <name>.txt
+]]
+
+local parse_pathname = request('!.formats.path_name.parse')
+local exists = request('!.file.exists')
+local get_keys = request('!.table.get_keys')
+
+return
+  function(self, files)
+    local docs = {}
+    local was_checked = {}
+    for i = 1, #files do
+      local possible_docs =
+        {
+          files[i]:gsub('%.lua$', '.txt'),
+          parse_pathname(files[i]).directory .. 'wtf.txt',
+        }
+      for _, file_name in ipairs(possible_docs) do
+        if not was_checked[file_name] then
+          if exists(file_name) then
+            docs[file_name] = true
+          end
+          was_checked[file_name] = true
+        end
+      end
+    end
+    docs = get_keys(docs)
+    return docs
+  end
