c0a7a72b1350847fa8bd29a046722505e82d3242
Ian <ian@somewhere.com>
2018-06-05 18:46:27 +0300

    [~] [system.create_deploy_script] Relinked with "deploy_script_generator".

--- system/create_deploy_script.lua
+++ system/create_deploy_script.lua
@@ -4,9 +4,11 @@
   File created in current directory.
 ]]
 
-local deploy_maker = request('!.mechs.deploy_script.interface')
+local c_deploy_maker = request('!.mechs.deploy_script_generator.interface')
 
 return
   function(modules, script_name, deploy_dir_name)
-    deploy_maker.save_script(modules, script_name, deploy_dir_name)
+    local deploy_maker = new(c_deploy_maker)
+    deploy_maker:populate(modules, deploy_dir_name)
+    deploy_maker:save_script(script_name)
   end
