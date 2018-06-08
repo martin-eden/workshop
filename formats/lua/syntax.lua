f844e90e519f3c127ee39f83a094c1b6428d12c6
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-06-08 14:23:42 +0300

    [.^] [formats.lua.syntax] Better comment.

--- formats/lua/syntax.lua
+++ formats/lua/syntax.lua
@@ -1,16 +1,22 @@
--- These are core cycle nodes. Loaded separately to avoid cycles in require()
+--[[
+  Grammar for expressions and statements is mutually linked.
+  To this moment it was just ">expression" and ">statements" strings.
+  Now we load their grammars and change those strings to that.
+]]
+
+local link = request('!.mechs.processor.link')
+local optimize = request('!.mechs.processor.optimize')
 
 local expression = request('syntax.expression')
 local statements = request('syntax.statements')
 
 local producers, consumers = {}, {}
 
-local link = request('!.mechs.processor.link')
 link(expression, producers, consumers)
 link(statements, producers, consumers)
+
 assert(not next(consumers))
 
-local optimize = request('!.mechs.processor.optimize')
 optimize(statements)
 
 return statements
