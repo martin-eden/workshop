17d0ec8d3da483d76be4e5c1e10aad9797872c7a
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-07-19 18:15:04 +0300

    [^] [syntax.lua.name] Documented and explained implementation.
    Also renamed inner variable.

--- formats/lua/syntax/words/name.lua
+++ formats/lua/syntax/words/name.lua
@@ -1,19 +1,39 @@
+--[[
+  Grammar for name.
+
+  "Name" is alphanumeric sequence of characters. With exclusion of
+  reserved keywords.
+
+  Checking implemented as function because it is a hot code and
+  you can't exclude list of words via Lua patterns.
+
+  You can implement this in existing elements as
+
+    { is_not(cho(<keyword>, ...)), match_regexp(<pattern>) }
+
+  but this is less effective algorithmically.
+]]
+
 local match_regexp = request('!.mechs.parser.handy').match_regexp
 local opt_spc = request('opt_spc')
 
-local keywords = request('!.formats.lua.keywords')
 local name_pattern = '^[_A-Za-z][_A-Za-z0-9]*'
+local keywords = request('!.formats.lua.keywords')
 
 local is_name =
   function(in_stream, out_stream)
     local init_pos = in_stream:get_position()
     if in_stream:match_regexp(name_pattern) then
-      local s = in_stream:get_segment(init_pos, in_stream:get_position() - init_pos)
-      if keywords[s] then
+      local capture =
+        in_stream:get_segment(
+          init_pos,
+          in_stream:get_position() - init_pos
+        )
+      if not keywords[capture] then
+        return true
+      else
         in_stream:set_position(init_pos)
         return false
-      else
-        return true
       end
     end
   end
