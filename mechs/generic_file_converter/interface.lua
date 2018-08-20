b73d9bb8c216ca8d6b0f6b5d038d4e0585f6dba8
Ian <ian@somewhere.com>
2018-08-20 09:34:58 +0300

    [/] [mechs.generic_file_converter] Added .transform(). Fallback logic change..
    .. table result of compile() no longer serialized to string.
    Also this result no longer required to be string.
    
    Case to made these changes was processing table and writing result
    to stream. This may be done only in save() method and overriding
      * transform(), compile() to identity function
      * save() to init stream by file name, and doing actual processing

--- mechs/generic_file_converter/interface.lua
+++ mechs/generic_file_converter/interface.lua
@@ -1,3 +1,22 @@
+--[[
+  Generic file converter interface.
+
+  Usage core:
+
+    Fill (<f_in_name> <f_out_name>)
+    Call <init>
+    Call <run>
+
+  For useful usage you'll need to override some of the following
+  methods: (<load> <parse> <transform> <compile> <save>)
+
+  Execution stages:
+
+    arg/result:  file string  table     table   string file
+                   ~~~~~  ~~~~~~ ~~~~~~~~~ ~~~~~~~  ~~~~~
+    method:        load   parse  transform compile  save
+]]
+
 return
   {
     -- parameters
@@ -6,6 +25,7 @@ return
     action_name = '',
     load = request('!.file.text_file_as_string'),
     parse = request('!.formats.lua_table_code.load'),
+    transform = request('!.function.identity'),
     compile = request('!.table.as_string'),
     save = request('!.string.save_to_file'),
     -- runmes
