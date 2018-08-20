2b3cd05b8113412bdf13d86fdbd26d0fc17a7a1c
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-08-20 12:32:28 +0300

    [.] [streams.file.init]

--- mechs/streams/mergeable/file/init.lua
+++ mechs/streams/mergeable/file/init.lua
@@ -3,7 +3,8 @@ return
     local f_type = io.type(f)
     if (f_type ~= 'file') then
       error(
-        ('Wrong <f> type: "%s" while "%s" awaited.'):format(f_type, 'file'), 2
+        ('Wrong <f> type: "%s" while "file" awaited.'):
+        format(f_type), 2
       )
     end
     self.f = f
