ce0e5418a163f519d4fe64360bca93f893a08345
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-08-20 14:55:03 +0300

    [*] [streams.file.read] Added handling of border case.

--- mechs/streams/mergeable/file/read.lua
+++ mechs/streams/mergeable/file/read.lua
@@ -1,6 +1,19 @@
+--[[
+  Implementation detail:
+
+    Explicit check for <num_positions> == 0 is needed for case
+    when we read zero bytes at end of file. In this case io.read()
+    will return nil instead of empty string.
+]]
+
 return
   function(self, num_positions)
     -- print(('raw_read [%d, %d]'):format(self:get_position(), num_positions))
+
+    if (num_positions == 0) then
+      return '', 0
+    end
+
     local s = self.f:read(num_positions)
     if s then
       return s, #s
