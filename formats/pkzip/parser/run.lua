33aeb215924138c3ec03d16aec895ed7ca35f557
Ian <ian@somewhere.com>
2018-08-25 19:07:29 +0300

    [~] [formats.pkzip.parser.run] <retrieve_file_data> default value.

--- formats/pkzip/parser/run.lua
+++ formats/pkzip/parser/run.lua
@@ -2,13 +2,20 @@ local process_locators_link = request('process_locators_link')
 local process_locators = request('process_locators')
 local process_files = request('process_files')
 
+local default_options =
+  {
+    retrieve_file_data = false,
+  }
+
 return
-  function(stream)
+  function(stream, options)
+    options = new(default_options, options)
+
     local result = {}
 
     process_locators_link(stream, result)
     process_locators(stream, result)
-    process_files(stream, result)
+    process_files(stream, result, options)
 
     return result
   end
