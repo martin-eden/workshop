152de67b5bbef792a65dd54206366cab0ecc8f63
Ian <ian@somewhere.com>
2018-08-28 19:10:53 +0300

    [~] [formats.pkzip.parser.process_files] Link with [get_data_size].

--- formats/pkzip/parser/process_files.lua
+++ formats/pkzip/parser/process_files.lua
@@ -36,6 +36,7 @@ local parse_file_rec = request('parse.file_rec')
 local parse_post_file_rec = request('parse.post_file_rec')
 local locate_zip64_rec = request('locate_zip64_rec')
 local name_zip64_fields_file_rec = request('name_zip64_fields_file_rec')
+local get_data_size = request('get_data_size')
 
 local FFx4 = 0xFFFFFFFF
 
@@ -62,36 +63,16 @@ return
         name_zip64_fields_file_rec(file_rec)
       end
 
-      local data_size
-      local bit_3_is_set =
-        ('< I2'):unpack(file_rec.bit_flag) & (1 << 2) > 0
-
-      -- Actual compressed size is stored in locator rec?
-      if bit_3_is_set then
-        data_size = locator.compressed_size
-        -- Size too big, so stored in Zip64 record?
-        if (data_size == FFx4) then
-          local zip64_rec = locate_zip64_rec(locator)
-          if zip64_rec then
-            data_size = zip64_rec.compressed_size
-          end
-        end
-      else
-        data_size = file_rec.compressed_size
-        -- Size too big, so stored in Zip64 record?
-        if (data_size == FFx4) then
-          local zip64_rec = locate_zip64_rec(file_rec)
-          if zip64_rec then
-            data_size = zip64_rec.compressed_size
-          end
-        end
-      end
+      local data_size = get_data_size(locator)
 
       if options and options.retrieve_file_data then
         file_rec.data = stream:read(data_size)
       else
         stream:set_relative_position(data_size)
       end
+
+      local bit_3_is_set =
+        ('< I2'):unpack(file_rec.bit_flag) & (1 << 2) > 0
 
       if bit_3_is_set then
         file_rec.post_rec = parse_post_file_rec(stream)
