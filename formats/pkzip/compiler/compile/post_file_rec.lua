0a6442e6b8995308fc1f9e438045b85635e2436f
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-08-28 19:29:39 +0300

    [/] [formats.pkzip.compile.post_file_rec] Do not use stream, return string.
    This is due passed "meta.offset" of record.

--- formats/pkzip/compiler/compile/post_file_rec.lua
+++ formats/pkzip/compiler/compile/post_file_rec.lua
@@ -8,19 +8,17 @@
   compressed_size, 4
   original_size, 4
 
-  As signature is optional this creates problems in restioring file.
+  As signature is optional this creates problems in restoring file.
   For this reason signature, if present, is stored in <meta.signature>.
 ]]
 
 local record = '< I4 I4 I4'
 
 return
-  function(rec, stream)
-    stream:set_position(rec.meta.offset + 1)
+  function(rec)
+    local result = {}
 
-    if rec.meta.signature then
-      stream:write(rec.meta.signature)
-    end
+    table.insert(result, rec.meta.signature)
 
     local chunk =
       record:pack(
@@ -28,5 +26,9 @@ return
         rec.compressed_size,
         rec.original_size
       )
-    stream:write(chunk)
+    table.insert(result, chunk)
+
+    result = table.concat(result)
+
+    return result
   end
