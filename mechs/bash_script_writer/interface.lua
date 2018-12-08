bf2aa7cd3b74aab0b8db805e9885c8a09a1d8642
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-12-08 08:20:40 +0300

    [+] [mechs.bash_script_writer.interface] <dirs_to_delete> list.

--- mechs/bash_script_writer/interface.lua
+++ mechs/bash_script_writer/interface.lua
@@ -3,17 +3,19 @@
 
     * Clone.
 
-    *  ----------------
-       V              /
-      --- copy_file -+--+- get_script --+-
-                        +- save_script -+
+    *  ----------------.
+       V               |
+      --+- copy_file --+---+- get_script --+-
+        +- delete_dir -+   +- save_script -+
 ]]
 
 return
   {
     copy_file = request('copy_file'),
+    delete_dir = request('delete_dir'),
     get_script = request('get_script'),
     save_script = request('save_script'),
     --
     files_to_copy = {},
+    dirs_to_delete = {},
   }
