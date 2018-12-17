f3465eff83289ac985ebef6442caf04b8b6ffcf7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2018-12-17 17:30:13 +0300

    [*] [formats.lua.quote_string.intact] Fixed incorrect newline addition.
    Specific case when it was incorrect is "\0x0D@". Which was converted
    to "\0x0D0x0A@" which is still one newline.
    
    Case and elegant solution provided by Andrew Gierth from lua-l list.

--- formats/lua/quote_string/intact.lua
+++ formats/lua/quote_string/intact.lua
@@ -2,7 +2,7 @@ return
   function(s)
     assert_string(s)
 
-    -- see (1)
+    -- (1)
     s = s .. ']'
 
     local eq_chunk = ''
@@ -17,12 +17,13 @@ return
 
     local prefix = '[' .. eq_chunk .. '['
 
-    -- Handling special case: heading newline dropped in long strings.
+    -- (2)
+    local first_char = s:sub(1, 1)
     if
-      (s:sub(1, 2) == '\x0d\x0a') or
-      (s:sub(1, 1) == '\x0a')
+      (first_char == '\x0d') or
+      (first_char == '\x0a')
     then
-      prefix = prefix .. '\n'
+      prefix = prefix .. first_char
     end
 
     return prefix .. s .. eq_chunk .. ']'
@@ -43,4 +44,21 @@ return
 
     Case pointed by Roberto Ierusalimschy 2018-12-14 in Lua Mail
     List.
+
+  [2] Heading newline dropped from long string quote. So we need
+    duplicate it to preserve second copy. Before it all variants
+    of newlines are converted to 0x0A.
+
+    Tricky case here is if we have 0x0D we cant add 0x0A or it still
+    be interpreted as one newline.
+
+    Nice workaround is duplicate first byte of newline:
+
+      0D    | 0D 0D    | \
+      0A    | 0A 0A    |   still two line
+      0D 0A | 0D 0D 0A |   delimiters!
+      0A 0D | 0A 0A 0D | /
+
+    Case and solution pointed by Andrew Gierth 2018-12-15 in Lua
+    Mail List.
 ]===]
