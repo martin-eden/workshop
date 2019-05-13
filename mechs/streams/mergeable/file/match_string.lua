95218e55c178b606ed0caf60719d8d9f1dbb7ddd
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2019-05-13 18:06:14 +0300

    [*] [mechs.streams.file.match_string] Now keeps position on fail.

--- mechs/streams/mergeable/file/match_string.lua
+++ mechs/streams/mergeable/file/match_string.lua
@@ -1,6 +1,12 @@
 return
   function(self, str)
-    local segment = self:block_read(#str)
+    local orig_pos = self:get_position()
+    local segment = self:read(#str)
     -- print(('match [%d, "%s"]'):format(self:get_position(), str), segment == str)
-    return (segment == str)
+    if (segment ~= str) then
+      self:set_position(orig_pos)
+      return false
+    else
+      return true
+    end
   end
