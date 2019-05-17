6210dc477df5506b986e9d21ee00662d0a38d827
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2019-05-17 04:15:53 +0300

    [^~] [mechs.streams.file.get_length] seek() use [assert_no_error] now.
    Need to introduce somethinl like [!.file.safe_seek] to avoid this
    ugly wrappings.

--- mechs/streams/mergeable/file/get_length.lua
+++ mechs/streams/mergeable/file/get_length.lua
@@ -1,7 +1,7 @@
 return
   function(self)
     local orig_pos = self:get_position()
-    local result = self.f:seek('end')
+    local result = self.assert_no_error(self.f:seek('end'))
     self:set_position(orig_pos)
     return result
   end
