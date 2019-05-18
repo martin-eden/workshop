079bbe1a18d2d9a87553884baf1d50d08b756f65
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2019-05-18 18:46:59 +0300

    [.^] [mechs.streams.file.write] assert_no_error() usage.

--- mechs/streams/mergeable/file/write.lua
+++ mechs/streams/mergeable/file/write.lua
@@ -1,6 +1,5 @@
 return
   function(self, s)
     -- print(('raw_write [@%d, %d chars]'):format(self:get_position(), #s))
-    local is_ok, err_msg = self.f:write(s)
-    assert(is_ok, err_msg)
+    self.assert_no_error(self.f:write(s))
   end
