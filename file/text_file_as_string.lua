35a30b6f247dfdd177b00effb1dc3b4ed1e1331f
Ian <ian@somewhere.com>
2019-05-31 06:16:19 +0300

    [.^] [file.text_file_as_string]

--- file/text_file_as_string.lua
+++ file/text_file_as_string.lua
@@ -1,7 +1,8 @@
 --[[
   Fucken editors tend to save files in UTF-8 or UTF-16.
-  In this case staring bytes are (EF BB BF), (FF FE) or (FE FF).
-  They wrecks any further data parsing.
+  In this case staring bytes are (EF BB BF) for UTF-8,
+  (FF FE) or (FE FF) for UTF-16 little- and bin-endian.
+  These bytes wrecks any further data parsing.
 
   This routine just strips UTF-8 sequence if found at start of file.
   UTF-16 sequences is not stripped as characters will be two-byte
