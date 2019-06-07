e7e60d23548268c8ff76b2ee072cae280499eb44
Ian <ian@somewhere.com>
2019-06-07 21:05:28 +0300

    [+~] [formats.firmata.compiler.set_pin_value_long]

---
+++ formats/firmata/compiler/request/set_pin_value_long.lua
@@ -0,0 +1,23 @@
+local signatures = request('!.formats.firmata.protocol.signatures')
+local assert_valid_pin_number =
+  request('!.formats.arduino_uno.assert_valid_pin_number')
+local slice_bits = request('!.number.slice_bits')
+local get_msb = request('!.number.get_msb')
+
+return
+  function(self, pin_number, value)
+    assert_valid_pin_number(pin_number)
+
+    local msb = get_msb(value)
+    self:emit(
+      signatures.sysex_start,
+      signatures.set_pin_value_long,
+      pin_number
+    )
+    local highest_bit = 0
+    repeat
+      self:emit(slice_bits(value, highest_bit, highest_bit + 6))
+      highest_bit = highest_bit + 7
+    until (highest_bit >= msb)
+    self:emit(signatures.sysex_end)
+  end
