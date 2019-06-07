0fe6e24243fe57248b8e634d6c647acb552e899a
Ian <ian@somewhere.com>
2019-06-07 22:26:33 +0300

    [+~] [formats.firmata.compiler.get_pin_state]

---
+++ formats/firmata/compiler/request/get_pin_state.lua
@@ -0,0 +1,11 @@
+local signatures = request('!.formats.firmata.protocol.signatures')
+
+return
+  function(self, pin_number)
+    self:emit(
+      signatures.sysex_start,
+      signatures.query_pin_state,
+      pin_number,
+      signatures.sysex_end
+    )
+  end
