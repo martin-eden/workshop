fbaecc70a71b97993f03350ad6ce72deddec4d19
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2019-07-13 02:29:35 +0300

    [~] [mechs.file_lister.get_files_list]

--- mechs/file_lister/get_files_list.lua
+++ mechs/file_lister/get_files_list.lua
@@ -2,11 +2,12 @@
   Return list of file names in <.start_dir>.
 ]]
 
+local get_cmd_listfiles = request('!.bare.file_system.get_cmd_listfiles')
 local get_program_output_lines = request('!.system.get_program_output_lines')
 
 return
   function(self)
-    local cmd_get_files = ('find %s -maxdepth 1 -type f'):format(self.start_dir)
+    local cmd_get_files = get_cmd_listfiles(self.start_dir)
     local result = get_program_output_lines(cmd_get_files)
     self:simplify_file_names(result)
     return result
