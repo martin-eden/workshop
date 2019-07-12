cf808ff740b6b247e894517d8bfdc75dec3f5110
Ian <ian@somewhere.com>
2019-07-13 02:29:12 +0300

    [~] [mechs.file_lister.get_directories_list]

--- mechs/file_lister/get_directories_list.lua
+++ mechs/file_lister/get_directories_list.lua
@@ -2,11 +2,12 @@
   Return list of directory names in <.start_dir>.
 ]]
 
+local get_cmd_listdirs = request('!.bare.file_system.get_cmd_listdirs')
 local get_program_output_lines = request('!.system.get_program_output_lines')
 
 return
   function(self)
-    local cmd_get_dirs = ('find %s -maxdepth 1 -type d'):format(self.start_dir)
+    local cmd_get_dirs = get_cmd_listdirs(self.start_dir)
     local result = get_program_output_lines(cmd_get_dirs)
     self:simplify_file_names(result)
     self:remove_start_dir(result)
