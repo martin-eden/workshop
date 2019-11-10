955556c1cb159ce30f242a418722adb7cb99858a
Ian <ian@somewhere.com>
2019-11-10 11:38:47 +0400

    [^+] [table.merge] Allow passing nil argument.

--- table/merge.lua
+++ table/merge.lua
@@ -1,9 +1,14 @@
 return
   function(t_dest, t_src)
-    assert_table(t_src)
     assert_table(t_dest)
+    if (t_src == nil) then
+      return t_dest
+    end
+
+    assert_table(t_src)
     for k, v in pairs(t_src) do
       t_dest[k] = v
     end
+
     return t_dest
   end
