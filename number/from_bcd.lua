84e4bef8b8b29cf0dd6c6d49001393383dd36972
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2019-11-18 21:44:55 +0400

    [^] [number.from_bcd] Added validity assertions.

--- number/from_bcd.lua
+++ number/from_bcd.lua
@@ -2,5 +2,9 @@ local slice_bits = request('slice_bits')
 
 return
   function(v)
-    return slice_bits(v, 4, 7) * 10 + slice_bits(v, 0, 3)
+    local high_digit = slice_bits(v, 4, 7)
+    local low_digit = slice_bits(v, 0, 3)
+    assert(high_digit <= 9)
+    assert(low_digit <= 9)
+    return high_digit * 10 + low_digit
   end
