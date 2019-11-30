e79bbd3dfe33f572c0b55591041abde990c8517a
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2019-11-30 20:38:08 +0400

    [/] [formats.ds3231] Renamed some fields of codec to that I like more.

--- formats/ds3231/compile.lua
+++ formats/ds3231/compile.lua
@@ -70,13 +70,13 @@ return
         [7] = splice_bits(0, 0, 6, to_bcd(rec.alarm[1].second)),
         [8] = splice_bits(0, 0, 6, to_bcd(rec.alarm[1].minute)),
         [9] = compile_hour(rec.alarm[1]),
-        [10] = splice_bits(0, 0, 5, to_bcd(rec.alarm[1].date_day)),
+        [10] = splice_bits(0, 0, 5, to_bcd(rec.alarm[1].date_dow)),
         [11] = splice_bits(0, 0, 6, to_bcd(rec.alarm[2].minute)),
         [12] = compile_hour(rec.alarm[2]),
-        [13] = splice_bits(0, 0, 5, to_bcd(rec.alarm[2].date_day)),
+        [13] = splice_bits(0, 0, 5, to_bcd(rec.alarm[2].date_dow)),
         [14] = 0,
         [15] = 0,
-        [16] = int8_to_uint8(rec.aging),
+        [16] = int8_to_uint8(rec.clock_speed),
       }
 
     result[5] = set_bit(result[5], 7, rec.moment.is_next_century)
@@ -84,14 +84,14 @@ return
     result[8] = set_bit(result[8], 7, rec.alarm[1].ignore_minute)
     result[9] = set_bit(result[9], 7, rec.alarm[1].ignore_hour)
 
-    result[10] = set_bit(result[10], 6, rec.alarm[1].is_date_not_day)
-    result[10] = set_bit(result[10], 7, rec.alarm[1].ignore_date_day)
+    result[10] = set_bit(result[10], 6, rec.alarm[1].is_date_not_dow)
+    result[10] = set_bit(result[10], 7, rec.alarm[1].ignore_date_dow)
 
     result[11] = set_bit(result[11], 7, rec.alarm[2].ignore_minute)
     result[12] = set_bit(result[12], 7, rec.alarm[2].ignore_hour)
 
-    result[13] = set_bit(result[13], 6, rec.alarm[2].is_date_not_day)
-    result[13] = set_bit(result[13], 7, rec.alarm[2].ignore_date_day)
+    result[13] = set_bit(result[13], 6, rec.alarm[2].is_date_not_dow)
+    result[13] = set_bit(result[13], 7, rec.alarm[2].ignore_date_dow)
 
     result[14] = set_bit(result[14], 0, rec.alarm[1].enabled)
     result[14] = set_bit(result[14], 1, rec.alarm[2].enabled)
@@ -106,8 +106,8 @@ return
     result[14] = set_bit(result[14], 6, rec.at_battery.custom_wave_output_allowed)
     result[14] = set_bit(result[14], 7, rec.at_battery.clock_disabled)
 
-    result[15] = set_bit(result[15], 0, rec.alarm[1].triggered)
-    result[15] = set_bit(result[15], 1, rec.alarm[2].triggered)
+    result[15] = set_bit(result[15], 0, rec.alarm[1].occurred)
+    result[15] = set_bit(result[15], 1, rec.alarm[2].occurred)
     result[15] = set_bit(result[15], 2, rec.is_busy)
     result[15] = set_bit(result[15], 3, rec.fixed_wave_32k_enabled)
     result[15] = set_bit(result[15], 7, rec.clock_was_stopped)
