de89c07255af6412f97ded27bb5bef87a55f648f
Ian <ian@somewhere.com>
2019-12-06 23:36:04 +0400

    [.^] [number.from_bcd] Added error message at failed assertion.

--- number/from_bcd.lua
+++ number/from_bcd.lua
@@ -2,9 +2,13 @@ local slice_bits = request('slice_bits')
 
 return
   function(v)
+    assert(v >= 0)
     local high_digit = slice_bits(v, 4, 7)
     local low_digit = slice_bits(v, 0, 3)
-    assert(high_digit <= 9)
-    assert(low_digit <= 9)
+    if not ((high_digit <= 9) and (low_digit <= 9)) then
+      local err_msg =
+        ("Number 0x%02X can't be interpreted as BCD."):format(v)
+      error(err_msg, 2)
+    end
     return high_digit * 10 + low_digit
   end
