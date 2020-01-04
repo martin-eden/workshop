01882eef5c77f6dd0bd99833f9e3c390e21db7b0
Ian <ian@somewhere.com>
2020-01-04 11:27:42 +0200

    [/] [system.install_is_functions] Now returns function.
    .. not does things directly. Cause I consider it bad behavior when
    "require('some_name')" yields to code execution.

--- system/install_is_functions.lua
+++ system/install_is_functions.lua
@@ -1,17 +1,25 @@
 --[[
-  Create "is_<type>" family of global functions.
+  Function to spawn "is_<type>" family of global functions.
 ]]
 
 local data_types = request('!.lua.data_types')
 
-for _, type_name in ipairs(data_types) do
-  _G['is_' .. type_name] =
-    function(a)
-      return (type(a) == type_name)
-    end
-end
+local generic_is =
+  function(type_name)
+    return
+      function(val)
+        return (type(val) == type_name)
+      end
+  end
 
-_G.is_integer =
-  function(n)
-    return (math.type(n) == 'integer')
+return
+  function()
+    for _, type_name in ipairs(data_types) do
+      _G['is_' .. type_name] = generic_is(type_name)
+    end
+
+    _G.is_integer =
+      function(n)
+        return (math.type(n) == 'integer')
+      end
   end
