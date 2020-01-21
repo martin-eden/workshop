2f4f033de10c0193e6a26564b5e4f07b0c44a2f2
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2020-01-21 19:50:48 +0300

    [+] [doc][mechs.tty.set_non_blocking_read]

--- mechs/tty/set_non_blocking_read.lua
+++ mechs/tty/set_non_blocking_read.lua
@@ -1,3 +1,11 @@
+--[[
+  Set baud and non-blocking read mode for TTY device given by name.
+
+  Non-blocking read means that if we do read(10) (read ten bytes) and
+  in receive buffer is only five bytes, we read up to <max_wait_time>
+  (in seconds) for more and then return bytes we have.
+]]
+
 return
   function(tty_name, max_wait_time, baud)
     assert_string(tty_name)
@@ -9,7 +17,8 @@ return
     baud = baud or 57600
 
     local cmd =
-      ('stty --file=%s %d raw time %d min 0 cs8 -echo -echoctl -echoe -echok -echoke -icanon -icrnl -iexten -isig -ixon -opost'):
+      ('stty --file=%s %d raw time %d min 0 cs8 -echo -echoctl' ..
+       ' -echoe -echok -echoke -icanon -icrnl -iexten -isig -ixon -opost'):
       -- ('stty --file=%s %d -echoctl raw time %d min 0 cs8'):
       format(tty_name, baud, max_wait_time)
     -- print(cmd)
