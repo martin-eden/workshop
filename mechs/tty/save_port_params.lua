607aba728e4934531145b461d91e1e88c4758fb9
Ian <ian@somewhere.com>
2020-01-21 19:50:02 +0300

    [+] [doc][mechs.tty.save_port_params]

--- mechs/tty/save_port_params.lua
+++ mechs/tty/save_port_params.lua
@@ -1,8 +1,12 @@
+--[[
+  Save TTY device parameters as special-format string for later
+  restoration.
+]]
+
 return
   function(tty_name)
     assert_string(tty_name)
-    local cmd =
-      ('stty --file=%s --save'):format(tty_name)
+    local cmd = ('stty --file=%s --save'):format(tty_name)
     local f_output = assert(io.popen(cmd))
     local result = f_output:read('l')
     f_output:close()
