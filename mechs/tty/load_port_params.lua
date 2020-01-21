199c1930a77aadec1953962e960a013561b001de
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2020-01-21 19:49:39 +0300

    [+] [doc][mechs.tty.load_port_params]

--- mechs/tty/load_port_params.lua
+++ mechs/tty/load_port_params.lua
@@ -1,9 +1,15 @@
+--[[
+  Set TTY device parameters from stty-readable string.
+
+  Stty-readable string usually produced by "$ stty --save"
+  or from save_port_params().
+]]
+
 return
   function(tty_name, params)
     assert_string(tty_name)
     assert_string(params)
-    local cmd =
-      ('stty --file=%s %s'):format(tty_name, params)
+    local cmd = ('stty --file=%s %s'):format(tty_name, params)
     -- print(cmd)
     assert(os.execute(cmd))
   end
