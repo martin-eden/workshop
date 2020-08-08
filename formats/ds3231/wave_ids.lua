a244addac7f8e34d32207eee3c2dc995c32928fa
Ian <ian@somewhere.com>
2020-08-08 19:46:13 +0200

    [^+] [formats.ds3231.wave_(ids, freqs)] Description comment.

--- formats/ds3231/wave_ids.lua
+++ formats/ds3231/wave_ids.lua
@@ -1,5 +1,8 @@
-local invert = request('!.table.invert')
+--[[
+  Mapping of frequency (integer in Hz) to frequency id.
+]]
 
+local invert = request('!.table.invert')
 local wave_freqs = request('wave_freqs')
 
 return invert(wave_freqs)
