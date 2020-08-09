6b96e596d32be23a761d7d0f718999e47a0ed579
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2020-08-09 02:48:18 +0200

    [~] [number.assert_byte] Now just a wrapper for is_byte().

--- number/assert_byte.lua
+++ number/assert_byte.lua
@@ -2,13 +2,9 @@
   Assert that passed value is integer in byte range.
 ]]
 
+local is_byte = request('is_byte')
+
 return
   function(v)
-    assert_integer(v)
-    if (v ~= v & 0xFF) then
-      local msg =
-        ('Given number 0x%X is not in byte range (0x00/0xFF)'):
-        format(v)
-      error(msg, 2)
-    end
+    assert(is_byte(v))
   end
