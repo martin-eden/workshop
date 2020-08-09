49622ebf028cc66a00f10f152f2ff9b76aa0a87e
Ian <ian@somewhere.com>
2020-08-09 03:07:19 +0200

    [~] [install_assert_functions]

--- system/install_assert_functions.lua
+++ system/install_assert_functions.lua
@@ -5,16 +5,13 @@
 local data_types = request('!.lua.data_types')
 
 local generic_assert =
-  function(type_name)
+  function(is_checker)
     return
       function(val, responsibility_level)
         local responsibility_level = (responsibility_level or 1)
-        if (type(val) ~= type_name) then
-          error(
-            ('Argument must have a type "%s", not "%s".'):
-            format(type_name, type(val)),
-            responsibility_level + 1
-          )
+        local result, err_msg = is_checker(val)
+        if not result then
+          error(err_msg, responsibility_level + 1)
         end
       end
   end
@@ -22,17 +19,7 @@ local generic_assert =
 return
   function()
     for _, type_name in ipairs(data_types) do
-      _G['assert_' .. type_name] = generic_assert(type_name)
+      _G['assert_' .. type_name] = generic_assert(_G['is_' .. type_name])
     end
-
-    _G.assert_integer =
-      function(a, responsibility_level)
-        local responsibility_level = (responsibility_level or 1)
-        if (math.type(a) ~= 'integer') then
-          error(
-            ('Argument must be integer, not %s.'):format(type(a)),
-            responsibility_level + 1
-          )
-        end
-      end
+    _G.assert_integer = generic_assert(_G.is_integer)
   end
