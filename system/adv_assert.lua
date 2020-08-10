bc37528a4fe4ca7e336a817efe49c77d06330458
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2020-08-10 04:17:30 +0200

    [+] [system.adv_assert]

---
+++ system/adv_assert.lua
@@ -0,0 +1,30 @@
+--[[
+  assert() extended to following result format:
+
+    <bool> result
+    <str> error_message
+    <table> error_details
+
+  In standard Lua third paramater in case of error is integer or
+  absent.
+]]
+
+local t2s = request('!.formats.lua_table.save')
+
+return
+  function(...)
+    local result = select(1, ...)
+    local msg = select(2, ...)
+    local report = select(3, ...)
+    if not result then
+      if is_string(msg) then
+        local report_msg = msg
+        if is_table(report) then
+          report_msg = report_msg .. '\n' .. t2s(report)
+        end
+        error(report_msg)
+      else
+        error('')
+      end
+    end
+  end
