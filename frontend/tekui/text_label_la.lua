8c6bab39e0ed7e746696596664ae154603df3c14
Ian <ian@somewhere.com>
2020-08-26 20:22:34 +0200

    [+] [frontend.tekui.text_label_la] Left-aligned label.

---
+++ frontend/tekui/text_label_la.lua
@@ -0,0 +1,13 @@
+local text_label = request('text_label')
+local merge = request('!.table.merge')
+
+return
+  function(text, overrides)
+    overrides =
+      merge(
+        {Style = 'text-align: left'},
+        overrides
+      )
+
+    return text_label(text, overrides)
+  end
