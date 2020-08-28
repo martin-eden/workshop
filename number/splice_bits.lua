90bba6fb594eb7c7a154f10d07dde97bf02fc6df
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2020-08-28 03:27:27 +0200

    [^] [number.splice_bits] Assertion that given number fits in bit range.

--- number/splice_bits.lua
+++ number/splice_bits.lua
@@ -12,11 +12,16 @@ return
     assert_bit_offs(end_offs)
     assert(start_offs <= end_offs)
     assert_integer(v)
+    assert((v << start_offs) < (1 << (end_offs + 1)))
 
     local mask
+    -- start_offs: 2, end_offs: 5
     mask = (1 << (end_offs + 1)) - 1
+    -- mask: 111111
     mask = mask & ~((1 << start_offs) - 1)
+    -- mask: 111100
     mask = ~mask
+    -- mask: 000011
 
     return (n & mask) | (v << start_offs)
   end
