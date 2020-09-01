23f296b06c8c6dad63f9d5a288795c41ce92a18a
Ian <ian@somewhere.com>
2020-09-01 21:17:34 +0200

    [/] [concepts.daytime.to_ampm_hour]: Loosed function contract.

--- concepts/daytime/from_ampm_hour.lua
+++ concepts/daytime/from_ampm_hour.lua
@@ -1,10 +1,18 @@
+--[[
+  Calculate 24h-format hour from 12h AM/PM format.
+
+  There is no zero hour. 00h -> 12 a.m., 12h -> 12p.m.
+
+  This function does not check that AM/PM hour in valid range [1, 12].
+  Reason is that in my use cases it may be not. And result
+  verification is job of other functions.
+]]
+
 return
   function(hour, is_pm)
     assert_integer(hour)
-    assert((hour >= 1) and (hour <= 12))
     assert_boolean(is_pm)
 
-    -- There is no zero hour. 00h -> 12 a.m., 12h -> 12p.m.
     hour = hour % 12
     if is_pm then
       hour = hour + 12
