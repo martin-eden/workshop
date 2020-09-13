ee46911c1e22396698da9aa9b67d0b6b83338d43
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2020-09-13 14:14:05 +0200

    [.^] [number.to_bcd]

--- number/to_bcd.lua
+++ number/to_bcd.lua
@@ -1,7 +1,7 @@
 return
   function(v)
     assert_integer(v)
-    assert(v >= 0)
+    assert(v >= 0, "Can't represent negative number in BCD.")
     local high_digit = v // 10
     local low_digit = v % 10
     if not ((high_digit <= 9) and (low_digit <= 9)) then
