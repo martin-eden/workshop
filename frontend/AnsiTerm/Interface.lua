4571dc101817d8daca3f75164d951054c43724b1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2022-01-30 19:11:04 -0500

    [.] [frontend.AnsiTerm]

--- frontend/AnsiTerm/Interface.lua
+++ frontend/AnsiTerm/Interface.lua
@@ -2,7 +2,7 @@ local CSI = '\027['
 
 return
   {
-    ClrEol = CSI .. 'K',
+    ClrEOL = CSI .. 'K',
     ScreenClear = CSI .. '2J',
     ScreenClearToStart = CSI .. '1J',
     ScreenClearToEnd = CSI .. 'J',
@@ -10,28 +10,22 @@ return
     CursorShow = CSI .. '?25h',
     CursorGotoXY =
       function(x, y)
-        assert_integer(x)
-        assert_integer(y)
         return (CSI .. '%d;%dH'):format(y, x)
       end,
     GetScreenSize =
       function()
-        io.write('\027[18t')
-        local result = ''
+        io.write(CSI .. '18t')
+        local Response = ''
         while true do
           local c = io.read(1)
-          result = result .. c
-          if c == 't' then
+          Response = Response .. c
+          if (c == 't') then
             break
           end
         end
-        local width, height
-        if is_string(result) then
-          height, width = result:match('\027%[%d+;(%d+);(%d+)t')
-          height = tonumber(height)
-          width = tonumber(width)
-          -- print('!', 'screen size', ('(%d x %d)'):format(width, height))
-        end
-        return width, height
+        local Height, Width = Response:match('\027%[%d+;(%d+);(%d+)t')
+        Height = tonumber(Height)
+        Width = tonumber(Width)
+        return Width, Height
       end,
   }
