2070f61f10671b58564e41ec39ad964d480eb543
Ian <ian@somewhere.com>
2022-02-20 15:15:12 -0500

    [^] [FromIsoDateTime] Added diagnostic error.

--- concepts/IsoTime/FromIsoDateTime.lua
+++ concepts/IsoTime/FromIsoDateTime.lua
@@ -7,6 +7,12 @@ return
   function(DateTimeStr)
     assert_string(DateTimeStr)
     local DateStr, TimeStr = DateTimeStr:match(DateTimeFmt)
+    if not DateStr then
+      error(
+        ('Unable to select ISO 9601 date-time parts from string %q.'):
+        format(DateTimeStr)
+      )
+    end
     local Year, Month, Day = FromIsoDate(DateStr)
     local Hour, Minute, Second = FromIsoTime(TimeStr)
     return Year, Month, Day, Hour, Minute, Second
