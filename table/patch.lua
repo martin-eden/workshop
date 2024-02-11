7719957ce06aa4ba6267838fb518b20be6976d0e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-02-11 18:08:00 +0000

    [/] [table.patch] Rewrote implementation..
    .. and probably fixed logical bug.

--- table/patch.lua
+++ table/patch.lua
@@ -1,18 +1,59 @@
-local patch
-patch =
-  function(t_src, t_patch)
-    for k, v in pairs(t_patch) do
-      if (t_src[k] == nil) then
-        local err_msg =
-          ('Destination table dont have key "%s".'):format(tostring(k))
-        error(err_msg, 2)
-      end
-      if is_table(t_src[k]) and is_table(v) then
-        patch(t_src[k], v)
-      else
-        t_src[k] = v
+-- Apply patch to table
+
+--[[
+  Status: should work
+  Version: 2
+  Last mod.: 2024-02-11
+]]
+
+--[[
+  Example
+
+    local MainConfig =
+      {
+        Name = 'PathName',
+        PathName = { Path = 'Data/Results/', Name = 'Default.json'},
+      }
+
+    local PatchConfig =
+      {
+        PathName = { Name = 'Custom.json' },
+      }
+
+    DoPatch(MainConfig, PatchConfig)
+]]
+
+local DoPatch
+DoPatch =
+  function(MainTable, PatchTable)
+    assert_table(MainTable)
+    assert_table(PatchTable)
+
+    for PatchKey, PatchValue in pairs(PatchTable) do
+      -- We work only with string and integer keys in patch table
+      if (is_string(PatchKey) or is_integer(PatchKey)) then
+        local MainValue = MainTable[PatchKey]
+
+        -- Raise error if main table doesn't have patch key
+        if is_nil(MainValue) then
+          local ErrorMsg =
+            ([[Destination table doesn't have key "%s".]]):
+            format(
+              tostring(PatchKey)
+            )
+
+          error(ErrorMsg, 2)
+        end
+
+        -- Recursive call when we writing table to table
+        if is_table(MainValue) and is_table(PatchValue) then
+          DoPatch(MainValue, PatchValue)
+        -- Else just overwrite value
+        else
+          MainTable[PatchKey] = PatchValue
+        end
       end
     end
   end
 
-return patch
+return DoPatch
