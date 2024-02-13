98187ede5f58459126f517fe6eae5d0e73d9abda
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-02-13 19:35:54 +0000

    [+] [mechs.cmdline.get_cmd_rmfile]

---
+++ mechs/cmdline/get_cmd_rmfile.lua
@@ -0,0 +1,15 @@
+-- Get GNU/Bash command to remove file by pathname
+
+--[[
+  Status: passed maiden flight
+  Last mod.: 2024-02-13
+]]
+
+local CmdFormatStr = 'rm %q'
+
+return
+  function(PathName)
+    assert_string(PathName)
+    local Result = string.format(CmdFormatStr, PathName)
+    return result
+  end
