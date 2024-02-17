8a44403feb1fc85047f9982b9a90aea2624c6bef
Ian <ian@somewhere.com>
2024-02-17 00:31:38 +0000

    [./] [mechs.cmdline.get_cmd_rmdir] Removed "force" flag, no need for me.

--- mechs/cmdline/get_cmd_rmdir.lua
+++ mechs/cmdline/get_cmd_rmdir.lua
@@ -1,10 +1,8 @@
---[[
-  Return shell command to delete directory.
+-- Return shell command to delete directory. GNU/bash assumed.
 
-  GNU/bash assumed.
-]]
+-- Last mod.: 2024-02-17
 
 return
   function(dir_name)
-    return ('rm -rf %s'):format(dir_name)
+    return ('rm -r %s'):format(dir_name)
   end
