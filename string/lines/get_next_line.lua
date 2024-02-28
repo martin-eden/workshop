f7876e3e23f66436fe3dc6ab7981290c443882a8
Ian <ian@somewhere.com>
2024-02-28 05:50:04 +0000

    [?/] [string.lines.get_next_line] Reworked code..
    .. but not tested it.
    
    These days I don't like optional parameters and Lua's iterators design.
    
    It's smart but not obvious enough:
    
    When get_next_line('123\n456', 1) returns (5, '123') it's weird.
    I would expect it to return ('123', 5), not to change parameters order.
    
    I don't remember exactly why I implemented it so six years ago
    but guess it's due "generic for" iterators specification in Lua.

--- string/lines/get_next_line.lua
+++ string/lines/get_next_line.lua
@@ -1,38 +1,94 @@
---[[
-  Get line from given string and position. Return it with next line
-  position.
+-- Get line from given string and start position
 
+--[[
   Input
 
-    <str>
-          <int>
+    <s>: string
+      Given string.
+
+    | <start_pos>: int = 1
+      Position where to start extraction of next line.
 
   Output
 
-    <int> <str>
-          <nil>
+    Return argument for next iteration and result or nil:
 
-  Notes
+    If <start_pos> is beyond <s>, return
 
-    * next() function format used.
-    * Special cases handled:
-      * Last line may not have tail newline. It handled as line
-        anyway.
-      * Source line may be empty. (1, <nil>) is returned.
+      <nil>
+
+    Else return
+
+      <int>
+        Position where next line starts. Expected to become <start_pos>
+        value for next call.
+
+      <str>
+        Extracted line (without newline character).
+
+  Special cases
+
+    1. No tail newline:
+
+      If there is no tail newline
+
+        Handle that part as if newline was present
+
+    2. Empty string
+
+      If <s> is empty
+
+        Return ('', 1)
 ]]
+
+-- Last mod.: 2024-02-28
 
 return
   function(s, start_pos)
-    start_pos = start_pos or 1
-    local start, finish, result = s:find('(.-)\n', start_pos)
-    local new_start
-    if not start then
-      if (start_pos <= #s) then
-        result = s:sub(start_pos)
-        new_start = #s + 1
+    -- If <start_pos> is nil we consider it as a first iteration.
+    if (is_nil(start_pos)) then
+      -- For empty line return empty line. Make sure it's the last iteration.
+      if (s == '') then
+        return 1, ''
+      else
+        start_pos = 1
       end
-    else
-      new_start = finish + 1
     end
-    return new_start, result
+
+    assert_string(s)
+
+    assert_integer(start_pos)
+
+    -- Throw error for negative indexes:
+    if (start_pos <= 0) then
+      error('get_next_line(): start index must be natural number')
+    end
+
+    -- Termination condition:
+    if (start_pos > #s) then
+      return nil
+    end
+
+    -- Here <start_pos> is index inside string <s>.
+
+    local pattern = '(.-)\n'
+    local entry_start, entry_finish, capture = string.find(s, pattern, start_pos)
+
+    if is_nil(capture) then
+      --[[
+        String doesn't ends with newline.
+
+        Treat remained string suffix as line.
+      ]]
+      entry_start = start_pos
+      entry_finish = #s
+      capture = string.sub(s, entry_start, entry_finish)
+    end
+
+    return entry_finish + 1, capture
   end
+
+--[[
+  2018-02-06
+  2024-02-28
+]]
