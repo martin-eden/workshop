d04fe0a1a71ceeb7979ceb30308f4daf40df6ac0
Ian <ian@somewhere.com>
2024-03-01 01:45:27 +0000

    [!/] [string.from_lines] There is always tail newline at result..
    .. I dropped special case handling: no newline when one element, else
    newline.
    
    External code already had troubles doing "print(from_lines(s))" (print
    emits extra newline). He switched to "io.write". And get diverged output
    for one and more-than-one line cases.
    
    So no special cases. This is block function, blocks are for raw writing.

--- string/from_lines.lua
+++ string/from_lines.lua
@@ -7,22 +7,11 @@
 
   Output
 
-    string - concatenated string from sequence, separated by newlines
-
-  Special case
-
-    When sequence is just one element
-      Result string has no tail newline
-    When sequence is more than one element
-      Result string has tail newline
-
-    Illustration
-
-      from_lines({'One-liner'}) -> "One-liner"
-      from_lines({'Two-', 'liner'}) -> "Two-"" \n "liner" \n
+    string - concatenated string from sequence, separated by newlines.
+      NB: There is always tail newline at result string.
 ]]
 
--- Last mod.: 2024-02-28
+-- Last mod.: 2024-02-29
 
 --[[
   Good design is not about avoiding implementing simple things.
@@ -39,10 +28,7 @@ return
 
     Result = table.concat(Lines, '\n')
 
-    -- Special case.
-    if (#Lines > 1) then
-      Result = Result .. '\n'
-    end
+    Result = Result .. '\n'
 
     return Result
   end
