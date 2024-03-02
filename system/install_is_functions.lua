554ab19a40a57271c5e1855da513e89fd91a2ce4
Ian <ian@somewhere.com>
2024-03-02 22:45:12 +0000

    [!/] [system.install_is_functions] Reduced output format of is_ funcs..
    .. before they were returning two values: boolean result and
    string message when result is false. This change was made 2020-08-09
    and looks like I wanted to capture specific reason of error and
    saw no way to do it in outer code.
    
    Also that change enabled cool feature that "assert(is_integer(x))"
    will return meaningful message in case of fail.
    
    Now I reverting design back to functions that return just boolean.
    
    2024-03-02

--- system/install_is_functions.lua
+++ system/install_is_functions.lua
@@ -1,21 +1,36 @@
+-- Function to spawn "is_<type>" family of global functions.
+
 --[[
-  Function to spawn "is_<type>" family of global functions.
+  It spawns "is_nil", "is_boolean", ... for all Lua data types.
+  Also it spawns "is_integer" and "is_float" for number type.
 ]]
 
-local data_types = request('!.lua.data_types')
+--[[
+  Design
 
-local generic_is =
+    f(:any) -> bool
+
+    Original design was
+
+      f(:any) -> bool, (string or nil)
+
+      Use case was "assert(is_number(x))" which will automatically
+      provide error message when "x" is not a number.
+
+      Today I prefer less fancy designs. Caller has enough information
+      to build error message itself.
+]]
+
+-- Last mod.: 2024-03-02
+
+local data_types = request('!.lua.data_types')
+local data_mathtypes = request('!.lua.data_mathtypes')
+
+local type_is =
   function(type_name)
     return
       function(val)
-        local result, err_msg
-        result = (type(val) == type_name)
-        if not result then
-          err_msg =
-            ('Value "%s" has type "%s", not "%s".'):
-            format(val, type(val), type_name)
-        end
-        return result, err_msg
+        return (type(val) == type_name)
       end
   end
 
@@ -23,23 +38,30 @@ local number_is =
   function(type_name)
     return
       function(val)
-        local result, err_msg
-        result, err_msg = is_number(val)
-        if result then
-          result, err_msg = (math.type(val) == type_name)
-          if not result then
-            err_msg = ('Number "%s" type is "%s", not "%s".'):format(val, type(val), type_name)
-          end
+        --[[
+          math.type() throws error for non-number types.
+          This function returns "false" for non-number types.
+        ]]
+        if not is_number(val) then
+          return false
         end
-        return result, err_msg
+        return (math.type(val) == type_name)
       end
   end
 
 return
   function()
     for _, type_name in ipairs(data_types) do
-      _G['is_' .. type_name] = generic_is(type_name)
+      _G['is_' .. type_name] = type_is(type_name)
     end
-    _G.is_integer = number_is('integer')
-    _G.is_float = number_is('float')
+    for _, math_type_name in ipairs(data_mathtypes) do
+      _G['is_' .. math_type_name] = number_is(math_type_name)
+    end
   end
+
+--[[
+  2018-02
+  2020-01
+  2022-01
+  2024-03 Changed design
+]]
