15a0c2fee593ceeef558f28ad2d2b15ba0196a94
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-03-02 22:32:48 +0000

    [+] [lua.data_mathtypes] ..
    Similar approach as for [data_types]: return list of possible results.
    
    Just two values, "float" and "number". But I understands number of
    values does not matter from design standpoint.

---
+++ lua/data_mathtypes.lua
@@ -0,0 +1,26 @@
+-- Return table sequence with string values returned by math.type()
+
+--[[
+  Output
+
+    table
+
+      Sequence of strings with number type names. As they are returned
+      by math.type() function.
+
+  Note
+
+    This list is used in code generation.
+]]
+
+-- Last mod.: 2024-03-02
+
+return
+  {
+    'integer',
+    'float',
+  }
+
+--[[
+  2024-03-02
+]]
