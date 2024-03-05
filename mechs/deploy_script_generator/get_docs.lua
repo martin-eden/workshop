f740e3a90538b05d920b1bf5f68ff844706768c0
Ian <ian@somewhere.com>
2024-03-05 01:40:27 +0000

    [^+] [mechs.deploy_script_generator.get_docs] Added more possible names.
    .. specifically "readme's" "txt's" or "markdown's".
    
    Probably it's easier just to do "dir" and then filter results by
    regexp.
    
    But this six-years old code snippet, written because of practical need,
    works without problems and used in my projects. So let it live.

--- mechs/deploy_script_generator/get_docs.lua
+++ mechs/deploy_script_generator/get_docs.lua
@@ -19,10 +19,18 @@
 
   Details
 
-    Given pathname "d/e.lua" we check for existence following names:
+    Given pathname "dir/e.lua" we check for existence following names
+    in "dir/":
 
-      "d/wtf.txt"
-      "d/e.txt"
+      e.txt
+      wtf.txt
+      readme.txt
+      readme.md
+      readme.markdown
+      ReadMe.txt
+      ReadMe.md
+      ReadMe.markdown
+
 ]]
 
 -- Last mod.: 2024-03-05
@@ -31,16 +39,32 @@ local parse_pathname = request('!.concepts.path_name.parse')
 local exists = request('!.file_system.file.exists')
 local get_keys = request('!.table.get_keys')
 
+local hardcoded_doc_filenames =
+  {
+    'wtf.txt',
+    'readme.txt',
+    'readme.md',
+    'readme.markdown',
+    'ReadMe.txt',
+    'ReadMe.md',
+    'ReadMe.markdown',
+  }
+
 return
   function(self, files)
     local docs = {}
     local was_checked = {}
     for i = 1, #files do
-      local possible_docs =
-        {
-          files[i]:gsub('%.lua$', '.txt'),
-          parse_pathname(files[i]).directory .. 'wtf.txt',
-        }
+      local possible_docs = {}
+      do
+        local name_txt = files[i]:gsub('%.lua$', '.txt')
+        table.insert(possible_docs, name_txt)
+
+        local basedir = parse_pathname(files[i]).directory
+        for _, doc_filename in ipairs(hardcoded_doc_filenames) do
+          table.insert(possible_docs, basedir .. doc_filename)
+        end
+      end
       for _, file_name in ipairs(possible_docs) do
         if not was_checked[file_name] then
           if exists(file_name) then
