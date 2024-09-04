74c1dba2d73deded85c12ef1274bf523f76825c3
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-09-04 06:30:10 +0200

    [.] [concepts.lua_table.save.serialize_ast] Tail newline for result.

--- concepts/lua_table/save/serialize_ast.lua
+++ concepts/lua_table/save/serialize_ast.lua
@@ -3,5 +3,5 @@ local compile = request('!.struc.compile')
 return
   function(self, ast)
     compile(ast, self.node_handlers)
-    return self.text_block:get_text()
+    return self.text_block:get_text() .. '\n'
   end
