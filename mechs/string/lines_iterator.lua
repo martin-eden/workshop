f97f412bad670cbaa86091281e05ce64d649f3f6
Ian <ian@somewhere.com>
2024-09-18 20:35:21 +0200

    [.^] [mechs.string.lines_iterator] Better documentation.

--- mechs/string/lines_iterator.lua
+++ mechs/string/lines_iterator.lua
@@ -1,10 +1,18 @@
 --[[
   String lines iterator class.
 
-  Main intention - encapsulate parameters (string, index) to object.
+  Interface
+
+    init(self, s)
+
+      Set base string with lines to iterate.
+
+    get_next_line(self): line
+
+      Return next line. Return "nil" if we beyond last line.
 ]]
 
--- Last mod.: 2024-02-28
+-- Last mod.: 2024-09-18
 
 local get_next_line = request('!.string.get_next_line')
 
@@ -12,22 +20,36 @@ return
   {
     s = nil,
     idx_start = nil,
+
     init =
       function(self, s)
         assert_string(s)
         self.s = s
         self.idx_start = nil
       end,
+
     get_next_line =
       function(self)
         local result
+
         self.idx_start, result = get_next_line(self.s, self.idx_start)
+
         --[[
-          nil new index means that this is last line,
-          nil starting index means get first line.
+          "nil" in "nil, result = get_next_line(s, start)"
+          means that this is last line,
+          "nil" in "get_next_line(s, nil)" means get first line.
           Avoiding this collision.
         ]]
-        self.idx_start = self.idx_start or (#self.s + 1)
+        if is_nil(self.idx_start) then
+          self.idx_start = #self.s + 1
+        end
+
         return result
       end,
   }
+
+--[[
+  2018-02
+  2024-02
+  2024-09
+]]
