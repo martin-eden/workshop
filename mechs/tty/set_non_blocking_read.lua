f98c4028d652f23e9fdd13fb9247e9eb7ee950ef
Ian <ian@somewhere.com>
2024-09-18 18:24:36 +0200

    [.^] [mechs.tty] Better documentation and minor code layout change.

--- mechs/tty/set_non_blocking_read.lua
+++ mechs/tty/set_non_blocking_read.lua
@@ -4,6 +4,12 @@
   Non-blocking read means that if we do read(10) (reading ten bytes) and
   receive buffer has only five bytes, we wait up to <max_wait_time>
   seconds for more and then return bytes we have.
+
+  Input:
+
+    string - Port name
+    [float] - Max wait time (seconds). Default: 0.5
+    [int] - Port baud. Default: 57600
 ]]
 
 return
@@ -16,11 +22,40 @@ return
 
     baud = baud or 57600
 
-    local cmd =
-      ('stty --file=%s %d raw time %d min 0 cs8 -echo -echoctl' ..
-       ' -echoe -echok -echoke -icanon -icrnl -iexten -isig -ixon -opost'):
-      -- ('stty --file=%s %d -echoctl raw time %d min 0 cs8'):
-      format(tty_name, baud, max_wait_time)
+    --[[
+      Kinda long I know. I tried simpler
+
+        stty --file=%s %d -echoctl raw time %d min 0 cs8
+
+      but looks like it didn't work. Code was commented.
+    ]]
+    local cmd_fmt =
+      'stty' ..
+      ' --file=%s' ..
+      ' %d' ..
+      ' raw' ..
+      ' time %d' ..
+      ' min 0' ..
+      ' cs8' ..
+      ' -echo' ..
+      ' -echoctl' ..
+      ' -echoe' ..
+      ' -echok' ..
+      ' -echoke' ..
+      ' -icanon' ..
+      ' -icrnl' ..
+      ' -iexten' ..
+      ' -isig' ..
+      ' -ixon' ..
+      ' -opost'
+
+    local cmd = string.format(cmd_fmt, tty_name, baud, max_wait_time)
 
     assert(os.execute(cmd))
   end
+
+--[[
+  2020-01
+  2021-11
+  2024-09
+]]
