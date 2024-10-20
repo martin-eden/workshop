302f4d510e624b6b5f69b7edcf7f4f2336b6a97b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-20 06:33:07 +0200

    [.^] [table.map_values] Documentation comment.

--- table/map_values.lua
+++ table/map_values.lua
@@ -1,9 +1,28 @@
+-- Map table values to keys
+
+--[[
+  Useful when you want to check presence and have list.
+
+    { 'A', 'A', a = 'A', b = 'A'} ->
+    { [1] = true, [2] = true, A = true }
+]]
+
+-- Last mod.: 2024-10-20
+
 return
   function(t)
     assert_table(t)
-    local result = {}
+
+    local Result = {}
+
     for k, v in pairs(t) do
-      result[v] = true
+      Result[v] = true
     end
-    return result
+
+    return Result
   end
+
+--[[
+  2016-09-06
+  2024-10-20
+]]
