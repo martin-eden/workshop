7c5785be784c8e3fb4e5a210fe89286e86a5ee04
Ian <ian@somewhere.com>
2024-10-20 08:08:26 +0200

    [+] [concepts.List.ToString]

---
+++ concepts/List/ToString.lua
@@ -0,0 +1,24 @@
+-- Concatenate list of string values to string
+
+-- Last mod.: 2024-10-20
+
+--[[
+  Implementer may or may not check input arguments.
+
+  Usually implemented do check input parameters for convenience of
+  debugging. But for performance reasons it may not check them.
+
+  If something explodes we blame party that violated contract.
+  Contract is stated in comments. If comment says you should pass
+  string and you passed nil, don't blame implementer.
+]]
+
+return
+  function(List)
+    -- Meh, in Lua it's simple
+    return table.concat(List)
+  end
+
+--[[
+  2024-10-20
+]]
