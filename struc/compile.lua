5282ad6b1134cad6bdecdb506532ce3776b40415
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-21 14:25:55 +0200

    [^] [struc.compile] Basic documentation what it does.

--- struc/compile.lua
+++ struc/compile.lua
@@ -1,22 +1,51 @@
+-- Exotic table-to-string conversion
+
+--[[
+  Input
+
+    Node: string or table
+      if table then
+        <Node.type> should be present
+
+    NodeHandlers: table of functions
+      key: <Node.type>
+
+  Idea is to
+
+    compile(
+      { 'A', {type = 'Special', Value = 'X'}}, 'B' },
+      { Special = function(Node) return ' -= ' .. Node.Value .. ' =- '}
+    ) -> 'A -= X =- B'
+]]
+
+-- Last mod.: 2024-10-21
+
 local compile_core = request('compile_core')
-local unfold = request('!.table.unfold')
+local ListToString = request('!.concepts.List.ToString')
 
 return
-  function(t, node_handlers)
-    if is_string(t) then
-      return t
+  function(Node, NodeHandlers)
+    if is_string(Node) then
+      return Node
     else
-      assert_table(t)
+      assert_table(Node)
     end
 
-    node_handlers = node_handlers or {}
-    assert_table(node_handlers)
+    NodeHandlers = NodeHandlers or {}
+    assert_table(NodeHandlers)
 
-    local result = {}
+    local Result = {}
 
-    compile_core(t, node_handlers, result)
-    result = unfold(result)
-    result = table.concat(result)
+    compile_core(Node, NodeHandlers, Result)
+    Result = ListToString(Result)
 
-    return result
+    return Result
   end
+
+--[[
+  2017-02-13
+  2017-05-09
+  2017-08-27
+  2018-08-08
+  2024-10-21
+]]
