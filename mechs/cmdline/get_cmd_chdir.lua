d4a67672306ddadb27bf420a643a8b46c89d69ef
Ian <ian@somewhere.com>
2024-10-21 13:49:07 +0200

    [*] [mechs.cmdline] Quoting file names..
      return ('cp %s %s'):format(src_name, dest_name)
    
    Works fine until there are no spaces in file names.
    
    If there ARE spaces in file names, quotes needed.
    
    We can try
    
      return ('cp "%s" "%s"'):format(src_name, dest_name)
    
    but if file name has quote in it it should be escaped.
    Lua's "%q" comes to rescue!
    
      string.format('%q', 'te "st') -> '"te \"st"
    
    Exactly what we needing.
    
    Also I'm updating history comments in touched files.

--- mechs/cmdline/get_cmd_chdir.lua
+++ mechs/cmdline/get_cmd_chdir.lua
@@ -1,11 +1,17 @@
 -- Return shell command to change current directory. GNU/Bash.
 
--- Last mod.: 2024-02-19
+-- Last mod.: 2024-10-21
 
-local CommandFmt = 'cd "%s"'
+local CommandFmt = 'cd %q'
+
 return
   function(Path)
     assert_string(Path)
     local Command = string.format(CommandFmt, Path)
     return Command
   end
+
+--[[
+  2024-02-19
+  2024-10-21
+]]
--- mechs/cmdline/get_cmd_listfiles.lua
+++ mechs/cmdline/get_cmd_listfiles.lua
@@ -5,7 +5,14 @@
   GNU/Bash assumed.
 ]]
 
+-- Last mod.: 2024-10-21
+
 return
   function(dir_name)
-    return ('find %s -maxdepth 1 -type f'):format(dir_name)
+    return ('find %q -maxdepth 1 -type f'):format(dir_name)
   end
+
+--[[
+  2019-12-01
+  2024-10-21
+]]
