27e68b2d64e29b46cf5f149d78f451c0b3dca353
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-24 16:34:58 +0200

    [.^] [string.trim_trail] Documentation comment.

--- string/trim_tail.lua
+++ string/trim_tail.lua
@@ -1,6 +1,13 @@
+-- Remove spaces at end of string
+
+-- Last mod.: 2024-10-24
+
 return
   function(s)
+    assert_string(s)
+
     local result
+
     if (s:sub(-1, -1) == ' ') then
       local finish_pos = #s - 1
       while (s:sub(finish_pos, finish_pos) == ' ') do
@@ -10,5 +17,10 @@ return
     else
       result = s
     end
+
     return result
   end
+
+--[[
+  2017-01-20
+]]
