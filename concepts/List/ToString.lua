57ab2ba10dffed758ce33ab12e4497cc77d53359
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-10-24 11:31:46 +0200

    [+] [List.ToString:] Added <Separator> argument.

--- concepts/List/ToString.lua
+++ concepts/List/ToString.lua
@@ -2,23 +2,15 @@
 
 -- Last mod.: 2024-10-20
 
---[[
-  Implementer may or may not check input arguments.
-
-  Usually implementer do check input parameters for convenience of
-  debugging. But for performance reasons it may not check them.
-
-  If something explodes we blame party that violated contract.
-  Contract is stated in comments. If comment says you should pass
-  string and you passed table, take the blame.
-]]
-
 return
-  function(List)
+  function(List, Separator)
+    Separator = Separator or ''
+
     -- Meh, in Lua it's simple
-    return table.concat(List)
+    return table.concat(List, Separator)
   end
 
 --[[
   2024-10-20
+  2024-10-24
 ]]
