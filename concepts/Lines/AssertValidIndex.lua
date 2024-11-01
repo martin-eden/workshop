0d02553bda91add93870f75319898fce925069ae
Ian <ian@somewhere.com>
2024-11-01 03:52:03 +0200

    [*] [Lines.AssertEmptyIndex] Handling empty list special case.

--- concepts/Lines/AssertValidIndex.lua
+++ concepts/Lines/AssertValidIndex.lua
@@ -1,15 +1,24 @@
 -- Assert that value is valid index
 
--- Last mod.: 2024-10-31
+-- Last mod.: 2024-11-01
 
 -- Exports:
 return
   function(self, Index)
     assert_integer(Index)
-    assert(Index >= 0)
-    assert(Index <= self:GetNumLines())
+
+    local NumLines = self:GetNumLines()
+    local IsEmpty = (NumLines == 0)
+
+    if IsEmpty then
+      assert((Index == 0) or (Index == 1))
+    else
+      assert(Index >= 1)
+      assert(Index <= NumLines)
+    end
   end
 
 --[[
   2024-10-31
+  2024-11-01
 ]]
