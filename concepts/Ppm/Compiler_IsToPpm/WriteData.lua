61d1da9b907e1800e744a7d2f5775c6fa1c43052
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-11-06 14:14:13 +0200

    [*] [Ppm.Compiler_IsToPpm.WriteData] Last line pieces are not lost..
    Silly bug. When line length was not divisible by self.NumColumns,
    remainder was lost.

--- concepts/Ppm/Compiler_IsToPpm/WriteData.lua
+++ concepts/Ppm/Compiler_IsToPpm/WriteData.lua
@@ -1,6 +1,6 @@
 -- Write pixels to output. We're doing some formatting
 
--- Last mod.: 2024-11-03
+-- Last mod.: 2024-11-06
 
 -- Imports:
 local ListToString = request('!.concepts.List.ToString')
@@ -29,6 +29,12 @@ return
 
           self:WriteLine(ChunksStr)
         end
+      end
+
+      -- Write remained chunk
+      if (Header.Width % ChunkSize ~= 0) then
+        local ChunksStr = ListToString(Chunks, ColumnsDelim)
+        self:WriteLine(ChunksStr)
       end
 
       self:WriteLine(LinesDelim)
