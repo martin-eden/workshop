0ab8349d7c49c61dc34478ec0f04c5409d777fd0
Ian <ian@somewhere.com>
2024-11-11 11:23:01 +0200

    [~] [DS3231.fix_final_structure] Resync due refactored [verify_structure].

--- concepts/rtc_ds3231/validators/fix_final_structure.lua
+++ concepts/rtc_ds3231/validators/fix_final_structure.lua
@@ -1,14 +1,26 @@
 --[[
   Make given table formally comply to reference template.
 
-  Missing keys added. Mismatched value types set to match
+  Missing keys added. Mismatched value types are set to match
   corresponding types in reference table.
 ]]
 
-local verify_structure = request('!.table.verify_structure')
-local reference = request('^.data.reference_structure')
+-- Last mod.: 2024-11-11
 
+-- Imports:
+local Merge = request('!.table.merge')
+local SoftPatch = request('!.table.soft_patch')
+
+local Reference = request('^.data.reference_structure')
+
+-- Exports:
 return
-  function(data)
-    verify_structure(data, reference, true)
+  function(Sample)
+    Merge(Sample, Reference)
+    SoftPatch(Sample, Reference)
   end
+
+--[[
+  2020-09-13
+  2024-11-11
+]]
