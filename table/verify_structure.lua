2764ecd1c2d633c107bc6318f299321f3cf1a4c8
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-11-11 09:23:55 +0200

    [!/+] [table.verify_structure] Changed function contract.
    [-] Dropped option to add missing values.
    [+] Added parameter to also match types.

--- table/verify_structure.lua
+++ table/verify_structure.lua
@@ -1,55 +1,83 @@
 --[[
   Verify structure of given table versus reference table.
 
-  Has option to generate missing data into passed table.
+  We're checking that key in reference is present in sample.
 
-  Returns true if no issues were found.
+  If third parameter is true then we're also checking that
+  value types are same.
 
-  Reference table is ordinary Lua table. Valid value type in given
-  table is type of value in reference table.
+  If reference value is table, we'll dive into table and
+  call ourselves for that subtable.
 
-  For numbers we allow sample to be integer for float reference.
+  Examples:
 
-  So table {type = 'point_2d', x = 10.0, y = 0.0} matches
-  reference {type = 'any string', x = 0.0, y = 1.1}.
+    True (no type checks):
+
+      ({ FileHandle = 0 }, { FileHandle = io.stdin })
+
+    False (type checks):
+
+      ({ FileHandle = 0 }, { FileHandle = io.stdin }, true)
+      ({ Answer = 42.0 }, { Answer = 42 }, true)
+
+    False (missing key in sample):
+
+      ({ FileHandle = nil }, { FileHandle = io.stdin })
+
+    True (nested structure):
+
+      (
+        { Pixel = { Red = 128, Green = 128, Blue = 128 } },
+        { Pixel = { Red = 0, Green = 0, Blue = 0 } }
+      )
+
+    False (missing structure in sample):
+
+      (
+        { Pixel = '#808080' },
+        { Pixel = { Red = 0.0, Green = 1.0, Blue = 1.0 } }
+      )
 ]]
 
-return
-  function(sample, reference, generate_missing_data)
-    assert_table(sample)
-    assert_table(reference)
-    local result = true
+-- Last mod.: 2024-11-11
 
-    local process_table
-    process_table =
-      function(sample, reference)
-        for key, value in pairs(reference) do
-          if
-            (type(sample[key]) ~= type(value)) or
-            (
-              is_number(sample[key]) and
-              not is_integer(sample[key]) and
-              is_integer(value)
-            )
-          then
-            result = false
-            if generate_missing_data then
-              sample[key] = value
-            else
-              break
-            end
-          end
-          if is_table(value) then
-            process_table(sample[key], value)
-          end
+local VerifyStructure
+VerifyStructure =
+  function(Sample, Reference, CheckTypes)
+    assert_table(Sample)
+    assert_table(Reference)
+
+    for ReferenceKey, ReferenceValue in pairs(Reference) do
+      local SampleValue = Sample[ReferenceKey]
+
+      if is_nil(SampleValue) then
+        return false
+      end
+
+      if CheckTypes then
+        if (type(SampleValue) ~= type(ReferenceValue)) then
+          return false
         end
       end
 
-    process_table(sample, reference)
+      if is_table(ReferenceValue) then
+        if not is_table(SampleValue) then
+          return false
+        end
+        if not VerifyStructure(SampleValue, ReferenceValue) then
+          return false
+        end
+      end
+    end
 
-    return result
+    -- Success if we're still alive
+    return true
   end
+
+-- Exports:
+return VerifyStructure
 
 --[[
   2020-09-13
+  2024-11-11
 ]]
