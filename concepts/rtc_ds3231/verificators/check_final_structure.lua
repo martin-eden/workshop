986fe6413112bf37402185a2c0eafa8f841056d4
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-11-11 09:32:19 +0200

    [~] [DS3231.check_final_structure] Sync with [table.verify_structure].

--- concepts/rtc_ds3231/verificators/check_final_structure.lua
+++ concepts/rtc_ds3231/verificators/check_final_structure.lua
@@ -1,15 +1,21 @@
---[[
-  Verify parsed table for formal correctness.
+-- Check that given table has required fields and value types
 
-  So it should have all required fields with correct type.
-]]
+-- Last mod.: 2024-11-11
 
-local verify_structure = request('!.table.verify_structure')
-local reference = request('^.data.reference_structure')
+-- Imports:
+local VerifyStructure = request('!.table.verify_structure')
+local Reference = request('^.data.reference_structure')
 
+-- Exports:
 return
-  function(data)
-    if not verify_structure(data, reference) then
+  function(Sample)
+    local TypesMatching = true
+    if not VerifyStructure(Sample, Reference, TypesMatching) then
       coroutine.yield()
     end
   end
+
+--[[
+  2020-09
+  2024-11
+]]
