890eda338c235003fd1a11164ecd31d8dfa0f604
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-11-20 15:16:56 +0200

    [.^] [system.get_program_output_lines] Using [string.to_lines].

--- system/get_program_output_lines.lua
+++ system/get_program_output_lines.lua
@@ -1,23 +1,26 @@
 --[[
-  Execute shell command from given string.
-  Return text output as table with a sequence of lines.
-
-  (cmd:string) -> { ...:string }
+  Execute shell command in given string.
+  Return command output as list of lines.
 ]]
 
+-- Last mod.: 2024-11-20
+
+local StringToLines = request('!.string.to_lines')
+
 return
-  function(cmd)
-    assert_string(cmd)
-    local result = {}
-    local result_file = io.popen(cmd, 'r')
-    for line in result_file:lines() do
-      result[#result + 1] = line
-    end
-    result_file:close()
-    return result
+  function(Command)
+    assert_string(Command)
+    local OutputPipe = io.popen(Command, 'r')
+    local OutputText = OutputPipe:read('a')
+    OutputPipe:close()
+
+    local Result = StringToLines(OutputText)
+
+    return Result
   end
 
 --[[
   2017-08-11
   2024-02-11 Documentation change
+  2024-11-20
 ]]
