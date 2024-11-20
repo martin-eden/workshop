05b4da004a06593d2ef9fd3ad9210cdd918f8d3f
Ian <ian@somewhere.com>
2024-11-20 19:30:56 +0200

    [/] [concepts.Lines.AssertValidValue] Added string now can have newlines..
    Original design was that values of list are strings without newlines.
    
    But now I'm adding multiline values (lines with code) and I don't want
    to convert them to separate lines before adding. It's not bothering,
    it's wrong. Indent() will add two spaces to each line of multiline
    value. If there is string, we're done.
    
    So I don't want Indent() to touch contents of value, only prefix.

--- concepts/Lines/AssertValidValue.lua
+++ concepts/Lines/AssertValidValue.lua
@@ -1,16 +1,14 @@
--- Assert that value is string without newlines
+-- Assert that value is string
 
--- Last mod.: 2024-10-31
-
-local AssertNoNewlines = request('!.string.assert_no_newlines')
+-- Last mod.: 2024-11-20
 
 -- Exports:
 return
   function(self, String)
     assert_string(String)
-    AssertNoNewlines(String)
   end
 
 --[[
   2024-10-31
+  2024-11-20
 ]]
