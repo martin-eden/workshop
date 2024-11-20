42f65be3bf909ba85da3a2c5b318be9c38d33af6
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-11-20 20:34:00 +0200

    [*] [Geometry.Segments.GetIntersection] Pacman ate periods from source!..
    Seriously I found this issue only at testing melder on my framework.

--- concepts/Geometry/Segments/GetIntersection.lua
+++ concepts/Geometry/Segments/GetIntersection.lua
@@ -23,14 +23,20 @@
 
 ]]
 
-local Segment = request('^ Segment Interface')
+-- Last mod.: 2024-11-20
+
+local Segment = request('^.Segment.Interface')
 
 return
   function(SegA, SegB)
     assert_table(SegA)
     assert_table(SegB)
     local result = new(Segment)
-    result:SetStart(math max(SegA:GetStart(), SegB:GetStart()))
-    result:SetFinish(math min(SegA:GetFinish(), SegB:GetFinish()))
+    result:SetStart(math.max(SegA:GetStart(), SegB:GetStart()))
+    result:SetFinish(math.min(SegA:GetFinish(), SegB:GetFinish()))
     return result
   end
+
+--[[
+  2022-01
+]]
