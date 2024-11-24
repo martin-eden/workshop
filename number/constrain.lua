aaa87d7435d10ab74d9458949352fe4406b2fce9
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-11-24 03:16:53 +0200

    [.^] [number.constrain] Code style change.

--- number/constrain.lua
+++ number/constrain.lua
@@ -1,14 +1,20 @@
---[[
-  Constrain given number between min and max values.
-]]
+-- Constrain given number between min and max values
+
+-- Last mod.: 2024-11-24
 
 return
   function(num, min, max)
     if (num < min) then
       return min
-    elseif (num > max) then
-      return max
-    else
-      return num
     end
+
+    if (num > max) then
+      return max
+    end
+
+    return num
   end
+
+--[[
+  2020-09
+]]
