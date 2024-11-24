cc42682cb36bbdb56509ba88bec331b3106dc20a
Ian <ian@somewhere.com>
2024-11-24 03:48:41 +0200

    [.] [mechs.graph.dfs] Renamed int var "deep" to "depth".

--- mechs/graph/dfs/dfs.lua
+++ mechs/graph/dfs/dfs.lua
@@ -1,5 +1,7 @@
 -- DFS-pass of given graph
 
+-- Last mod.: 2024-11-24
+
 return
   function(self, graph)
     self.nodes_status = {}
@@ -19,7 +21,7 @@ return
     local dfs_visit
 
     local process =
-      function(parent, parent_key, node, deep)
+      function(parent, parent_key, node, depth)
         init_node_rec(node)
         local node_rec = nodes_status[node]
         node_rec.refs = node_rec.refs or {}
@@ -28,7 +30,7 @@ return
         if not node_rec.color then
           node_rec.parent = parent
           node_rec.parent_key = parent_key
-          dfs_visit(node, deep + 1)
+          dfs_visit(node, depth + 1)
         elseif (node_rec.color == 'gray') then
           node_rec.part_of_cycle = true
           nodes_status[parent].part_of_cycle = true
@@ -36,21 +38,26 @@ return
       end
 
     dfs_visit =
-      function(node, deep)
+      function(node, depth)
         time = time + 1
         local node_rec = nodes_status[node]
         node_rec.discovery_time = time
         node_rec.color = 'gray'
-        handle_discovery(node, node_rec, deep)
+        handle_discovery(node, node_rec, depth)
         for _, child in ipairs(self:get_children(node)) do
-          process(node, child.key, child.value, deep)
+          process(node, child.key, child.value, depth)
         end
         time = time + 1
         node_rec.color = 'black'
         node_rec.finish_time = time
-        handle_leave(node, node_rec, deep)
+        handle_leave(node, node_rec, depth)
       end
 
     init_node_rec(graph)
     dfs_visit(graph, 0)
   end
+
+--[[
+  2016
+  2018
+]]
