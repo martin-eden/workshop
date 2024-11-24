cc9f6c41ade3ef322f0f7c480444a2ce41fd4fad
Ian <ian@somewhere.com>
2024-11-24 18:42:38 +0200

    [~] [concepts.List.ApplyFunction]

--- concepts/List/ApplyFunction.lua
+++ concepts/List/ApplyFunction.lua
@@ -1,23 +1,21 @@
 -- Apply function to each element in list. Return new list
 
--- Last mod.: 2024-10-24
+-- Last mod.: 2024-11-24
+
+-- Imports:
+local ApplyFunc = request('ApplyFunctionInPlace')
 
 -- Exports:
 return
   function(Func, List)
-    assert_function(Func)
-    assert_table(List)
+    local Result = new(List)
 
-    local Result = {}
-
-    for Key, Value in ipairs(List) do
-      local FuncValue = Func(Value)
-      table.insert(Result, FuncValue)
-    end
+    ApplyFunc(Func, Result)
 
     return Result
   end
 
 --[[
   2024-10-24
+  2024-11-24
 ]]
