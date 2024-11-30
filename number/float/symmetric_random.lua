7d7851dd2f6f2bc87ea749dd06a0e0167a9a5f50
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-11-30 13:14:53 +0200

    [>] [number:] More disgustingly trivial functions..
    [+] float.symmetric_random
    [+] float.get_middle
    [+] integer.get_middle
    [+] integer.get_gap

---
+++ number/float/symmetric_random.lua
@@ -0,0 +1,10 @@
+-- Return random value from flat distribution in interval [-1.0, +1.0]
+
+return
+  function()
+    return (math.random() * 2.0 - 1.0)
+  end
+
+--[[
+  2024-09
+]]
---
+++ number/integer/get_middle.lua
@@ -0,0 +1,15 @@
+-- Return integer in the middle of given two
+
+-- Last mod.: 2024-11-30
+
+local GetMiddle =
+  function(Left, Right)
+    return (Left + Right) // 2
+  end
+
+-- Exports:
+return GetMiddle
+
+--[[
+  2024-11-30
+]]
