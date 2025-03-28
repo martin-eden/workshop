4427f0b909a5dec72902823ebc29de97061e92af
Ian <ian@somewhere.com>
2025-03-28 18:01:08 +0200

    [.] [concepts.LinearPlasmGenerator.TransformDistance] Code style retouch.

--- concepts/LinearPlasmGenerator/TransformDistance.lua
+++ concepts/LinearPlasmGenerator/TransformDistance.lua
@@ -1,21 +1,21 @@
 -- Distance transformation function
 
--- Last mod.: 2024-11-25
+-- Last mod.: 2025-03-28
 
 --[[
-  Distance transformation function maps value
-  from [0.0, 1.0] to another value in [0.0, 1.0].
+  Distance transformation function maps value from [0.0, 1.0]
+  to another value in [0.0, 1.0].
+
+  That is the essence of "plasm" pattern. Noise is proportional to
+  distance and distance is pre-processed here.
 
   Try plugging wilder functions!
 ]]
 
-local DistanceIdentity =
+return
   function(self, Distance)
     return Distance
   end
-
--- Exports:
-return DistanceIdentity
 
 --[[
   2024-11-25
