e3ee54faaa23873e2c95ace7eaf94f92f71e4f96
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-03-28 15:06:04 +0200

    [/] [concepts.Image.Color.Rgb] Renamed from "Interface".

---
+++ concepts/Image/Color/Rgb.lua
@@ -0,0 +1,35 @@
+-- RGB color structure for images
+
+-- Last mod.: 2025-03-28
+
+--[[
+  For our processing "color" is just a fixed-length list of
+  something. Type of "something" depends what functions you will
+  call for that list.
+
+  Okay, for RGB it's list of three floats.
+  For grayscale it's list of one float.
+  For bitmap it's list of one integer.
+
+  (Text above is not a contract, just try to describe structure
+  in simple terms.)
+]]
+
+-- Imports:
+local NameList = request('!.concepts.List.AddNames')
+
+local Color = { 0.0, 0.0, 0.0 }
+
+local Names = { 'Red', 'Green', 'Blue' }
+
+-- Annotate component indices (sets metatable)
+NameList(Color, Names)
+
+-- Exports:
+return Color
+
+--[[
+  2024-11-24
+  2024-11-25
+  2025-03-28
+]]
