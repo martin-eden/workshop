ffc416ea8645e2dad1e232215fac600ab3cc082b
Ian <ian@somewhere.com>
2025-03-28 15:26:25 +0200

    [.*] [concepts.Ppm.Parser.CharacterClassifier.Init] Syntax typo fix.

--- concepts/Ppm/Parser_PpmToIs/ItemGetter/CharacterClassifier/Init.lua
+++ concepts/Ppm/Parser_PpmToIs/ItemGetter/CharacterClassifier/Init.lua
@@ -9,9 +9,9 @@ local MapValues = request('!.table.map_values')
 -- Exports:
 return
   function(self)
-    self.SpaceMap = MapValues(ToList(self.Space)),
-    self.LineCommentStartMap = MapValues(ToList(self.LineCommentStart)),
-    self.LineCommentEndMap = MapValues(ToList(self.LineCommentEnd)),
+    self.SpaceMap = MapValues(ToList(self.Space))
+    self.LineCommentStartMap = MapValues(ToList(self.LineCommentStart))
+    self.LineCommentEndMap = MapValues(ToList(self.LineCommentEnd))
   end
 
 --[[
