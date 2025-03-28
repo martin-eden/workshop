dbe2534733b29ea2e11c210025a946565d8c09b5
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-03-28 18:00:30 +0200

    [/] [concepts.LinearPlasmGenerator] "Color" record abstracted..
    Before it was concrete list of three floats from "Image.Color.Interface".
    But we don't really need to know what "color" is. We use generic
    operations to
      * calculate middle color
      * add noise to color
    
    They don't care how much color components color has. They just callings
    functions to every element of "color components" list.
    
    We want to use same algorithm for grayscale colors. I.e. color will
    have just one component.

--- concepts/LinearPlasmGenerator/Interface.lua
+++ concepts/LinearPlasmGenerator/Interface.lua
@@ -1,6 +1,6 @@
 -- Plasm generator interface
 
--- Last mod.: 2024-11-25
+-- Last mod.: 2025-03-28
 
 -- Exports:
 return
@@ -16,14 +16,18 @@ return
     -- Flag to create tileable pattern
     OnRing = false,
 
+    -- Distance scaling function [0.0, 1.0] -> [0.0, 1.0]
+    TransformDistance = request('TransformDistance'),
+
+    -- Type of color. Three-floats list for rgb, one-float list for grayscale.
+    BaseColor = request('!.concepts.Image.Color.Rgb'),
+
     -- [Main]
 
     -- Generate 1-D gradient
     Run = request('Run'),
 
-    -- [Output]
-
-    -- Result image
+    -- Result image line
     Image = {},
 
     -- [Internal]
@@ -45,13 +49,11 @@ return
 
     -- Noise function [0.0, 1.0] -> [-1.0, 1.0]
     MakeDistanceNoise = request('MakeDistanceNoise'),
-
-    -- Distance scaling function [0.0, 1.0] -> [0.0, 1.0]
-    TransformDistance = request('TransformDistance'),
   }
 
 --[[
   2024-09-30
   2024-11-06
   2024-11-24
+  2025-03-28
 ]]
