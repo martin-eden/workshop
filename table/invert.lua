af123d412cfed89ff7c1b67db394d63407f5984b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-03-30 01:13:54 +0200

    [^+] [table.invert] Function documentation.

--- table/invert.lua
+++ table/invert.lua
@@ -1,9 +1,36 @@
-return
-  function(t)
-    assert_table(t)
-    local result = {}
-    for k, v in pairs(t) do
-      result[v] = k
+-- Invert table
+
+-- Last mod.: 2025-03-30
+
+--[[
+  Invert table - for simple substitution tables
+
+  Examples:
+
+    { 'value_a', 'value_b' } -> { value_a = 1, value_b = 2 }
+
+  See also
+
+    * [get_paths] to get a list of paths to values in tree
+]]
+local InvertTable =
+  function(Table)
+    assert_table(Table)
+
+    local Result = {}
+
+    for Key, Value in pairs(Table) do
+      Result[Value] = Key
     end
-    return result
+
+    return Result
   end
+
+
+-- Exports:
+return InvertTable
+
+--[[
+  2019-12-01
+  2025-03-30
+]]
