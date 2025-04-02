e9f4908297032d1b4d47e98bf975a07327663638
Ian <ian@somewhere.com>
2025-04-02 22:56:29 +0200

    [+] [number.integer.get_distance]

---
+++ number/integer/get_distance.lua
@@ -0,0 +1,13 @@
+-- Return distance between two integer numbers
+
+-- Last mod.: 2025-04-02
+
+-- Exports:
+return
+  function(Left, Right)
+    return (Right - Left)
+  end
+
+--[[
+  2025-04-02
+]]
