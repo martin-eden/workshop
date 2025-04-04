cdc4cdaae30da0ff97e0ee08da1a8c76c50be995
Ian <ian@somewhere.com>
2025-04-04 23:13:47 +0200

    [^] [number.float.get_middle] Changed implementation to support varargs.

--- number/float/get_middle.lua
+++ number/float/get_middle.lua
@@ -1,11 +1,30 @@
--- Return float between two floats
+-- Return average of float arguments
+
+-- Last mod.: 2025-04-04
+
+--[[
+  Calculate average of given float-numbers
+
+  Accepts sequence of arguments. No type checks.
+]]
+local FloatMid =
+  function(...)
+    local NumArgs = select('#', ...)
+
+    local Sum = 0.0
+
+    for Index = 1, NumArgs do
+      local Arg = select(Index, ...)
+      Sum = Sum + Arg
+    end
+
+    return Sum / NumArgs
+  end
 
 -- Exports:
-return
-  function(Left, Right)
-    return (Left + Right) / 2
-  end
+return FloatMid
 
 --[[
   2024-11-30
+  2025-04-04
 ]]
