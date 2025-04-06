ef4f3b37aeab044bb983224e99995b667892b7e0
Ian <ian@somewhere.com>
2025-04-06 21:51:50 +0200

    [+] [frontend.AnsiTerm] Added background-set-color and reset-attributes.

--- frontend/AnsiTerm/Interface.lua
+++ frontend/AnsiTerm/Interface.lua
@@ -32,10 +32,16 @@ return
         Width = tonumber(Width)
         return Width, Height
       end,
+    BackgroundSetColor =
+      function(Red, Green, Blue)
+        return (CSI .. '48;2;%d;%d;%dm'):format(Red, Green, Blue)
+      end,
+    ResetAttributes = CSI .. '0m',
   }
 
 --[[
   2021-11-28
   2021-12-07
   2022-01-31
+  2025-04-06
 ]]
