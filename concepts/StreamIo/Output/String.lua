d5b7a2b5bb2c7165230cab624a2c23175dde2d01
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-04-07 08:19:03 +0200

    [/-] [StreamIo.Output.String] Interface reduction..
    Before there were four methods: Write(), GetString(), Flush() and
    Aggregate(). Now just two: Write() and GetString().
    
    Aggregate() was for manual memory packing. Better use GetString()
    and unlink this class to let Lua's garbage collector do the job.
    
    Flush() was deleting data. It's not the scope of interface.

--- concepts/StreamIo/Output/String.lua
+++ concepts/StreamIo/Output/String.lua
@@ -1,46 +1,19 @@
--- Writes strings to memory. Implements [Output]
+-- [Output] stream interface for strings
+
+-- Last mod.: 2025-04-07
 
 --[[
-  Implementation
+  "Output" interface is just one method: Write(string)
 
-  Example
-                  Chunks
-    ---------------------
-                | {}
-    Write("a")  | {'a'}
-    Write("bc") | {'a', 'bc'}
-    Write("")   | ditto
-    GetString() | ditto
-      > "abc"   |
-    Aggregate() | {'abc'}
-    Flush()     | {}
+  But string concatenation is expensive in Lua. So Write()
+  adds string to internal table.
 
-  Data
-
-    Chunks: {}
-
-      List of strings.
-
-  Methods
-
-    Write(string): int, bool
-
-      Actually adds string to chunks.
-
-    GetString(): str
-
-      Returns string as chunks concatenation.
-
-    Flush()
-
-      Empties chunks.
-
-    Aggregate()
-
-      Merges several chunks to one
+  To get concatenated string call GetString().
 ]]
 
--- Contract: Write string
+--[[
+  Add string to result string
+]]
 local Write =
   function(self, Data)
     assert_string(Data)
@@ -52,45 +25,27 @@ local Write =
     return #Data, true
   end
 
--- Intestines: flush chunks
-local Flush =
-  function(self)
-    self.Chunks = {}
-  end
-
--- Intestines: return sum of chunks
+--[[
+  Return result string
+]]
 local GetString =
   function(self)
     return table.concat(self.Chunks)
   end
 
--- Intestines: merge chunks
-local Aggregate =
-  function(self)
-    if (#self.Chunks >= 2) then
-      local Data = GetString(self)
-      Flush(self)
-      self:Write(Data)
-    end
-  end
-
+-- Exports:
 return
   {
-    -- Interface
+    -- [Interface]
     Write = Write,
-
-    -- Intestines
-    Chunks = {},
-
-    -- Intestines management
-    Flush = Flush,
     GetString = GetString,
-    Aggregate = Aggregate,
+
+    -- [Internals]
+    Chunks = {},
   }
 
 --[[
-  2024-07-19
-  2024-07-24
-  2024-08-04
-  2024-08-09
+  2024-07 # #
+  2024-08 # #
+  2025-04-07
 ]]
