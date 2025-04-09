2a4198eb0b4c1e9641af787d06f34b7b37c456ef
Ian <ian@somewhere.com>
2025-04-09 06:33:49 +0200

    [/+] [Image.Color.SpawnColor] Now explodes in case of unknown format.

--- concepts/Image/Color/SpawnColor.lua
+++ concepts/Image/Color/SpawnColor.lua
@@ -1,22 +1,31 @@
 -- Create color record, depending of color format
 
--- Last mod.: 2025-03-31
+-- Last mod.: 2025-04-09
 
 -- Imports:
 local RgbColor = request('Rgb')
 local GsColor = request('Grayscale')
 
--- Exports:
-return
+--[[
+  Create color instance depending of string with color format.
+
+  In case of fail explodes.
+]]
+local SpawnColor =
   function(ColorType)
     if (ColorType == 'Rgb') then
       return new(RgbColor)
     elseif (ColorType == 'Gs') then
       return new(GsColor)
+    else
+      error(('Unknown color format %q.'):format(ColorType))
     end
   end
 
+-- Exports:
+return SpawnColor
+
 --[[
-  2025-03-29
-  2025-03-31
+  2025-03 # #
+  2025-04-09
 ]]
