eac4066a96c2a9073d8fc2090ac3f30c480f9a6c
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-04-26 03:31:52 +0200

    [+] [Gradient.1d.BintreeExecutionPlan] ..
    Somewhat surprisingly using binary segments tree partitioning
    produces similar patterns to left-side-squishing partitioning.

---
+++ concepts/Gradient/1d/Alternatives/BintreeExecutionPlan/CreateExecutionPlan.lua
@@ -0,0 +1,51 @@
+-- .CreateExecutionPlan() replacement with middle divisions
+
+-- Last mod.: 2025-04-26
+
+-- Imports:
+local GetIntMid = request('!.number.integer.get_middle')
+
+local Divide
+Divide =
+  function(Left, Right, Result)
+    if (Right - Left <= 1) then
+      return
+    end
+
+    local Mid = GetIntMid(Left, Right)
+
+    if (Mid == Left) then
+      return
+    end
+
+    table.insert(Result, { Mid, Left, Right })
+
+    Divide(Left, Mid, Result)
+    Divide(Mid, Right, Result)
+  end
+
+
+local CreateExecutionPlan =
+  function(self)
+    local Left = 1
+    local Right = self.Line.Length
+
+    local Result = {}
+
+    Divide(Left, Right, Result)
+
+    return Result
+  end
+
+-- Exports:
+return CreateExecutionPlan
+
+--[[
+  2024-09 #
+  2024-10 #
+  2024-11 # # # #
+  2025-04-09
+  2025-04-16
+  2025-04-23
+  2025-04-26
+]]
