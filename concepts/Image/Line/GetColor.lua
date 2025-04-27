daefde2ac80aaf2d78a58a395a4a620f4682dba9
Ian <ian@somewhere.com>
2025-04-27 21:12:45 +0200

    [?+~] [Image.Line.(Get/Set)Color] ..
    Experimental design change with removing knowledge of image line
    structure from gradient generation code. It does not want to know
    (and support!) all gore details!

---
+++ concepts/Image/Line/GetColor.lua
@@ -0,0 +1,15 @@
+-- Return color of pixel (color is list)
+
+-- Last mod.: 2025-04-27
+
+local GetColor =
+  function(Line, Point)
+    return Line[Point]
+  end
+
+-- Exports:
+return GetColor
+
+--[[
+  2025-04-27
+]]
