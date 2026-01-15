8a3f9d210e623bee6bab19f0a9ce9a561eb27bdf
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-01-15 07:50:11 +0200

    [/] [Gradient] Noisy CreatePixel() moved out of 1-d directory.

--- concepts/Gradient/CreatePixel_Noisy/ObservePixel.lua
+++ concepts/Gradient/CreatePixel_Noisy/ObservePixel.lua
@@ -1,16 +1,20 @@
 -- Returns pixel's color with distance-dependent noise
 
--- Last mod.: 2025-04-23
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-01-14
+]]
 
 -- Imports:
 local ClampUi = request('!.number.constrain_ui')
+local GetDistance = request('!.concepts.Geometry.TwoPoints.GetDistance')
 
 local ObservePixel =
-  function(Ours, Theirs, AnotherPoint, OurPoint)
+  function(Ours, Theirs, OurPoint, AnotherPoint)
     local Color = new(Theirs:GetPixel(AnotherPoint))
 
     local Distance =
-      math.abs(OurPoint - AnotherPoint) / Ours.PixelsPerDistance
+      GetDistance(OurPoint, AnotherPoint) / Ours.PixelsPerDistance
 
     for ComponentIndex = 1, #Color do
       local ComponentValue =
@@ -29,4 +33,5 @@ return ObservePixel
 
 --[[
   2025-04-23
+  2026-01-13
 ]]
