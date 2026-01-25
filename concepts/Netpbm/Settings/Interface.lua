27293c5048026f941b9c6d306b5faa2f25600a88
Ian <ian@somewhere.com>
2026-01-25 22:43:41 +0200

    [.^<] [NetPbm.Settings] Format Labels moved to separate file.

--- concepts/Netpbm/Settings/Interface.lua
+++ concepts/Netpbm/Settings/Interface.lua
@@ -2,11 +2,11 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-01-15
+  Last mod.: 2026-01-25
 ]]
 
 --[[
-  That's programmatic description "netpbm" family of formats.
+  That's programmatic description of "netpbm" family of formats.
 ]]
 
 --[[
@@ -14,65 +14,51 @@
 
     https://netpbm.sourceforge.net/doc/#formats
 
-  Well, there are fucking lot of annoying words but at core it's
-  simple.
+  Well there are fucking lot of annoying words but at core it's simple.
 
   We're dealing with images. Image is rectangular 2-d matrix of colors.
   So any image will have height and width.
 
-  Color can be black/white (one bit), grayscale (1 to 16 bits) and
-  RGB (24 bits).
+  * Color can be
 
-  That color data can be encoded as plain text or as binary integers.
+      * black/white (one bit)
+      * grayscale (1 to 16 bits)
+      * RGB (24 bits)
+
+  * Color data can be encoded as
+
+    * plain text
+    * binary integers
 
   That makes six variations.
 
-  Also, one more format to rule them all "pam" - (p)ortable (a)rbitrary
-  (m)ap. Late child to embrace them all and add "transparency"
-  color component support.
-
-    ( "pam" (P7) is not supported in this code. No practical need. )
+  Also one more format to rule them all "pam" - (p)ortable (a)rbitrary
+  (m)ap. Late child to embrace them all and add "transparency" color
+  component support. It is not supported in this code. No need.
 ]]
-
--- Color and data formats
-local FormatLabels =
-  {
-    Text =
-      {
-        bw = 'P1',
-        gs = 'P2',
-        rgb = 'P3',
-      },
-    Binary =
-      {
-        bw = 'P4',
-        gs = 'P5',
-        rgb = 'P6',
-      },
-  }
 
 -- Exports:
 return
   {
-    -- ( Map of available values
-    FormatLabels = FormatLabels,
-    -- )
-
+    -- [Internal] Mapping for available format signatures
+    FormatLabels = request('Internal.FormatLabels'),
     -- ( Actual location on that map
     ColorFormat = 'rgb',
     DataEncoding = 'Text',
-    GetFormatLabel = request('GetFormatLabel'),
-    SetFormatLabel = request('SetFormatLabel'),
     -- )
+    -- Get format signature from Color Format and Data Encoding
+    GetFormatLabel = request('GetFormatLabel'),
+    -- Set Color Format and Data Encoding from format signature
+    SetFormatLabel = request('SetFormatLabel'),
 
     -- Line comment character
     LineCommentChar = '#',
 
     --[[
-       Max color component value
+      Max color component value
 
-       Format allows any integer in [1, 65535]. But we're fixing it
-       to 255 because it's the only value we need.
+      Format allows any integer in [1, 65535]. But we're fixing it
+      to 255 because it's the only value we need.
     ]]
     MaxColorValue = 255,
   }
@@ -80,6 +66,7 @@ return
 --[[
   2024-11-02
   2025-03-29
-  2025-03-30
+  2025-04-01
   2026-01-15
+  2026-01-25
 ]]
---
+++ concepts/Netpbm/Settings/Internal/FormatLabels.lua
@@ -0,0 +1,36 @@
+-- Mapping from our data type record to NetPBM string signature
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-01-25
+]]
+
+--[[
+  Color and data formats
+
+  Key names are important. They are used as values for interface fields
+  <DataEncoding> and <ColorFormat>.
+]]
+local FormatLabels =
+  {
+    Text =
+      {
+        bw = 'P1',
+        gs = 'P2',
+        rgb = 'P3',
+      },
+    Binary =
+      {
+        bw = 'P4',
+        gs = 'P5',
+        rgb = 'P6',
+      },
+  }
+
+-- Export:
+return FormatLabels
+
+--[[
+  2025-04-01
+  2026-01-25
+]]
