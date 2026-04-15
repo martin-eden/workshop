81ee2a312ec49bb176f2b222f4a1b82b787fbdd7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-15 04:21:07 +0200

    [~] [mechs.tty.set_non_blocking_read] Adjusted implementation style.

--- mechs/tty/set_non_blocking_read.lua
+++ mechs/tty/set_non_blocking_read.lua
@@ -1,12 +1,17 @@
 -- Set baud rate and read timeout for TTY device given by name
 
--- Last mod.: 2024-10-29
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-15
+]]
+
+local ToString = request('!.concepts.Words.ToString')
+local RunCommand = request('!.mechs.run_command')
 
 --[[
   Non-blocking read means that if we reading ten bytes and
-  receive buffer has only seven bytes, we wait up to given
-  amount of time awaiting that three bytes. And then return
-  bytes we got.
+  receive buffer has only seven bytes, we will try to get remained
+  three bytes until timeout. And then return bytes we got.
 
   In multithreading sense it's still blocking operation.
   But at worst case here we're wasting given amount of time
@@ -14,22 +19,19 @@
 
   Input:
 
-    string - Port name
-    [float] - Read timeout (seconds). Default: 0.1
-    [int] - Port baud. Default: 57600
+    DeviceName - File name for device. Like "/dev/ttyUSB0"
+    ReadTimeout_S - [0.1] - Read timeout in seconds
+    Speed_Bps - [115200] - Connection speed
 ]]
-
-return
+local SetNonBlockingRead =
   function(
     DeviceName,
     ReadTimeout_S,
     Speed_Bps
   )
     assert_string(DeviceName)
-
     ReadTimeout_S = ReadTimeout_S or 0.1
-
-    Speed_Bps = Speed_Bps or 57600
+    Speed_Bps = Speed_Bps or 115200
 
     -- "stty" accepts waiting time in deciseconds
     ReadTimeout_dS = math.floor(ReadTimeout_S * 10)
@@ -39,42 +41,41 @@ return
 
         stty --file=%s %d -echoctl raw time %d min 0 cs8
 
-      but looks like that didn't work. Code was commented.
+      but that didn't work.
     ]]
-    local CommandFmt =
-      'stty' ..
-      ' --file=%s' ..
-      ' %d' ..
-      ' raw' ..
-      ' time %d' ..
-      ' min 0' ..
-      ' cs8' ..
-      ' -echo' ..
-      ' -echoctl' ..
-      ' -echoe' ..
-      ' -echok' ..
-      ' -echoke' ..
-      ' -icanon' ..
-      ' -icrnl' ..
-      ' -iexten' ..
-      ' -isig' ..
-      ' -ixon' ..
-      ' -opost'
+    local Sentence =
+      {
+        'stty',
+        '--file=' .. DeviceName,
+        tostring(Speed_Bps),
+        'raw',
+        'time',
+        tostring(ReadTimeout_dS),
+        'min'
+        '0',
+        'cs8',
+        '-echo',
+        '-echoctl',
+        '-echoe',
+        '-echok',
+        '-echoke',
+        '-icanon',
+        '-icrnl',
+        '-iexten',
+        '-isig',
+        '-ixon',
+        '-opost'
+      }
 
-    local Command =
-      string.format(
-        CommandFmt,
-        DeviceName,
-        Speed_Bps,
-        ReadTimeout_dS
-      )
-
-    assert(os.execute(Command))
+    RunCommand(ToString(Sentence))
   end
 
+-- Export:
+return SetNonBlockingRead
+
 --[[
-  2020-01
-  2021-11
-  2024-09
-  2024-10-29
+  2020 #
+  2021 #
+  2024 # #
+  2026-04-15
 ]]
