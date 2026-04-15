3c44bfdd9e4ad13ed1446b59748250ccb62040ec
Ian <ian@somewhere.com>
2026-04-15 04:19:59 +0200

    [+] [mechs.run_command] os.execute() wrapper..
    Yes in Lua it's simple. Why isolate? Why not use os.execute() directly?
    
    Program wants to run shell command. It's not anywhere means os.execute().
    In some environments there will be additional implementation.
    
    But program does should not care about implementation. So it calls
    contractor who does this job.
    
    .. and also it is handy for debugging. Print any command before execution.

---
+++ mechs/run_command.lua
@@ -0,0 +1,23 @@
+-- Execute OS shell command
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-15
+]]
+
+--[[
+  Execute OS shell command
+]]
+local ExecuteCommand =
+  function(Command)
+    assert_string(Command);
+
+    assert(os.execute(Command))
+  end
+
+-- Export:
+return ExecuteCommand
+
+--[[
+  2026-04-15
+]]
