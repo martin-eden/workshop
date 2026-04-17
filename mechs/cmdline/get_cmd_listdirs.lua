fb5ce7752ec7700af54c6cf36161dbf91369ba27
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-17 19:16:34 +0200

    [^] [mechs.cmdline] Style adjustment.

--- mechs/cmdline/get_cmd_listdirs.lua
+++ mechs/cmdline/get_cmd_listdirs.lua
@@ -1,27 +1,33 @@
---[[
-  Return shell command to list all directories in given directory.
-
-  GNU/Bash assumed.
-]]
+-- Return shell command to list all directories in given directory
 
 --[[
   Author: Martin Eden
   Last mod.: 2026-04-17
 ]]
 
-local CommandFmt = 'find %s -maxdepth 1 -type d'
+-- Imports:
+local <const> quote = request('!.concepts.shell.quote')
+local <const> glue_words = request('!.concepts.words.to_string')
 
-local QuoteFilename = request('!.concepts.shell.quote')
-
+-- Export:
 return
-  function(DirName)
-    DirName = QuoteFilename(DirName)
-    local Command = string.format(CommandFmt, DirName)
-    return Command
+  function(dir_name)
+    local <const> Command =
+      {
+        'find',
+        quote(dir_name),
+        '-maxdepth',
+        '1',
+        '-type',
+        'd',
+      }
+
+    return glue_words(Command)
   end
 
 --[[
-  2019-12-01
-  2024-10-21
+  2019 #
+  2024 #
   2026-01-12
+  2026-04-17
 ]]
