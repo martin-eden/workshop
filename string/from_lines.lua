1f6389bf062a0a1d41f14d98a7f1ba8bbb5ff9fd
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-17 12:22:20 +0200

    [^+] [string.from_lines] Additional optional arguments, restyling.

--- string/from_lines.lua
+++ string/from_lines.lua
@@ -1,42 +1,44 @@
--- Concatenate sequence of lines to single string
+-- Concatenate sequence of strings
 
 --[[
-  Input
-
-    table
-
-      Sequence of strings without newlines
-
-  Output
-
-    string
-
-      Concatenated string from sequence, separated by newlines.
-      Result string will always end with newline.
+  Author: Martin Eden
+  Last mod.: 2026-04-17
 ]]
 
--- Last mod.: 2024-03-02
+-- Import:
+local <const> IsNatural = request('!.number.is_natural')
 
 --[[
-  Good design is not about implementing complex things.
-  Good design is providing uniform interface to many things.
-
-  2024-02-28, -03-02
+  Concatenate strings with newline separator. Tailing newline.
 ]]
-
-return
-  function(Lines)
+local <const> LinesToString =
+  function(Lines, StartIdx, EndIdx)
     assert_table(Lines)
+    assert(IsNatural(StartIdx))
+    assert(IsNatural(EndIdx))
+    assert(StartIdx <= EndIdx)
+    assert(EndIdx <= #Lines)
 
-    local Result = ''
+    local Result
+    local <const> Separator = '\n'
 
-    Result = table.concat(Lines, '\n')
-
+    Result = table.concat(Lines, Separator, StartIdx, EndIdx)
     Result = Result .. '\n'
 
     return Result
   end
 
+-- Export:
+return LinesToString
+
+--[[
+  Good design is not about implementing complex things.
+  Good design is providing uniform interface to many things.
+
+  2024-02-28, 2024-03-02
+]]
+
 --[[
   2024-02-28
+  2026-04-17
 ]]
