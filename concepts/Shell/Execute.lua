88286ca05fb0c8d8dd825ad015adecbd05d9596c
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-17 12:53:28 +0200

    [./^] [Shell.Execute]

--- concepts/Shell/Execute.lua
+++ concepts/Shell/Execute.lua
@@ -9,6 +9,22 @@ local <const> FileAsString = request('!.file_system.file.as_string')
 local <const> GetCmd_ExecuteWithRedirects =
   request('!.mechs.cmdline.get_cmd_execute_with_redirects')
 
+--[[
+  Execute shell command and capture results
+
+  Executes given string as shell (OS-dependent) command.
+  Captures termination code / return code, output and errors.
+
+  Output
+
+    {
+      ResultCode -- [int] Return code in case of normal ending
+      TerminationCode -- [int] Termination code (signal number) in
+        case of aborted ending
+      Output -- [str] Program output
+      Errors -- [str] Program errors
+    }
+]]
 local <const> ExecuteShellCommand =
   function(Command)
     local <const> OutputFileName = os.tmpname()
@@ -22,24 +38,21 @@ local <const> ExecuteShellCommand =
     local <const> IsDone, ResultCodeType, ResultCode =
       os.execute(ShellCommand)
 
-    local <const> Output = FileAsString(OutputFileName)
-    local <const> Errors = FileAsString(ErrorsFileName)
-
-    os.remove(OutputFileName)
-    os.remove(ErrorsFileName)
+    if (ResultCodeType == 'exit') then
+      Result.ResultCode = ResultCode
+    end
+    if (ResultCodeType == 'signal') then
+      Result.TerminationCode = ResultCode
+    end
 
     local <const> Result =
       {
-        Output = Output,
-        Errors = Errors,
+        Output = FileAsString(OutputFileName),
+        Errors = FileAsString(ErrorsFileName),
       }
 
-    if (ResultCodeType == 'exit') then
-      Result.Code = ResultCode
-    end
-    if (ResultCodeType == 'signal') then
-      Result.Signal = ResultCode
-    end
+    os.remove(OutputFileName)
+    os.remove(ErrorsFileName)
 
     return Result
   end
