65ce5c9c5feb7b6065afe3d808e071409eb1aea0
Ian <ian@somewhere.com>
2026-04-17 12:43:03 +0200

    [/?] [lua.code.get_ast] Changed implementation. Not tested.

--- lua/code/get_ast.lua
+++ lua/code/get_ast.lua
@@ -1,30 +1,70 @@
+-- Parse string with Lua code to AST table
+
 --[[
-  Get AST for string with Lua code.
-
-  str -> table
-
-  Handles possible "#!" at start of shell files.
+  Author: Martin Eden
+  Last mod.: 2026-04-17
 ]]
 
-local parse = request('!.mechs.generic_loader')
-local syntax = request('!.concepts.lua.syntax')
-local parse_sh = request('!.concepts.sh.load')
+-- Imports:
+local <const> Lines = request('!.concepts.Lines')
+local <const> LuaSyntax = request('!.concepts.lua.syntax')
+local <const> Parse = request('!.mechs.generic_loader')
 
-return
-  function(s)
-    assert_string(s)
+local <const> IsShebang =
+  function(Str)
+    return (string.sub(Str, 1, 2) == '#!')
+  end
 
-    local sh = parse_sh(s)
-    local shebang_str
-    if sh.tool then
-      shebang_str = '#! ' .. sh.tool
+-- Extract program name from #! shebang string
+local <const> GetProgramName =
+  function(Str)
+    return string.match(Str, '#!%s*(.*)')
+  end
+
+--[[
+  Parse string with Lua code to AST table
+
+  Handles possible shebang line ("#!") at the start of data.
+
+  Returns table:
+
+    Table returned from parsing with added fields:
+
+    .shebang_str -- [] Optional shebang line
+    .unparsed_tail -- [] Optional unparsed data at the end
+    }
+]]
+local <const> GetAst =
+  function(CodeStr)
+    assert_string(CodeStr)
+
+    local <const> CodeLines = new(Lines)
+
+    CodeLines:FromString(CodeStr)
+
+    local <const> FirstLine = CodeLines:GetFirstLine()
+
+    local ShebangStr
+    if IsShebang(FirstLine) then
+      ShebangStr = '#! ' .. GetProgramName(FirstLine)
+      CodeLines:RemoveFirstLine()
     end
 
-    local result, unparsed_tail = parse(sh.data, syntax)
+    CodeStr = CodeLines:ToString()
 
-    result = result or {}
-    result.shebang_str = shebang_str
-    result.unparsed_tail = unparsed_tail
+    local Result, UnparsedTail = Parse(CodeStr, LuaSyntax)
 
-    return result
+    Result = Result or {}
+    Result.shebang_str = ShebangStr
+    Result.unparsed_tail = UnparsedTail
+
+    return Result
   end
+
+-- Export:
+return GetAst
+
+--[[
+  2018-02-05
+  2026-04-17
+]]
