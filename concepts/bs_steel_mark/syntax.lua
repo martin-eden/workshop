5ee822e14a7c0c9ef41698e0f720725aaca53875
Ian <ian@somewhere.com>
2026-04-17 19:57:41 +0200

    [-] [bs_steel_mark] Didn't find usage..
    Btw it was one of the oldest code. I started to write more
    parsers/compilers from this.

--- concepts/bs_steel_mark/syntax.lua
+++
@@ -1,9 +0,0 @@
-local handy = request('!.mechs.processor.handy')
-local match_regexp = request('!.mechs.parser.handy').match_regexp
-
-return
-  {
-    name = 'BS_steel_mark',
-    handy.is_not(match_regexp('2[5-9]')),
-    match_regexp('%d%d%d[AHMS]%d%d'),
-  }
