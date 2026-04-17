49ac690453d23505cdc5424c0b65390dee66bc7c
Ian <ian@somewhere.com>
2026-04-17 11:14:34 +0200

    [+] [cmdline.get_cmd_execute_with_redirects] Output/errors capturing.

---
+++ mechs/cmdline/get_cmd_execute_with_redirects.lua
@@ -0,0 +1,33 @@
+-- Return shell command to execute given string with output redirects
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-17
+]]
+
+local <const> Shell_QuoteString = request('!.concepts.BashString.Quote')
+
+--[[
+  Return command to execute command with stdout and stderr redirected
+]]
+local <const> Shell_GetCmd_ExecuteWithOutputRedirects =
+  function(Command, OutputFileName, ErrorsFileName)
+    local <const> QuotedCommand = Shell_QuoteString(Command)
+    local <const> CommandAndArgs =
+      {
+        'sh',
+        '-c', QuotedCommand,
+        '1>' .. OutputFileName,
+        '2>' .. ErrorsFileName,
+      }
+    local <const> WrappedCommand = table.concat(CommandAndArgs, ' ')
+
+    return WrappedCommand
+  end
+
+-- Export:
+return Shell_GetCmd_ExecuteWithOutputRedirects
+
+--[[
+  2026-04-17
+]]
