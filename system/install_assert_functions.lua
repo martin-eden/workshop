9e1af65100ba3b9004c92c19b31ee534689652aa
Ian <ian@somewhere.com>
2026-04-22 12:26:00 +0200

    [.^] [system.install_assert_functions] Code restyle.

--- system/install_assert_functions.lua
+++ system/install_assert_functions.lua
@@ -1,45 +1,49 @@
--- Function to spawn "assert_<type>" family of global functions
+-- Function to create "assert_<type>" global functions
 
-local data_types = request('!.lua.data_types')
-local data_mathtypes = request('!.lua.data_mathtypes')
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-22
+]]
 
-local generic_assert =
+-- Imports:
+local DataTypes = request('!.lua.data_types')
+local MathTypes = request('!.lua.data_mathtypes')
+
+local spawn_assert_func =
   function(type_name)
-    -- assert_string(type_name)
-    assert(type(type_name) == 'string')
+    local checker = _G['is_'.. type_name]
 
-    local checker_name = 'is_'.. type_name
-    local checker = _G[checker_name]
-
-    -- assert_function(checker)
-    assert(type(checker) == 'function')
+    assert(checker)
 
     return
       function(val)
         if not checker(val) then
           local err_msg =
             string.format('assert_%s(%s)', type_name, tostring(val))
+
           error(err_msg)
         end
       end
   end
 
-return
+local install_assert_funcs =
   function()
-    for _, type_name in ipairs(data_types) do
-      local global_name = 'assert_' .. type_name
-      _G[global_name] = generic_assert(type_name)
+    for _, type_name in ipairs(DataTypes) do
+      _G['assert_' .. type_name] = spawn_assert_func(type_name)
     end
 
-    for _, number_type_name in ipairs(data_mathtypes) do
-      local global_name = 'assert_' .. number_type_name
-      _G[global_name] = generic_assert(number_type_name)
+    for _, number_type_name in ipairs(MathTypes) do
+      _G['assert_' .. number_type_name] = spawn_assert_func(number_type_name)
     end
   end
+
+-- Export:
+return install_assert_funcs
 
 --[[
   2018-02
   2020-01
   2022-01
   2024-03
+  2026-04-22
 ]]
