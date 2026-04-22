02d1ec8db0bb4936075271d68e35717a43d97828
Ian <ian@somewhere.com>
2026-04-22 11:13:23 +0200

    [^] [system.sleep] Implementation uses shell.execute(). Code restyle.

--- system/sleep.lua
+++ system/sleep.lua
@@ -1,16 +1,31 @@
---[[
-  Delay of given number of seconds (real number).
+-- Delay for given amount of seconds (real number)
 
-  Using system "sleep" function but it is OS-dependent.
-  GNU/bash assumed.
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-22
 ]]
 
+-- Imports:
 local get_cmd_sleep = request('!.mechs.cmdline.get_cmd_sleep')
+local shell_execute = request('!.concepts.shell.execute')
 
-return
+--[[
+  Sleep for given real number of seconds
+]]
+local sleep =
   function(secs)
     assert_number(secs)
     assert(secs > 0)
-    local cmd = get_cmd_sleep(secs)
-    os.execute(cmd)
+
+    local shell_cmd = get_cmd_sleep(secs)
+
+    shell_execute(shell_cmd)
   end
+
+-- Export:
+return sleep
+
+--[[
+  2020 #
+  2026-04-22
+]]
