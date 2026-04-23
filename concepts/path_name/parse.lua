42dde99369c7b4c76dacbf28d60137a155e1b4d0
Ian <ian@somewhere.com>
2026-04-23 15:19:47 +0200

    [^] [path_name.parse] More work. Hope now implementation is complete..
    Well parsing pathname occurred to me more tricky than it seemed first.

--- concepts/path_name/parse.lua
+++ concepts/path_name/parse.lua
@@ -10,13 +10,45 @@
 
   Here is function to parse them.
 
-  It provides handy invariant: .Directory + .Name == Canonical path
-
   Also it provides list of node names in path.
 
   And flags that
     * path is absolute
     * ends on directory
+
+  Result structure
+
+    {
+      Directory -- [s] Parent directory path ending with '/'
+      Name -- [s] Name of leaf file or directory
+      IsDirectory -- [b] True if leaf is directory
+      IsAbsolute -- [b] True if Directory is absolute path
+      Path -- [t] List of directory names ending with leaf name
+    }
+]]
+
+--[[
+  Contract
+
+  * .Directory always ends with "/"
+  * .Name never contains "/"
+]]
+
+--[[
+  Using structure
+
+  * Canonical path == .Directory + .Name
+
+  * Using .Path
+
+    if .IsAbsolute
+      ChDir "/"
+
+    if not .IsDirectory
+      RemoveLastEntry .Path
+
+    for each NodeName in .Path
+      ChDir NodeName
 ]]
 
 -- Imports:
@@ -24,7 +56,7 @@ local starts_with = request('!.string.starts_with')
 local ends_with = request('!.string.ends_with')
 local split_string = request('!.string.split')
 
-local cleanup_pathname =
+local normalize_pathname =
   function(path_name)
     -- Replace "//" with "/"
     path_name = string.gsub(path_name, '//+', '/')
@@ -41,76 +73,28 @@ local cleanup_pathname =
       path_name, num_repl = string.gsub(path_name, '/%./', '/')
     until (num_repl == 0)
 
-    -- Replace tail "/." to "/"
-    path_name = string.gsub(path_name, '/%.$', '/')
+    -- If it's relative path and not starts with ".." ..
+    if
+      not starts_with(path_name, '/') and
+      not starts_with(path_name, '..')
+    then
+      -- .. add relative prefix
+      path_name = './' .. path_name
+    end
+
+    -- If it ends as directory ..
+    if ends_with(path_name, '/') then
+      -- .. add self name postfix
+      path_name = path_name .. '.'
+    end
+
+    assert(path_name ~= '')
 
     return path_name
   end
 
---[[
-  Parses string with Unix pathname. Returns table.
-
-  {
-    Directory -- [s] Parent directory path ending with '/'
-    Name -- [s] Name of leaf file or directory
-    IsDirectory -- [b] True if leaf is directory
-    IsAbsolute -- [b] True if Directory is absolute path
-    Path -- [t] List of directory names ending with leaf name
-  }
-
-  Note:
-
-    ".." element
-
-    It does not interpret "a/b/.." as "a/" because "b" may be
-    a symlink to another directory.
-
-    "IsDirectory" field may be false negative
-
-    We are using just provided string and can't check whether "a"
-    is directory or file. But for "a/" we know it's directory.
-
-  Examples:
-
-    /a
-      Directory = '/'
-      Name = 'a'
-      IsAbsolute = true
-      IsDirectory = false
-      Path = {'a'}
-    a/
-      Directory = ''
-      Name = 'a'
-      IsAbsolute = false
-      IsDirectory = true
-      Path = {'a'}
-    ././//a/./
-      Same as for "a/"
-    a/b/..
-      Directory = 'a/b/'
-      Name = '..'
-      IsDirectory = true
-      IsAbsolute = false
-      Path = {'a', 'b', '..'}
-    /
-      Directory = '/'
-      Name = ''
-      IsDirectory = true
-      IsAbsolute = true
-      Path = {}
-]]
-local parse_pathname =
+local get_path =
   function(path_name)
-    assert_string(path_name)
-
-    path_name = cleanup_pathname(path_name)
-
-    local is_absolute = starts_with(path_name, '/')
-
-    local is_directory =
-      ends_with(path_name, '/') or
-      ends_with(path_name, '..')
-
     if not ends_with(path_name, '/') then
       -- Adjust for split_string()
       path_name = path_name .. '/'
@@ -118,18 +102,53 @@ local parse_pathname =
 
     local path = split_string(path_name, '/')
 
-    if is_absolute then
-      -- For absolute paths like "/a" first entry in <path> is ""
+    assert(#path >= 2)
+
+    return path
+  end
+
+local get_parent_dir_name =
+  function(path)
+    return table.concat(path, '/', 1, #path - 1) .. '/'
+  end
+
+--[[
+  Parses string with Unix pathname. Returns table
+]]
+local parse_pathname =
+  function(path_name)
+    assert_string(path_name)
+
+    path_name = normalize_pathname(path_name)
+
+    local path = get_path(path_name)
+
+    local leaf_name = path[#path]
+
+    local parent_dir_name = get_parent_dir_name(path)
+
+    --[[
+      For absolute paths like "/a" first entry in <path> is "".
+      Relative paths may be prefixed with "./" or with "../".
+      So first entry is "." or "..".
+
+      We don't want first entry as "" or "." in export.
+    ]]
+    local first_item = path[1]
+    if
+      (first_item == '') or
+      (first_item == '.')
+    then
       table.remove(path, 1)
     end
 
-    local leaf_name = path[#path] or ''
+    local is_absolute =
+      starts_with(path_name, '/')
 
-    local parent_dir_name = table.concat(path, '/', 1, #path - 1)
-
-    if (parent_dir_name ~= '') or is_absolute then
-      parent_dir_name = parent_dir_name .. '/'
-    end
+    local is_directory =
+      ends_with(path_name, '/') or
+      ends_with(path_name, '/.') or
+      ends_with(path_name, '/..')
 
     local Result =
       {
@@ -145,6 +164,72 @@ local parse_pathname =
 
 -- Export:
 return parse_pathname
+
+--[[
+  Notes
+
+    * Empty string as pathname is illegal in POSIX
+
+      We treat it as ".".
+
+    * "./" prefix
+
+      We are adding "./" prefix to directory name for relative paths.
+      (Absolute paths have "/" as is.)
+
+      This is handy for outer code. It can rely that .Directory always
+      ends with "/".
+
+    * ".." element
+
+      It does not interpret "a/b/.." as "a/" because "b" may be
+      symlink to another directory.
+
+    * "IsDirectory" field may be false negative
+
+      We are using just provided string and can't check whether "a"
+      is directory or file. But for "a/" we know it's directory.
+
+  Examples
+
+    ""
+      Directory = './'
+      Name = '.'
+      Path = {'.'}
+      IsAbsolute = false
+      IsDirectory = true
+
+    /
+      Directory = '/'
+      Name = '.'
+      Path = {'.'}
+      IsAbsolute = true
+      IsDirectory = true
+
+    a/b/c
+      Directory = './a/b/'
+      Name = 'c'
+      Path = {'a', 'b', 'c'}
+      IsAbsolute = false
+      IsDirectory = false
+
+    a/
+      Directory = './a/'
+      Name = '.'
+      Path = {'a', '.'}
+      IsAbsolute = false
+      IsDirectory = true
+
+    ././//a/./
+      Same as for "a/"
+
+    a/../b
+      Directory = './a/../'
+      Name = 'b'
+      Path = {'a', '..', 'b'}
+      IsDirectory = false
+      IsAbsolute = false
+]]
 
 --[[
   2016-09
