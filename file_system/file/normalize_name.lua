6bf0c2e099395eefcaf1d0f4b7434014771cda5f
Ian <ian@somewhere.com>
2026-04-23 11:49:40 +0200

    [~] [file_system.file.normalize_name]

--- file_system/file/normalize_name.lua
+++ file_system/file/normalize_name.lua
@@ -1,14 +1,28 @@
+-- Normalize pathname
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-23
+]]
+
+-- Imports:
+local parse_pathname = request('!.concepts.path_name.parse')
+
 --[[
   Normalize string with Unix path-name.
 
   "/.////a" -> "a"
 ]]
-
-local parse_pathname = request('!.concepts.path_name.parse')
-
-return
+local normalize_name =
   function(path_name)
     local result = parse_pathname(path_name)
-    result = result.directory .. result.name
+    result = result.Directory .. result.Name
     return result
   end
+
+-- Export:
+return normalize_name
+
+--[[
+  2018-06
+]]
