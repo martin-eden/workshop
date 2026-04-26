8875b3ab0c04b585e84efa02f7f54b2efc80d22c
Ian <ian@somewhere.com>
2026-04-26 20:14:58 +0200

    [~.^] [tsv..get_next_rec] Core revisit and resync.

--- concepts/tsv/parse/specific/get_next_rec.lua
+++ concepts/tsv/parse/specific/get_next_rec.lua
@@ -1,11 +1,19 @@
-local trim_linefeed = request('^.^.^.^.string.trim_linefeed')
+-- Parses TSV line
 
-return
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-26
+]]
+
+-- Imports:
+local trim_tail_nls = request('!.string.trim_tail_nls')
+
+local get_next_rec =
   function(self)
     local result
     local line = self.lines_iterator:get_next_line()
     if line then
-      line = trim_linefeed(line)
+      line = trim_tail_nls(line)
       local pattern = '(.-)' .. '%' .. self.field_sep_char
       line = line .. self.field_sep_char
       result = {}
@@ -15,3 +23,10 @@ return
     end
     return result
   end
+
+-- Export:
+return get_next_rec
+
+--[[
+  2017-09
+]]
