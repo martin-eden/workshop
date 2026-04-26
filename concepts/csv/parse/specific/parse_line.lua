57c71328fcc408a28f1ae48e0ded3bbc8ec178f1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-26 20:15:48 +0200

    [~.^] [csv..parse_line] Code revisit and resync.

--- concepts/csv/parse/specific/parse_line.lua
+++ concepts/csv/parse/specific/parse_line.lua
@@ -1,11 +1,19 @@
+-- Parses CSV line
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-26
+]]
+
+-- Imports:
 local parse_record = request('parse_record')
 local fix_bad_line = request('^.^.fix_bad_line')
-local trim_linefeed = request('^.^.^.^.string.trim_linefeed')
+local trim_tail_nls = request('!.string.trim_tail_nls')
 
-return
+local parse_line =
   function(parent, s)
     assert_string(s)
-    s = trim_linefeed(s)
+    s = trim_tail_nls(s)
     local record, last_pos, has_problems = parse_record(parent, s)
     local is_unclosed_quote = has_problems and (last_pos > #s)
     if
@@ -20,3 +28,10 @@ return
     end
     return record, has_problems, is_unclosed_quote
   end
+
+-- Export:
+return parse_line
+
+--[[
+  2017-09
+]]
