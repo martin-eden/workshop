8ea5edb0b8a9c7e46d557c7cc4ccd7f18b5f8074
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-26 14:50:39 +0200

    [.^] [Lines.Interface] Source code clarification.

--- concepts/Lines/Interface.lua
+++ concepts/Lines/Interface.lua
@@ -2,100 +2,61 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-04-17
+  Last mod.: 2026-04-26
 ]]
 
--- "Line" is a string with newline. "String" is a line without newline.
-
 --[[
-  This class is about storing list of lines.
+  This class is about storing list of lines
 
   Lines are stored as strings (without newlines).
 
-  Text output facilities work with lines, not with multiline
-  strings. Among other things they want indentation. We're
-  providing indentation. They want add header or footer. We're
-  providing that.
+  Also we're providing convenience methods for indentation.
 ]]
 
--- Exports:
+-- Export:
 return
   {
     -- Import/export:
-
-    -- Import: Explode string to list of lines
     FromString = request('FromString'),
-
-    -- Export: Implode list of lines to string
     ToString = request('ToString'),
 
     -- Access:
 
-    -- Meta: get number lines
     GetNumLines = request('GetNumLines'),
 
-    -- MetaMeta: is one line?
     IsOneLine = request('IsOneLine'),
 
-    -- Access: get string at given line
     GetLineAt = request('GetLineAt'),
-
-    -- Access: set string at given line
     SetLineAt = request('SetLineAt'),
 
-    -- Meta: get first line string
     GetFirstLine = request('GetFirstLine'),
-
-    -- Meta: get last line string
     GetLastLine = request('GetLastLine'),
 
-    -- Modification:
-
-    -- Add string to start of list
-    AddFirstLine = request('AddFirstLine'),
-
-    -- Add string to end of list
-    AddLastLine = request('AddLastLine'),
-
-    -- Insert string before line at given index
     InsertLineBefore = request('InsertLineBefore'),
-
-    -- Insert string after line at given index
     InsertLineAfter = request('InsertLineAfter'),
 
-    -- Remove first line
-    RemoveFirstLine = request('RemoveFirstLine'),
+    AddFirstLine = request('AddFirstLine'),
+    AddLastLine = request('AddLastLine'),
 
-    -- Remove last line
-    RemoveLastLine = request('RemoveLastLine'),
-
-    -- Remove line
     RemoveLineAt = request('RemoveLineAt'),
 
+    RemoveFirstLine = request('RemoveFirstLine'),
+    RemoveLastLine = request('RemoveLastLine'),
+
     -- Indentation:
-
-    -- Indent
     Indent = request('Indent'),
-
-    -- Unindent
     Unindent = request('Unindent'),
 
     -- Internals:
 
-    -- Internal: list of lines
     Lines = {},
-
-    -- Config: indent chunk
     IndentChunk = '  ',
-
-    -- Internal: assert that value is valid index
     AssertValidIndex = request('AssertValidIndex'),
-
-    -- Internal: assert that value is string without newlines
     AssertValidValue = request('AssertValidValue'),
   }
 
 --[[
   2024-10-31
   2026-04-17
+  2026-04-26
 ]]
