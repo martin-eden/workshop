61bd73948db3ac5c4bf56450c8c2db8194300ac7
Ian <ian@somewhere.com>
2026-04-26 20:26:54 +0200

    [.^] [table.ordered_pass.default_comparator] Code revisit.

--- table/ordered_pass/default_comparator.lua
+++ table/ordered_pass/default_comparator.lua
@@ -1,33 +1,48 @@
-local val_rank =
+-- Default comparator for sorting generic tables
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-26
+]]
+
+local type_rank_map =
   {
-    string = 1,
-    number = 2,
+    ['string'] = 1,
+    ['number'] = 2,
     other = 3,
   }
 
-local comparable_types =
+local comparable_types_map =
   {
-    number = true,
-    string = true,
+    ['number'] = true,
+    ['string'] = true,
   }
 
-return
+local compare =
   function(a, b)
     local a_key = a.key
     local a_key_type = type(a_key)
-    local rank_a = val_rank[a_key_type] or val_rank.other
+    local rank_a = type_rank_map[a_key_type] or type_rank_map.other
 
     local b_key = b.key
     local b_key_type = type(b_key)
-    local rank_b = val_rank[b_key_type] or val_rank.other
+    local rank_b = type_rank_map[b_key_type] or type_rank_map.other
 
     if (rank_a ~= rank_b) then
       return (rank_a < rank_b)
     else
-      if comparable_types[a_key_type] and comparable_types[b_key_type] then
+      if comparable_types_map[a_key_type] and comparable_types_map[b_key_type] then
         return (a_key < b_key)
       else
         return (tostring(a_key) < tostring(b_key))
       end
     end
   end
+
+-- Export:
+return compare
+
+--[[
+  2016-09
+  2017-09
+]]
