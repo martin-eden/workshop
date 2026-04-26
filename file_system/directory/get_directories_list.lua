bb1de77cfee5d67fd1f4a59ce8780590ac314ed0
Ian <ian@somewhere.com>
2026-04-26 14:06:53 +0200

    [/] [file_system.directory.get_directories_list] No longer adds "/" ..
    Adding "/" at tail was used to distinguish string with directory name
    from string with file name.
    
    But we have separate methods to list files and to list directories.
    So outer code can keep that separation if it needs it. And output
    of methods can have entries with same format.

--- file_system/directory/get_directories_list.lua
+++ file_system/directory/get_directories_list.lua
@@ -2,30 +2,27 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-04-22
+  Last mod.: 2026-04-26
 ]]
 
 -- Imports:
 local get_cmd_listdirs = request('!.mechs.cmdline.get_cmd_listdirs')
 local get_program_output_lines = request('!.system.get_program_output_lines')
-local add_dir_postfix = request('!.string.file_name.add_dir_postfix')
 
 --[[
   Return directory names in base directory as list of strings
 
-  Each entry will contain base directory prefix.
-  Each entry will end with directory separator "/".
+  Each list entry is string with directory name with
+  following properties:
+
+    * Starts with base directory prefix
+    * Does not contain "/"
 ]]
 local get_dirs_list =
   function(base_dir)
     local cmd_get_files = get_cmd_listdirs(base_dir)
 
     local Dirs = get_program_output_lines(cmd_get_files)
-
-    -- Add directory separator at end of entries
-    for i = 1, #Dirs do
-      Dirs[i] = add_dir_postfix(Dirs[i])
-    end
 
     return Dirs
   end
@@ -36,4 +33,5 @@ return get_dirs_list
 --[[
   2017-08-11
   2026-04-22
+  2026-04-26
 ]]
