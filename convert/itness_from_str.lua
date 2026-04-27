35ed0788f10c7982d233c2d47185eece901042f2
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-27 18:33:36 +0200

    [.+] [convert:] Skeletons to Itness structure.

---
+++ convert/itness_from_str.lua
@@ -0,0 +1,19 @@
+-- Parse string as Itness table
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-27
+]]
+
+-- Itness from string
+local itness_from_string =
+  function(str)
+
+  end
+
+-- Export:
+return itness_from_string
+
+--[[
+  2026-04-27
+]]
---
+++ convert/itness_to_str.lua
@@ -0,0 +1,19 @@
+-- Compile Itness table to string
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-27
+]]
+
+-- Itness to string
+local itness_to_string =
+  function(Is)
+
+  end
+
+-- Export:
+return itness_to_string
+
+--[[
+  2026-04-27
+]]
