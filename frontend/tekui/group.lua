8a6667e0ed2009ee92f5c5666702631532276b52
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-28 15:04:42 +0200

    [.^] [tekui.group] Code revisit.

--- frontend/tekui/group.lua
+++ frontend/tekui/group.lua
@@ -1,26 +1,34 @@
--- Return group object
-
--- Last mod.: 2024-11-11
-
-local tui = require('tek.ui')
-local force_merge = request('!.table.merge_and_patch')
-
-return
-  function(header, overrides, ...)
-    local params =
-      force_merge(
-        {
-          Width = 'fill',
-          Height = 'auto',
-          Legend = header,
-          Children = {...},
-        },
-        overrides
-      )
-
-    return tui.Group:new(params)
-  end
+-- Create and return group object
 
 --[[
-  2020-02
+  Author: Martin Eden
+  Last mod.: 2026-04-27
+]]
+
+-- Imports:
+local TekUi = require('tek.ui')
+local force_merge = request('!.table.merge_and_patch')
+
+local create_group =
+  function(header, Overrides, ...)
+    local Params =
+      force_merge(
+        {
+          Legend = header,
+          Children = { ... },
+          Width = 'fill',
+          Height = 'auto',
+        },
+        Overrides
+      )
+
+    return TekUi.Group:new(Params)
+  end
+
+-- Export:
+return create_group
+
+--[[
+  2020 #
+  2026-04-27
 ]]
