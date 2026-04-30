76dfbb0f2371722dbcfe2740362c10f99f5118d6
Ian <ian@somewhere.com>
2026-04-30 15:08:41 +0200

    [.^] [rtc_ds3231.compile] Code revisit.

--- concepts/rtc_ds3231/compile.lua
+++ concepts/rtc_ds3231/compile.lua
@@ -1,32 +1,43 @@
---[[
-  Convert table with DS3231 values to array of bytes.
+-- Convert table with DS3231 values to array of bytes
 
-  Result data are supposed to be written to device.
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-04-30
 ]]
 
+-- Imports:
 local generic_convert = request('!.mechs.generic_convert')
 
-local stages =
+local Stages =
   {
     {
       verify = request('verificators.check_final_structure'),
       validate = request('validators.fix_final_structure'),
     },
-    {convert = request('converters.hours_denormalize')},
-    {convert = request('converters.year_pack')},
+    { convert = request('converters.hours_denormalize') },
+    { convert = request('converters.year_pack') },
     {
       verify = request('verificators.check_ranges'),
       validate = request('validators.constrain_number'),
       convert = request('converters.ints_to_bcds'),
     },
-    {convert = request('converters.serialize')},
+    { convert = request('converters.serialize') },
     {
       verify = request('verificators.check_masks'),
       validate = request('validators.apply_mask'),
     },
   }
 
-return
-  function(data, do_verify, do_validate)
-    return generic_convert(data, stages, do_verify, do_validate)
+local compile =
+  function(Data, do_verify, do_validate)
+    return generic_convert(Data, Stages, do_verify, do_validate)
   end
+
+-- Export:
+return compile
+
+--[[
+  2019 # # # # # # # # #
+  2020 # # # # #
+  2026-04-30
+]]
