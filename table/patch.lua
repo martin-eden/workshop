c81ee0d1692abddedf6c1bcedc4a0c6210248b32
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-30 15:55:12 +0200

    [^/~] [table.patch] Using [apply_table] ..
    [^] Code revisit
    [~] Uses [apply_table]
    [/] No longer explodes with error()
    [-] No longer accepts third argument to patch only different type values

--- table/patch.lua
+++ table/patch.lua
@@ -1,101 +1,47 @@
--- Apply patch to table
-
--- Last mod.: 2025-03-29
+-- Replace values to values from another table
 
 --[[
-  In-place table modification
-
-  Basically it means that we're writing every entity from patch table to
-  destination table.
-
-  Function returns nothing but may explode.
-
-  If there is no key in destination table, we'll explode:
-
-    ( { a = 'a'}, { a = 'a', b = 'b' } ) -> BOOM
-
-  Additional third parameter means that we're not overwriting
-  entity in destination table if it's value type is same as
-  in patch's entity.
-
-  That's useful when we want to force values to given types but
-  keep values if they have correct type:
-
-    ({ x = 42, y = '?' }, { x = 0, y = 0 }, false) -> { x = 0, y = 0 }
-    ({ x = 42, y = '?' }, { x = 0, y = 0 }, true) -> { x = 42, y = 0 }
-
-  Examples:
-
-    Always overwriting values:
-
-      ({ a = 'A' }, { a = '_A' }, false) -> { a = '_A' }
-
-    Overwriting values if different types:
-
-      ({ a = 'A' }, { a = '_A' }, true) -> { a = 'A' }
-      ({ a = 0 }, { a = '_A' }, true) -> { a = '_A' }
-
-    Nested values are supported:
-
-      ({ b = { bb = 'BB' } }, { b = { bb = '_BB' } }, false) ->
-      { b = { bb = '_BB' } }
-
-  See also:
-
-    table.merge
+  Author: Martin Eden
+  Last mod.: 2026-04-30
 ]]
 
-local Patch
-Patch =
-  function(MainTable, PatchTable, IfDifferentTypesOnly)
-    assert_table(MainTable)
+--[[
+  Existing values are overwritten:
+    { a = 'A'}, { a = 'X' } -> { a = 'X' }
 
-    if is_nil(PatchTable) then
+  New values are not added:
+    { a = 'A'}, { b = 'B' } -> { a = 'A' }
+]]
+
+-- Imports:
+local apply_table = request('apply_table')
+
+local patch
+patch =
+  function(Result, Additions)
+    assert_table(Result)
+
+    if is_nil(Additions) then
       return
     end
 
-    assert_table(PatchTable)
+    assert_table(Additions)
 
-    for PatchKey, PatchValue in pairs(PatchTable) do
-      local MainValue = MainTable[PatchKey]
+    local Rules =
+      {
+        { HasA = true, HasB = true, Action = 'use_b' },
+        { HasA = false, HasB = true, Action = 'use_a' },
+      }
 
-      -- Missing key in destination
-      if is_nil(MainValue) then
-        local ErrorMsg =
-          string.format(
-            [[Destination table doesn't have key "%s".]],
-            tostring(PatchKey)
-          )
-
-        error(ErrorMsg, 2)
-      end
-
-      local DoPatch = true
-
-      if IfDifferentTypesOnly then
-        MainValueType = type(MainValue)
-        PatchValueType = type(PatchValue)
-        DoPatch = (MainValueType ~= PatchValueType)
-      end
-
-      if DoPatch then
-        -- Recursive call when we're writing table to table
-        if is_table(MainValue) and is_table(PatchValue) then
-          Patch(MainValue, PatchValue, IfDifferentTypesOnly)
-        -- Else just overwrite value
-        else
-          MainTable[PatchKey] = PatchValue
-        end
-      end
-    end
+    apply_table(Result, Additions, Rules)
   end
 
 -- Exports:
-return Patch
+return patch
 
 --[[
-  2016-09
-  2024-02
-  2024-11
-  2025-03-29
+  2016 #
+  2024 # #
+  2025 #
+  2026-04-30
 ]]
