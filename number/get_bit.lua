ddac72e31a7374b2b8dd0e26359af640e36bc5e8
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-03 23:24:41 +0200

    [.^] [number.get_bit] Code revisit ..
    I tried to make it return int, not bool. But reverted back.
    
    get_bit() should return bool. Because it's mostly used for bit flags.

--- number/get_bit.lua
+++ number/get_bit.lua
@@ -1,6 +1,20 @@
+-- Get bit from given integer
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-04
+]]
+
+--[[
+  Result structure
+
+    boolean
+]]
+
+-- Imports:
 local assert_bit_offs = request('assert_bit_offs')
 
-return
+local get_bit =
   function(n, bit_offs)
     assert_integer(n)
     assert_integer(bit_offs)
@@ -8,3 +22,12 @@ return
 
     return (n & (1 << bit_offs) ~= 0)
   end
+
+-- Export:
+return get_bit
+
+--[[
+  2019
+  2026-05-03
+  2026-05-04
+]]
