638467f8594d38f0c551312beb996d754f2ead45
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-04 16:49:15 +0200

    [+] [list.add_item] I'll write note why I'm doing this.

---
+++ concepts/list/add_item.lua
@@ -0,0 +1,18 @@
+-- Add item to list
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-04
+]]
+
+local add_item =
+  function(OurList, item)
+    table.insert(OurList, item)
+  end
+
+-- Export:
+return add_item
+
+--[[
+  2026-05-04
+]]
