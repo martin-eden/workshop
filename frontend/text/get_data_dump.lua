758dcd9532eb28f65aded861f49d5e9b5d52c624
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-04 21:30:46 +0200

    [^/] [frontend.text.get_data_dump] Better output, signature changed ..
    [/] [text.get_data_dump] Now requires string, not list of bytes
    [^~] [text.get_data_dump] Using [bits_from_str] instead of smart code
    [/] Printing offset, not position

--- frontend/text/get_data_dump.lua
+++ frontend/text/get_data_dump.lua
@@ -1,27 +1,34 @@
--- Receives table with list of bytes. Returns string with text dump
+-- Receives string with data. Returns string with text dump
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-04-29
+  Last mod.: 2026-05-04
 ]]
 
---[=[
+--[[
   Example
 
-  { 66, 9, 22 } ->
-     Pos | Dec | BCD | Hex | Bin
+  string.char(66, 233, 22)
+    ->
+    Offs | Dec | BCD | Hex | Bin
     -----------------------------------------
-      01 | 066 |  42 |  42 | . X . . . . X .
-      02 | 009 |  09 |  09 | . . . . X . . X
-      03 | 022 |  16 |  16 | . . . X . X X .
+     000 | 066 |  42 |  42 | . X . . . . X .
+     001 | 233 |  ?? |  E9 | X X X . X . . X
+     002 | 022 |  16 |  16 | . . . X . X X .
     -----------------------------------------
-]=]
+]]
 
 -- Imports:
 local assert_byte = request('!.number.assert_byte')
 local from_bcd = request('!.number.from_bcd')
+local bits_from_str = request('!.convert.bits_from_str')
 local glue_words = request('!.concepts.words.to_string')
 local Lines = request('!.concepts.Lines.Interface')
+
+local to_dec_str =
+  function(byte)
+    return string.format('%03d', byte)
+  end
 
 local to_hex_str =
   function(byte)
@@ -45,52 +52,43 @@ local to_bcd_str =
 
 local to_bit_str =
   function(byte)
-    local BitChar_Map = { [0] = '.', [1] = 'X' }
-    local num_bits = 8
-    local Bits = {}
+    local result
 
-    for i = 1, num_bits do
-      Bits[num_bits - i + 1] = BitChar_Map[(byte >> (i - 1)) & 1]
-    end
+    result = bits_from_str(string.char(byte))
+    result = string.reverse(result)
+    result = string.gsub(result, '.', function(c) return c .. ' ' end)
 
-    local result_str = glue_words(Bits)
-    -- result_str = result_str:reverse()
-
-    return result_str
-  end
-
-local to_dec_str =
-  function(byte)
-    return string.format('%03d', byte)
+    return result
   end
 
 local get_dump =
-  function(Data)
-    assert_table(Data)
+  function(str)
+    assert_string(str)
+
+    local table_width = 41
 
     local Lines = new(Lines)
 
     local format_str = '%4s | %3s | %3s | %3s | %-15s'
 
     local header_str =
-      string.format(format_str, 'Pos', 'Dec', 'BCD', 'Hex', 'Bin')
+      string.format(format_str, 'Offs', 'Dec', 'BCD', 'Hex', 'Bin')
 
     Lines:AddLastLine(header_str)
 
-    local last_line_len = #Lines:GetLastLine()
-    local delimiter_str = string.rep('-', last_line_len + 1)
+    local table_separator = string.rep('-', table_width)
 
-    Lines:AddLastLine(delimiter_str)
+    Lines:AddLastLine(table_separator)
 
-    for pos = 1, #Data do
-      local byte = Data[pos]
+    for pos = 1, string.len(str) do
+      local byte = string.byte(string.sub(str, pos, pos))
 
       assert_byte(byte)
 
       local data_str =
         string.format(
           format_str,
-          string.format('%02d', pos),
+          to_dec_str(pos - 1),
           to_dec_str(byte),
           to_bcd_str(byte),
           to_hex_str(byte),
@@ -100,7 +98,7 @@ local get_dump =
       Lines:AddLastLine(data_str)
     end
 
-    Lines:AddLastLine(delimiter_str)
+    Lines:AddLastLine(table_separator)
 
     local result_str = Lines:ToString()
 
@@ -114,4 +112,5 @@ return get_dump
   2019
   2022
   2026-04-29
+  2026-05-04
 ]]
