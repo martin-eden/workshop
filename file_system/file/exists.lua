589c577a567b03d96607feb2382f93c0d81fa8ef
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-04 20:12:55 +0200

    [^] [file.exists] Code revisit. Normalizing name.

--- file_system/file/exists.lua
+++ file_system/file/exists.lua
@@ -1,9 +1,34 @@
-return
-  function(file_name)
-    local file_handle = io.open(file_name, 'r')
-    local result = (file_handle ~= nil)
+-- Check for file (or directory) presence
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-04
+]]
+
+-- Imports:
+local normalize_name = request('normalize_name')
+
+local pathname_exists =
+  function(pathname)
+    assert_string(pathname)
+
+    pathname = normalize_name(pathname)
+
+    local file = io.open(pathname, 'rb')
+
+    local result = not is_nil(file)
+
     if result then
-      io.close(file_handle)
+      file:close()
     end
+
     return result
   end
+
+-- Export:
+return pathname_exists
+
+--[[
+  2016
+  2026-05-04
+]]
