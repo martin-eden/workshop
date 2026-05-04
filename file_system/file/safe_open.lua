3456f0f9006e7fba4f9b1417c05598ec56cef638
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-04 19:51:27 +0200

    [^] [file.safe_open] Code revisit. Normalizing name.

--- file_system/file/safe_open.lua
+++ file_system/file/safe_open.lua
@@ -1,15 +1,43 @@
--- Open file and return handle or throw an error.
+-- Open file and return handle or throw an error
 
--- Last mod.: 2017-10-12
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-04
+]]
 
-return
-  function(file_name, mode)
-    assert_string(file_name)
-    mode = mode or 'rb'
-    assert_string(mode)
-    local f_object, err_msg = io.open(file_name, mode)
-    if not f_object then
+--[[
+  Contract is that this function returns file handle or explodes
+
+  No pesky ( nil error_msg ) from stock io.open().
+]]
+
+-- Imports:
+local normalize_name = request('normalize_name')
+
+local default_mode = 'rb'
+
+local open_file =
+  function(pathname, mode)
+    assert_string(pathname)
+    assert(is_nil(mode) or is_string(mode))
+
+    pathname = normalize_name(pathname)
+    mode = mode or default_mode
+
+    local file, err_msg = io.open(pathname, mode)
+
+    if not file then
       error(err_msg, 2)
     end
-    return f_object
+
+    return file
   end
+
+-- Export:
+return open_file
+
+--[[
+  2016 #
+  2017 #
+  2026-05-04
+]]
