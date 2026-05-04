d2b5c98fbda6fb73cd12c14044ab1d1e14bdccd7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-04 19:26:41 +0200

    [^] [file.create] Code revisit. Using [normalize_name].

--- file_system/file/create.lua
+++ file_system/file/create.lua
@@ -1,21 +1,37 @@
 -- Create file with given pathname and contents
 
 --[[
-  Design version: 2
-  Last mod.: 2024-02-17
+  Author: Martin Eden
+  Last mod.: 2026-05-04
 ]]
 
-return
-  function(PathName, Contents)
-    assert_string(PathName)
-    assert_string(Contents)
+-- Imports:
+local normalize_name = request('normalize_name')
 
-    local FileMode = 'wb'
-    local File, ErrorMsg = io.open(PathName, FileMode)
-    if not File then
-      error(ErrorMsg)
+local file_mode = 'wb'
+
+local create_file =
+  function(pathname, contents)
+    assert_string(pathname)
+    assert_string(contents)
+
+    pathname = normalize_name(pathname)
+
+    local file, error_str = io.open(pathname, file_mode)
+
+    if not file then
+      error(error_str)
     end
-    File:write(Contents)
 
-    File:close()
+    file:write(contents)
+
+    file:close()
   end
+
+-- Export:
+return create_file
+
+--[[
+  2024
+  2026-05-04
+]]
