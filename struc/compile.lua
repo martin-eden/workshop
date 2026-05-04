e2a424d8c62655eedfdea19de8908920eea9bf21
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-04 17:13:51 +0200

    [^] [struc.compile] Code restyled and documented.

--- struc/compile.lua
+++ struc/compile.lua
@@ -1,6 +1,11 @@
 -- Exotic table-to-string conversion
 
 --[[
+  Author: Martin Eden
+  Last mod.: 2026-05-04
+]]
+
+--[[
   Input
 
     Node: string or table
@@ -10,20 +15,46 @@
     NodeHandlers: table of functions
       key: <Node.type>
 
-  Idea is to
+  Idea is to call serializing functions when <.type> is set:
 
-    compile(
-      { 'A', {type = 'Special', Value = 'X'}}, 'B' },
-      { Special = function(Node) return ' -= ' .. Node.Value .. ' =- '}
-    ) -> 'A -= X =- B'
+    compile
+      { 'A', { type = 'Special', Value = 'X' } }, 'B' },
+      { Special = function(Node) return ' -= ' .. Node.Value .. ' =- ' end }
+      ->
+      'A -= X =- B'
 ]]
 
--- Last mod.: 2026-04-17
+-- Imports:
+local add_to_list = request('!.concepts.list.add_item')
+local list_to_string = request('!.concepts.list.to_string')
 
-local compile_core = request('compile_core')
-local ListToString = request('!.concepts.list.to_string')
+-- Execute node handlers for tree nodes. Collect results
+local compile
+compile =
+  function(Node, NodeHandlers, Result)
+    if is_string(Node) then
+      add_to_list(Result, Node)
+    elseif is_table(Node) then
+      local node_handler = NodeHandlers[Node.type]
 
-return
+      if Node.type and not node_handler then
+        local msg =
+          string.format('No node handler for type "%s".', Node.type)
+
+        io.stderr:write(msg)
+      end
+
+      if node_handler then
+        add_to_list(Result, node_handler(Node))
+      else
+        for idx, SubNode in ipairs(Node) do
+          compile(SubNode, NodeHandlers, Result)
+        end
+      end
+    end
+  end
+
+local compile_root =
   function(Node, NodeHandlers)
     if is_string(Node) then
       return Node
@@ -31,21 +62,22 @@ return
       assert_table(Node)
     end
 
-    NodeHandlers = NodeHandlers or {}
+    NodeHandlers = NodeHandlers or { }
     assert_table(NodeHandlers)
 
-    local Result = {}
+    local Result = { }
 
-    compile_core(Node, NodeHandlers, Result)
-    Result = ListToString(Result)
+    compile(Node, NodeHandlers, Result)
 
-    return Result
+    return list_to_string(Result)
   end
 
+-- Export:
+return compile_root
+
 --[[
-  2017-02-13
-  2017-05-09
-  2017-08-27
-  2018-08-08
-  2024-10-21
+  2017 # # #
+  2018 #
+  2024 #
+  2026-05-04
 ]]
