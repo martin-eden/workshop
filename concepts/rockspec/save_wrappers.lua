69c498980c4bef4d32471ca43bd0a1f3f084ca44
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-05 19:14:51 +0200

    [^] [rockspec.save_wrappers] Code revisit.

--- concepts/rockspec/save_wrappers.lua
+++ concepts/rockspec/save_wrappers.lua
@@ -1,11 +1,29 @@
+-- [Luarocks] Create wrappers texts and save them to files
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-05
+]]
+
+-- Imports:
 local get_wrappers = request('get_wrappers')
 local save_to_file = request('!.string.save_to_file')
 
-return
-  function(cfg)
-    assert_table(cfg.commands)
-    local wrappers = get_wrappers(cfg)
-    for i, rec in ipairs(cfg.commands) do
-      save_to_file(rec.wrapper, wrappers[i])
+local save_wrappers =
+  function(Config)
+    assert_table(Config.commands)
+
+    local Wrappers = get_wrappers(Config)
+
+    for idx, Rec in ipairs(Config.commands) do
+      save_to_file(Rec.wrapper, Wrappers[idx])
     end
   end
+
+-- Export:
+return save_wrappers
+
+--[[
+  2018
+  2026-05-05
+]]
