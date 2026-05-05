08ca03d01b38436b62a53258f506b69a5a2134b9
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-05 18:59:03 +0200

    [.] [convert.file_from_str]

--- convert/file_from_str.lua
+++ convert/file_from_str.lua
@@ -2,19 +2,19 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-04
+  Last mod.: 2026-05-05
 ]]
 
 -- Imports:
 local file_as_str = request('!.file_system.file.as_string')
 
-local load_str_from_file =
+local file_from_str =
   function(file_name)
     return file_as_str(file_name)
   end
 
 -- Export:
-return load_str_from_file
+return file_from_str
 
 --[[
   // similar code was at 201x's
