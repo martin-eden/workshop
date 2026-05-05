78b41362b88c706e2d575d4b8cf3b350ce1cd100
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-05 20:23:28 +0200

    [.+] [RangesTree.Interface] Okay, added remained free functions.

--- concepts/RangesTree/Interface.lua
+++ concepts/RangesTree/Interface.lua
@@ -9,6 +9,9 @@ local Interface =
   {
     create_range = request('Range.create'),
     create_ranges_tree = request('RangesTree.create'),
+    create_bits_value = request('BitsValue.create'),
+    create_string_value = request('StringValue.create'),
+    apply_ranges = request('apply_ranges'),
   }
 
 -- Export:
