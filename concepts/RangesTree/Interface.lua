b6d74edb2102e4e291d4455b9861169a1ec57652
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-05 18:56:01 +0200

    [+] [RangesTree.Interface] Interface to ranges tree ..
    Convenience wrapper to not request() commonly needed free functions.

---
+++ concepts/RangesTree/Interface.lua
@@ -0,0 +1,19 @@
+-- Interface to Ranges Tree
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-05
+]]
+
+local Interface =
+  {
+    create_range = request('Range.create'),
+    create_ranges_tree = request('RangesTree.create'),
+  }
+
+-- Export:
+return Interface
+
+--[[
+  2026-05-05
+]]
