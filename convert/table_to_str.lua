a8fd38a85e44f3c2ee5e47522745f49eeaab4ad0
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-05 20:00:40 +0200

    [+] [convert.table_to_str]

---
+++ convert/table_to_str.lua
@@ -0,0 +1,13 @@
+-- Save table to string with Lua table definition
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-05
+]]
+
+-- Export:
+return request('!.concepts.lua_table.save')
+
+--[[
+  2026-05-05
+]]
