378f3e460dbd7fb615ed3e45becb7e46f55bef0c
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-05 19:45:36 +0200

    [+] [convert.table_from_str]

---
+++ convert/table_from_str.lua
@@ -0,0 +1,13 @@
+-- Return Lua table from string with Lua code
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-05
+]]
+
+-- Export:
+return request('!.concepts.lua_table.load')
+
+--[[
+  2026-05-05
+]]
