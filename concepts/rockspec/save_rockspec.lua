03b48c8505a30d743c80fe54acb437675db74457
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-05 19:08:48 +0200

    [^] [rockspec.save_rockspec] Code revisit.

--- concepts/rockspec/save_rockspec.lua
+++ concepts/rockspec/save_rockspec.lua
@@ -1,7 +1,23 @@
+-- [LuaRocks] Create rockspec text and save it to file
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-05
+]]
+
+-- Imports:
 local get_rockspec = request('get_rockspec')
 local save_to_file = request('!.string.save_to_file')
 
-return
-  function(cfg)
-    save_to_file(cfg.rockspec_name, get_rockspec(cfg))
+local save_rockspec =
+  function(Config)
+    save_to_file(Config.rockspec_name, get_rockspec(Config))
   end
+
+-- Export:
+return save_rockspec
+
+--[[
+  2018 # #
+  2026-05-05
+]]
