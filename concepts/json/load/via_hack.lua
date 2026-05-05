ac3fc3e6401b6a192fca0e71cbb091f47234f6f7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-05 19:50:30 +0200

    [^] [json.load.via_hack] Code revisit.

--- concepts/json/load/via_hack.lua
+++ concepts/json/load/via_hack.lua
@@ -1,14 +1,39 @@
+-- Parse JSON
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-05
+]]
+
+--[[
+  Implementation adjusts JSON string to string with Lua table
+  definition and executes that string as code.
+
+  Hacky: fast, simple, dirty, dangerous.
+]]
+
+-- Imports:
 local str_as_lua_table = request('^.^.lua_table.load')
 
-return
+local parse_json =
   function(json_str)
     if not is_string(json_str) then
       return
     end
+
     local result = json_str
-    result = result:gsub('%[', '{')
-    result = result:gsub('%]', '}')
-    result = result:gsub('"([^"]+)"%s*:', '["%1"]=')
+    result = string.gsub(result, '%[', '{')
+    result = string.gsub(result, '%]', '}')
+    result = string.gsub(result, '"([^"]+)"%s*:', '["%1"]=')
     result = str_as_lua_table(result)
+
     return result
   end
+
+-- Export:
+return parse_json
+
+--[[
+  2017
+  2026-05-05
+]]
