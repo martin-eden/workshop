c4e2d6348e13d6ff3946a1d7ea659821faaf0abf
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-08 00:18:11 +0200

    [?+] [RangesTree.StringValue.Methods] Experimental method ..
    Added method to set value from input value and range.
    
    So if we have string value "abc" and given "ghi" with range ( 7 3 )
    then result value should be like "abc...ghi".
    
    Problem is that space-filling character. I need to think how to
    specify it.
    
    No changes to BitsValue container yet. But it needs similar
    functionality. My code already compiles ranges data tree to one item.

--- concepts/RangesTree/StringValue/Methods.lua
+++ concepts/RangesTree/StringValue/Methods.lua
@@ -27,8 +27,28 @@ return
       function(Me, Data, DataRange)
         Me.Value = Me.Value .. Data:GetRangeValue(DataRange)
       end,
+    Set =
+      function(Me, Data, DataRange)
+        --[[
+          If range is out of our string, we will extend string with
+          dummy char.
+        ]]
+        local dummy_char = '-'
+        local value_len = string.len(Me.Value)
+        local range_end_pos = DataRange:GetStop()
+        if (value_len < range_end_pos) then
+          local delta_len = range_end_pos - value_len
+          local dummy_str = string.rep(dummy_char, delta_len)
+          Me.Value = Me.Value .. dummy_str
+        end
+        Me.Value =
+          string.sub(Me.Value, 1, DataRange:GetStart() - 1) ..
+          string.sub(Data:GetValue(), 1, DataRange:GetLength()) ..
+          string.sub(Me.Value, DataRange:GetStop() + 1, value_len)
+      end,
   }
 
 --[[
   2026-05-02
+  2026-05-07
 ]]
