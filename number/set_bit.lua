8923bafa343e6e39d778e8cec029b54ed3e40624
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-07 22:10:13 +0200

    [^] [number.set_bit] Code revisit.

--- number/set_bit.lua
+++ number/set_bit.lua
@@ -1,13 +1,28 @@
+-- Set bit in given integer
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-07
+]]
+
+--[[
+  Input structure
+
+    [i] -- source integer
+    [i] -- bit offset
+    [b] -- bit value
+]]
+
+-- Imports:
 local assert_bit_offs = request('assert_bit_offs')
 
-return
-  function(n, bit_offs, v)
+local set_bit =
+  function(n, bit_offs, bit_val)
     assert_integer(n)
-    assert_integer(bit_offs)
     assert_bit_offs(bit_offs)
-    assert_boolean(v)
+    assert_boolean(bit_val)
 
-    if v then
+    if bit_val then
       n = n | (1 << bit_offs)
     else
       n = n & ~(1 << bit_offs)
@@ -15,3 +30,11 @@ return
 
     return n
   end
+
+-- Export:
+return set_bit
+
+--[[
+  2019
+  2026-05-07
+]]
