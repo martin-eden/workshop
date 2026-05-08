92139cc9bae689ea75b9804d8d20971f3df838aa
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-08 20:10:42 +0200

    [^] [table.get_keys] Code revisit.

--- table/get_keys.lua
+++ table/get_keys.lua
@@ -1,9 +1,30 @@
-return
-  function(t)
-    assert_table(t)
-    local result = {}
-    for k, v in pairs(t) do
-      result[#result + 1] = k
+-- Return list with table keys
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-08
+]]
+
+-- Imports:
+local add_to_list = request('!.concepts.list.add_item')
+
+local get_keys =
+  function(Table)
+    assert_table(Table)
+
+    local Keys = { }
+
+    for key in pairs(Table) do
+      add_to_list(Keys, key)
     end
-    return result
+
+    return Keys
   end
+
+-- Export:
+return get_keys
+
+--[[
+  2016
+  2026-05-08
+]]
