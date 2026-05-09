c8cbfc5df74d62dfa521bdda33cfdc2e5bdd32e5
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-09 23:59:52 +0200

    [.^] [number.splice_bits] Code revisit.

--- number/splice_bits.lua
+++ number/splice_bits.lua
@@ -1,33 +1,52 @@
 -- Insert given integer into specified bit range.
 
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-09
+]]
+
+-- Imports:
 local assert_bit_offs = request('assert_bit_offs')
 
-return
+local splice_bits =
   function(v, start_offs, end_offs, n)
     n = is_nil(n) and 0 or n
+
     assert_integer(n)
+
     assert_integer(v)
     assert(v >= 0)
-    assert_integer(start_offs)
+
     assert_bit_offs(start_offs)
-    assert_integer(end_offs)
     assert_bit_offs(end_offs)
     assert(start_offs <= end_offs)
+
     if ((v << start_offs) > (1 << (end_offs + 1))) then
       error(
-        ('Value 0x%X is too large to fit given bit range [%d, %d].'):
-        format(v, start_offs, end_offs)
+        string.format(
+          'Value 0x%X is too large to fit given bit range [%d, %d].',
+          v, start_offs, end_offs
+        )
       )
     end
 
+    -- Example for ( start_offs: 2, end_offs: 5 )
     local mask
-    -- start_offs: 2, end_offs: 5
+    -- mask: 00111111
     mask = (1 << (end_offs + 1)) - 1
-    -- mask: 111111
+    -- mask: 00111100
     mask = mask & ~((1 << start_offs) - 1)
-    -- mask: 111100
+    -- mask: 00000011
     mask = ~mask
-    -- mask: 000011
 
     return (n & mask) | (v << start_offs)
   end
+
+-- Export:
+return splice_bits
+
+--[[
+  2019
+  2020
+  2026-05-09
+]]
