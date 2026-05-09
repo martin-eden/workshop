6330e62ddb6b008a17ee8e9d328df6313ab94d43
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-09 20:25:43 +0200

    [~] [codec_ds3231.Compile]

--- concepts/codec_ds3231/Compile.lua
+++ concepts/codec_ds3231/Compile.lua
@@ -2,25 +2,29 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-08
+  Last mod.: 2026-05-09
 ]]
 
 -- Imports:
 local ds3231_values_to_bits = request('Internals.ds3231_values_to_bits')
+local StringValue = request('!.concepts.RangesTree.StringValue.Interface')
 local fold_tree = request('!.concepts.RangesTree.fold_tree')
 local bits_to_bytes = request('!.convert.bits_to_bytes')
 local bytes_to_str = request('!.convert.bytes_to_str')
 
-local create_string_val = request('!.concepts.RangesTree.StringValue.create')
-
 local Compile =
   function(Me, Ds3231_Data)
+    local default_bit_char = '.'
+
     assert_table(Ds3231_Data)
 
     local BitsDataTree = ds3231_values_to_bits(Ds3231_Data)
 
+    local OutputStr = StringValue.create()
+    OutputStr:SetDefaultItemValue(default_bit_char)
+
     local data_bits =
-      fold_tree(BitsDataTree, Me.Bitfields, create_string_val)
+      fold_tree(BitsDataTree, Me.Bitfields, OutputStr)
 
     local DataBytes = bits_to_bytes(data_bits)
 
