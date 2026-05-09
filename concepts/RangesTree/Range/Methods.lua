6db1c698755ed94c20c1d361934345c30d47f982
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-09 22:47:42 +0200

    [>] [RangesTree.Range.Methods:] Incorporated create() function.

--- concepts/RangesTree/Range/Methods.lua
+++ concepts/RangesTree/Range/Methods.lua
@@ -2,7 +2,7 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-03
+  Last mod.: 2026-05-09
 ]]
 
 --[[
@@ -12,8 +12,12 @@
     Length [i]
 ]]
 
--- Export:
-return
+-- Imports:
+local is_natural_num = request('!.number.is_natural')
+local attach_methods = request('!.table.attach_methods')
+
+local Interface
+Interface =
   {
     GetStart =
       function(Range)
@@ -27,10 +31,26 @@ return
       function(Range)
         return Range.Length
       end,
+
+    create =
+      function(start, length)
+        assert(is_natural_num(start))
+        assert(is_natural_num(length))
+
+        local Range = { Start = start, Length = length }
+
+        attach_methods(Range, Interface)
+
+        return Range
+      end
   }
+
+-- Export:
+return Interface
 
 --[[
   2026-04-30
   2026-05-01
   2026-05-03
+  2026-05-09
 ]]
