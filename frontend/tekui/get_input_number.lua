582e24cadfcae2103dedb059d038368f12b7dc63
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-10 21:53:13 +0200

    [^/] [frontend.tekui.get_input_number] Code revisit, contract change ..
    Now in case of non-numeric text returns zero, not nil.

--- frontend/tekui/get_input_number.lua
+++ frontend/tekui/get_input_number.lua
@@ -1,12 +1,26 @@
---[[
-  Get numeric value of input box addressed by id.
+-- Get numeric value of input box addressed by id
 
-  In case of non-numeric text returns nil.
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-10
 ]]
 
+--[[
+  In case of non-numeric text returns zero
+]]
+
+-- Imports:
 local get_input_text = request('get_input_text')
 
-return
+local get_input_number =
   function(app, input_box_id)
-    return tonumber(get_input_text(app, input_box_id))
+    return tonumber(get_input_text(app, input_box_id)) or 0
   end
+
+-- Export:
+return get_input_number
+
+--[[
+  2020
+  2026-05-10
+]]
