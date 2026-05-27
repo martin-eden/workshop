73a13333f84f53637af018347a280f98d400b294
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-28 01:21:11 +0200

    [.^] [concepts.List] Structural simplification for performance reasons.

--- concepts/List/Interface.lua
+++ concepts/List/Interface.lua
@@ -46,197 +46,204 @@
 ]]
 
 -- Imports:
-local merge_table = request('!.table.merge')
 local create_instance = request('!.table.create_instance')
-
-local Meta_Level_1 =
-  {
-    GetFirstIndex =
-      function(Items)
-        return 1
-      end,
-
-    GetLastIndex =
-      function(Items)
-        return #Items
-      end,
-
-    IsValidItem =
-      function(Item)
-        return not is_nil(Item)
-      end,
-  }
-
-local Meta_Level_2 =
-  {
-    GetNumItems =
-      function(Items)
-        return
-          1 +
-          (
-            Meta_Level_1.GetLastIndex(Items) -
-            Meta_Level_1.GetFirstIndex(Items)
-          )
-      end,
-
-    IsValidIndex =
-      function(Items, index)
-        return
-          is_integer(index) and
-          (index >= Meta_Level_1.GetFirstIndex(Items)) and
-          (index <= Meta_Level_1.GetLastIndex(Items))
-      end,
-
-    AssertValidItem =
-      function(Item)
-        assert(Meta_Level_1.IsValidItem(Item), 'Invalid item')
-      end,
-  }
-
-local Meta_Level_3 =
-  {
-    IsEmpty =
-      function(Items)
-        return (Meta_Level_2.GetNumItems(Items) == 0)
-      end,
-
-    AssertValidIndex =
-      function(Items, index)
-        assert(Meta_Level_2.IsValidIndex(Items, index), 'Invalid index')
-      end,
-  }
-
-local Meta_Level_4 =
-  {
-    AssertNotEmpty =
-      function(Items)
-        assert(not Meta_Level_3.IsEmpty(Items), 'List is empty')
-      end,
-  }
-
-local Access_Core =
-  {
-    GetAt =
-      function(Items, index)
-        return Items[index]
-      end,
-
-    InsertBefore =
-      function(Items, index, Item)
-        table.insert(Items, index, Item)
-      end,
-
-    InsertAfter =
-      function(Items, index, Item)
-        table.insert(Items, index + 1, Item)
-      end,
-
-    RemoveAt =
-      function(Items, index)
-        table.remove(Items, index)
-      end,
-  }
-
-local Access_Level_1 =
-  {
-    GetAt =
-      function(Items, index)
-        Meta_Level_3.AssertValidIndex(Items, index)
-
-        return Access_Core.GetAt(Items, index)
-      end,
-
-    InsertBefore =
-      function(Items, index, Item)
-        Meta_Level_3.AssertValidIndex(Items, index)
-        Meta_Level_2.AssertValidItem(Item)
-
-        Access_Core.InsertBefore(Items, index, Item)
-      end,
-
-    InsertAfter =
-      function(Items, index, Item)
-        Meta_Level_3.AssertValidIndex(Items, index)
-        Meta_Level_2.AssertValidItem(Item)
-
-        Access_Core.InsertAfter(Items, index, Item)
-      end,
-
-    RemoveAt =
-      function(Items, index)
-        Meta_Level_3.AssertValidIndex(Items, index)
-
-        Access_Core.RemoveAt(Items, index)
-      end,
-  }
-
-local Access_Level_2 =
-  {
-    GetFirst =
-      function(Items)
-        return
-          Access_Level_1.GetAt(Items, Meta_Level_1.GetFirstIndex(Items))
-      end,
-
-    GetLast =
-      function(Items)
-        return
-          Access_Level_1.GetAt(Items, Meta_Level_1.GetLastIndex(Items))
-      end,
-
-    InsertFirst =
-      function(Items, item)
-        Access_Level_1.InsertBefore(Items, Meta_Level_1.GetFirstIndex(Items), item)
-      end,
-
-    InsertLast =
-      function(Items, item)
-        Access_Level_1.InsertAfter(Items, Meta_Level_1.GetLastIndex(Items), item)
-      end,
-
-    RemoveFirst =
-      function(Items)
-        Access_Level_1.RemoveAt(Items, Meta_Level_1.GetFirstIndex(Items))
-      end,
-
-    RemoveLast =
-      function(Items)
-        Access_Level_1.RemoveAt(Items, Meta_Level_1.GetLastIndex(Items))
-      end,
-  }
-
-local Access_Level_3 =
-  {
-    Get =
-      function(Items)
-        return Access_Level_2.GetFirst(Items)
-      end,
-
-    Insert =
-      function(Items, item)
-        return Access_Level_2.InsertLast(Items, item)
-      end,
-
-    Remove =
-      function(Items)
-        return Access_Level_2.RemoveFirst(Items)
-      end,
-  }
 
 local Interface = { }
 
-merge_table(Interface, Meta_Level_1)
-merge_table(Interface, Meta_Level_2)
-merge_table(Interface, Meta_Level_3)
-merge_table(Interface, Meta_Level_4)
-merge_table(Interface, Access_Level_1)
-merge_table(Interface, Access_Level_2)
-merge_table(Interface, Access_Level_3)
+-- ( Meta Level 1
+local GetFirstIndex =
+  function(Items)
+    return 1
+  end
 
-Interface.create =
+local GetLastIndex =
+  function(Items)
+    return #Items
+  end
+
+local IsValidItem =
+  function(Item)
+    return not is_nil(Item)
+  end
+
+Interface.GetFirstIndex = GetFirstIndex
+Interface.GetLastIndex = GetLastIndex
+Interface.IsValidItem = IsValidItem
+-- )
+
+-- ( Meta Level 2
+local GetNumItems =
+  function(Items)
+    return 1 + (GetLastIndex(Items) - GetFirstIndex(Items))
+  end
+
+local IsValidIndex =
+  function(Items, index)
+    return
+      is_integer(index) and
+      (index >= GetFirstIndex(Items)) and
+      (index <= GetLastIndex(Items))
+  end
+
+local AssertValidItem =
+  function(Item)
+    assert(IsValidItem(Item), 'Invalid item')
+  end
+
+Interface.GetNumItems = GetNumItems
+Interface.IsValidIndex = IsValidIndex
+Interface.AssertValidItem = AssertValidItem
+-- )
+
+-- ( Meta Level 3
+local IsEmpty =
+  function(Items)
+    return (GetNumItems(Items) == 0)
+  end
+
+local AssertValidIndex =
+  function(Items, index)
+    assert(IsValidIndex(Items, index), 'Invalid index')
+  end
+
+Interface.IsEmpty = IsEmpty
+Interface.AssertValidIndex = AssertValidIndex
+-- )
+
+-- ( Meta_Level_4
+local AssertNotEmpty =
+  function(Items)
+    assert(not IsEmpty(Items), 'List is empty')
+  end
+
+Interface.AssertNotEmpty = AssertNotEmpty
+-- )
+
+-- ( Access Core
+local GetAt =
+  function(Items, index)
+    return Items[index]
+  end
+
+local InsertBefore =
+  function(Items, index, Item)
+    table.insert(Items, index, Item)
+  end
+
+local InsertAfter =
+  function(Items, index, Item)
+    table.insert(Items, index + 1, Item)
+  end
+
+local RemoveAt =
+  function(Items, index)
+    table.remove(Items, index)
+  end
+-- )
+
+-- ( Access Level 1
+local GetAt =
+  function(Items, index)
+    AssertValidIndex(Items, index)
+
+    return GetAt(Items, index)
+  end
+
+local InsertBefore =
+  function(Items, index, Item)
+    AssertValidIndex(Items, index)
+    AssertValidItem(Item)
+
+    InsertBefore(Items, index, Item)
+  end
+
+local InsertAfter =
+  function(Items, index, Item)
+    AssertValidIndex(Items, index)
+    AssertValidItem(Item)
+
+    InsertAfter(Items, index, Item)
+  end
+
+local RemoveAt =
+  function(Items, index)
+    AssertValidIndex(Items, index)
+
+    RemoveAt(Items, index)
+  end
+
+Interface.GetAt = GetAt
+Interface.InsertBefore = InsertBefore
+Interface.InsertAfter = InsertAfter
+Interface.RemoveAt = RemoveAt
+-- )
+
+-- ( Access Level 2
+local GetFirst =
+  function(Items)
+    return GetAt(Items, GetFirstIndex(Items))
+  end
+
+local GetLast =
+  function(Items)
+    return GetAt(Items, GetLastIndex(Items))
+  end
+
+local InsertFirst =
+  function(Items, item)
+    InsertBefore(Items, GetFirstIndex(Items), item)
+  end
+
+local InsertLast =
+  function(Items, item)
+    InsertAfter(Items, GetLastIndex(Items), item)
+  end
+
+local RemoveFirst =
+  function(Items)
+    RemoveAt(Items, GetFirstIndex(Items))
+  end
+
+local RemoveLast =
+  function(Items)
+    RemoveAt(Items, GetLastIndex(Items))
+  end
+
+Interface.GetFirst = GetFirst
+Interface.GetLast = GetLast
+Interface.InsertFirst = InsertFirst
+Interface.InsertLast = InsertLast
+Interface.RemoveFirst = RemoveFirst
+Interface.RemoveLast = RemoveLast
+-- )
+
+-- ( Access Level 3
+local Get =
+  function(Items)
+    return GetFirst(Items)
+  end
+
+local Insert =
+  function(Items, item)
+    return InsertLast(Items, item)
+  end
+
+local Remove =
+  function(Items)
+    return RemoveFirst(Items)
+  end
+
+Interface.Get = Get
+Interface.Insert = Insert
+Interface.Remove = Remove
+-- )
+
+local create =
   function(Items)
     return create_instance(Items, Interface)
   end
+
+Interface.create = create
 
 -- Export:
 return Interface
