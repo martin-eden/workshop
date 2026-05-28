a652028f3b16fad3034c5fac566bc2dd78451f22
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-29 01:17:07 +0200

    [^] [concepts.gviz.save_dependencies] Code revisit.

--- concepts/gviz/save_dependencies.lua
+++ concepts/gviz/save_dependencies.lua
@@ -1,19 +1,47 @@
+-- Serialize module dependencies map to GraphViz string
+
 --[[
-  To test save output to some file, say "test.gv",
-  run "dot -Tsvg test.gv -o result.svg" and open "result.svg".
+  Author: Martin Eden
+  Last mod.: 2026-05-29
 ]]
 
-return
-  function()
-    local result = {}
+-- Imports:
+local add_to_list = request('!.concepts.list.add_item')
+local lines_to_str = request('!.convert.lines_to_str')
+
+--[[
+  Test
+
+  * Save output string to "test.gv"
+  * $ dot -Tsvg test.gv -o result.svg
+  * Open "result.svg"
+]]
+
+local save_dependencies =
+  function(Dependencies_Map)
+    local Lines = { }
+
     local graph_name = 'dependencies'
-    result[#result + 1] = ('digraph %s {'):format(graph_name)
-    for src_name, children in pairs(_G.dependencies) do
-      for dest_name in pairs(children) do
-        result[#result + 1] = ('  "%s" -> "%s"'):format(src_name, dest_name)
+    local header_str = ('digraph %s {'):format(graph_name)
+    add_to_list(Lines, header_str)
+
+    for src_name, Children in pairs(Dependencies_Map) do
+      for dest_name in pairs(Children) do
+        local dependency_str =
+          ('  "%s" -> "%s"'):format(src_name, dest_name)
+        add_to_list(Lines, dependency_str)
       end
     end
-    result[#result + 1] = '}'
-    result = table.concat(result, '\n')
-    return result
+
+    add_to_list(Lines, '}')
+
+    return lines_to_str(Lines)
   end
+
+-- Export:
+return save_dependencies
+
+--[[
+  2016
+  2026-05-29
+]]
