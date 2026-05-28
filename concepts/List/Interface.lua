f010bbc76d5a1808aae93626c0e22bb512a12ae6
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-28 15:40:36 +0200

    [^-] [concepts.List] Reduced API ..
    Removed all methods that is not O(1) time in Lua implementation.
    
    It means we can't delete or insert first element. Because it will shift
    all array. So we can just add or remove from end. It mean's it's stack
    but with random access.
    
    GetNumItems(Items) would become "return #Items" in this case and
    would provide only naming-space value. We removed it.

--- concepts/List/Interface.lua
+++ concepts/List/Interface.lua
@@ -17,32 +17,21 @@
   must return data table { } with attached metatable:
 
     (
-      [f] Get -- gets the first item
+      [f] Get -- gets last item
       [f] Add -- adds item to end
-      [f] Remove -- removes item from start
-
-      [f] GetFirst
-      [f] GetLast
-      [f] AddFirst
-      [f] AddLast
-      [f] RemoveFirst
-      [f] RemoveLast
-
-      [f] GetAt
-      [f] AddBefore
-      [f] AddAfter
-      [f] RemoveAt
+      [f] Remove -- removes last item
 
       [f] AssertNotEmpty
-      [f] AssertValidIndex
       [f] AssertValidItem
       [f] IsEmpty
-      [f] IsValidIndex
-      [f] GetNumItems
-      [f] GetFirstIndex
-      [f] GetLastIndex
       [f] IsValidItem
     )
+
+  Must raise error at:
+
+    * getting from empty list
+    * adding nil item
+    * removing from empty list
 ]]
 
 -- Imports:
@@ -56,198 +45,72 @@ local IsValidItem =
     return not is_nil(Item)
   end
 
-local GetNumItems =
+local IsEmpty =
   function(Items)
-    return #Items
+    return (#Items == 0)
   end
 
+Interface.IsEmpty = IsEmpty
 Interface.IsValidItem = IsValidItem
-Interface.GetNumItems = GetNumItems
 -- )
 
 -- ( Meta Level 2
-local IsEmpty =
-  function(Items)
-    return (GetNumItems(Items) == 0)
-  end
-
-local GetFirstIndex =
-  function(Items)
-    if IsEmpty(Items) then return 0 end
-
-    return 1
-  end
-
-local GetLastIndex =
-  function(Items)
-    if IsEmpty(Items) then return 0 end
-
-    return GetNumItems(Items)
-  end
-
 local AssertValidItem =
   function(Item)
     assert(IsValidItem(Item), 'Invalid item')
   end
 
-Interface.IsEmpty = IsEmpty
-Interface.GetFirstIndex = GetFirstIndex
-Interface.GetLastIndex = GetLastIndex
-Interface.AssertValidItem = AssertValidItem
--- )
-
--- ( Meta Level 3
 local AssertNotEmpty =
   function(Items)
     assert(not IsEmpty(Items), 'List is empty')
   end
 
-local IsValidIndex =
-  function(Items, index)
-    return
-      is_integer(index) and
-      (index >= GetFirstIndex(Items)) and
-      (index <= GetLastIndex(Items))
-  end
-
+Interface.AssertValidItem = AssertValidItem
 Interface.AssertNotEmpty = AssertNotEmpty
-Interface.IsValidIndex = IsValidIndex
--- )
-
--- ( Meta_Level_4
-local AssertValidIndex =
-  function(Items, index)
-    assert(IsValidIndex(Items, index), 'Invalid index')
-  end
-
-Interface.AssertValidIndex = AssertValidIndex
 -- )
 
 -- ( Access Core
-local GetAt =
-  function(Items, index)
-    return Items[index]
-  end
-
-local AddBefore =
-  function(Items, index, Item)
-    table.insert(Items, index, Item)
-  end
-
-local AddAfter =
-  function(Items, index, Item)
-    table.insert(Items, index + 1, Item)
-  end
-
-local RemoveAt =
-  function(Items, index)
-    table.remove(Items, index)
-  end
--- )
-
--- ( Access Level 1
-local GetAt =
-  function(Items, index)
-    AssertValidIndex(Items, index)
-
-    return GetAt(Items, index)
-  end
-
-local AddBefore =
-  function(Items, index, Item)
-    AssertValidIndex(Items, index)
-    AssertValidItem(Item)
-
-    AddBefore(Items, index, Item)
-  end
-
-local AddAfter =
-  function(Items, index, Item)
-    AssertValidIndex(Items, index)
-    AssertValidItem(Item)
-
-    AddAfter(Items, index, Item)
-  end
-
-local RemoveAt =
-  function(Items, index)
-    AssertValidIndex(Items, index)
-
-    RemoveAt(Items, index)
-  end
-
-Interface.GetAt = GetAt
-Interface.AddBefore = AddBefore
-Interface.AddAfter = AddAfter
-Interface.RemoveAt = RemoveAt
--- )
-
--- ( Access Level 2
-local GetFirst =
-  function(Items)
-    AssertNotEmpty(Items)
-
-    return GetAt(Items, GetFirstIndex(Items))
-  end
-
 local GetLast =
   function(Items)
-    AssertNotEmpty(Items)
-
-    return GetAt(Items, GetLastIndex(Items))
-  end
-
-local AddFirst =
-  function(Items, item)
-    AddBefore(Items, GetFirstIndex(Items), item)
+    return Items[#Items]
   end
 
 local AddLast =
   function(Items, item)
-    AddAfter(Items, GetLastIndex(Items), item)
+    table.insert(Items, item)
   end
 
-local RemoveFirst =
+local RemoveLast =
+  function(Items)
+    table.remove(Items)
+  end
+-- )
+
+-- ( Access Level 1
+local GetLast =
   function(Items)
     AssertNotEmpty(Items)
 
-    RemoveAt(Items, GetFirstIndex(Items))
+    return GetLast(Items)
+  end
+
+local AddLast =
+  function(Items, item)
+    AssertValidItem(item)
+
+    AddLast(Items, item)
   end
 
 local RemoveLast =
   function(Items)
     AssertNotEmpty(Items)
 
-    RemoveAt(Items, GetLastIndex(Items))
+    RemoveLast(Items)
   end
 
-Interface.GetFirst = GetFirst
-Interface.GetLast = GetLast
-Interface.AddFirst = AddFirst
-Interface.AddLast = AddLast
-Interface.RemoveFirst = RemoveFirst
-Interface.RemoveLast = RemoveLast
--- )
-
--- ( Access Level 3
-local Get =
-  function(Items)
-    return GetFirst(Items)
-  end
-
-local Add =
-  function(Items, item)
-    return AddLast(Items, item)
-  end
-
-local Remove =
-  function(Items)
-    return RemoveFirst(Items)
-  end
-
-Interface.Get = Get
-Interface.Add = Add
-Interface.Remove = Remove
+Interface.Get = GetLast
+Interface.Add = AddLast
+Interface.Remove = RemoveLast
 -- )
 
 local create =
@@ -262,4 +125,5 @@ return Interface
 
 --[[
   2026-05-27
+  2026-05-28
 ]]
