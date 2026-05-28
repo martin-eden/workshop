1766c46a88dbd8f47c43006b8af45f0180fa9db1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-28 23:17:42 +0200

    [^] [base] Code revisit.

--- base.lua
+++ base.lua
@@ -2,22 +2,54 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-08
+  Last mod.: 2026-05-28
 ]]
 
 --[[
-  This module installs global function "request" which is based on
-  "require" and makes possible relative module names.
+  Point of code organization with small files and directories
+  is relative "require()" and ability to deploy only used files.
+]]
 
-  Also this function tracks module dependencies. This allows to
-  get dependencies list for any module. Which is used to create
-  deploys without unused code.
+--[[
+  Installed globals
 
-  Price for tracking dependencies is global table "dependencies"
-  and function "get_require_name".
+  * Global "request()"
 
-  Lastly, global functions are added for convenience. Such functions
-  are "new" and families of "is_<type>" and "assert_<type>".
+    Relative "require()" which allows loading modules by name
+    related to caller's module directory.
+
+    Uses extended name syntax: "^." -- upper directory,
+    "!." -- directory of this "base.lua" module.
+
+  * Global "get_dependencies()"
+
+    Table with names map. Implementation of "request()" tracks run-time
+    dependencies.
+
+  * Global "get_base_prefix()"
+
+    Implementation of "request()" knows it's own module name prefix
+    for "require()". It's critical information for niche tools.
+
+  * Global "get_require_name()"
+
+    Not essential -- can be recreated with already provided information.
+    Used by [require_file]. Shared because we don't like duplicate code.
+
+  * Convenience globals
+
+    These functions are actually modules but made global because
+    they are using often:
+
+    * is_..() family
+
+      is_bool(), is_number, is_integer(), is_float(), is_table() ...
+
+    * assert_..() family
+
+      assert_bool(), assert_number(), assert_integer(), ...
+
+    * new() -- clones table and applies optional field overrides
 ]]
 
 local split_name =
@@ -54,14 +86,14 @@ local unite_prefixes =
     return base_prefix .. rel_prefix
   end
 
-local names = {}
+local Names = { }
 local depth = 1
 
 local get_caller_prefix =
   function()
     local result = ''
-    if names[depth] then
-      result = names[depth].prefix
+    if Names[depth] then
+      result = Names[depth].prefix
     end
     return result
   end
@@ -69,16 +101,18 @@ local get_caller_prefix =
 local get_caller_name =
   function()
     local result = 'anonymous'
-    if names[depth] then
-      result = names[depth].prefix .. names[depth].name
+
+    if Names[depth] then
+      result = Names[depth].prefix .. Names[depth].name
     end
+
     return result
   end
 
 local push =
   function(prefix, name)
     depth = depth + 1
-    names[depth] = {prefix = prefix, name = name}
+    Names[depth] = { prefix = prefix, name = name }
   end
 
 local pop =
@@ -86,25 +120,34 @@ local pop =
     depth = depth - 1
   end
 
-local dependencies = {}
+local Dependencies_Map = { }
+
 local add_dependency =
   function(src_name, dest_name)
-    dependencies[src_name] = dependencies[src_name] or {}
-    dependencies[src_name][dest_name] = true
+    Dependencies_Map[src_name] = Dependencies_Map[src_name] or { }
+
+    Dependencies_Map[src_name][dest_name] = true
   end
 
 local base_prefix = split_name((...))
 
 local get_require_name =
   function(qualified_name)
+    local caller_prefix
+
     local is_absolute_name = (qualified_name:sub(1, 2) == '!.')
+
     if is_absolute_name then
       qualified_name = qualified_name:sub(3)
+      caller_prefix = base_prefix
+    else
+      caller_prefix = get_caller_prefix()
     end
+
     local prefix, name = split_name(qualified_name)
-    local caller_prefix =
-      is_absolute_name and base_prefix or get_caller_prefix()
+
     prefix = unite_prefixes(caller_prefix, prefix)
+
     return prefix .. name, prefix, name
   end
 
@@ -115,64 +158,36 @@ local request =
     local require_name, prefix, name = get_require_name(qualified_name)
 
     push(prefix, name)
+
     local dest_name = get_caller_name()
+
     add_dependency(src_name, dest_name)
-    local results = table.pack(require(require_name))
+
+    local Results = table.pack(require(require_name))
+
     pop()
 
-    return table.unpack(results)
+    return table.unpack(Results)
   end
 
 local is_first_run = (_G.request == nil)
 
+-- Export globals:
 if is_first_run then
   _G.request = request
-  _G.dependencies = dependencies
-  _G.get_require_name = get_require_name
+  _G.get_dependencies = function() return Dependencies_Map end
   _G.get_base_prefix = function() return base_prefix end
-
-  --[[
-    At this point we installed "request()", so it's usable from
-    outer code.
-
-    Also we made global "dependencies{}" and "get_require_name()".
-    "dependenices" is optional and not mission-critical. But stores
-    inter-module call edges which are tricky to obtain other ways.
-    "get_require_name()" is part of "request()" internal machinery.
-    Both has niche uses in my code tree tools. And they can't be
-    moved to module because it's request() that provides modules.
-  ]]
-  --[[
-    Below we call optional modules which install additional
-    global functions.
-
-    Functions made global because they are widely used in my code.
-
-    They are inside other files. We use freshly added "request()"
-    to load them and add them to dependencies of this module.
-
-    We need add record to call stack with our name because these
-    calls of "request()" are inside "if", so the call will not be
-    done until actual execution.
-  ]]
-
-  -- First element is invocation module name, second - module file path
-  local our_require_name = (...)
-
-  push('', our_require_name)
-
+  _G.get_require_name = get_require_name
   request('!.system.install_is_functions')()
   request('!.system.install_assert_functions')()
   _G.new = request('!.table.new')
-
-  pop()
 end
 
 --[[
-  2016-06
-  2017-09
-  2018-02
-  2018-05
-  2024-03
+  2016 #
+  2017 #
+  2018 # #
+  2024 #
   2026-05-08
+  2026-05-28
 ]]
