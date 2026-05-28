c2b902b8332f2925dca4fff5c3e6c19cafa29092
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-29 00:27:50 +0200

    [^] [system.get_modules_dependencies] Code revisit.

--- system/get_modules_dependencies.lua
+++ system/get_modules_dependencies.lua
@@ -1,54 +1,54 @@
--- Return set of modules required for given set of modules
+-- Return list of modules required for given list of modules
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-29
+]]
 
 --[[
   Input
 
-    table
-
-      Sequence of module names.
+    [t] Modules -- list of root modules names
 
   Output
 
-    table
-
-      Sequence of module names.
+    [t] -- List of all required modules
 
   So if module "a" requires "lib" and module "b" requires it,
   result of call ({'a', 'b'}) will be ({'a', 'b', 'lib'}).
 ]]
 
--- Last mod.: 2026-05-28
+-- Imports:
+local get_module_dependencies = request('get_module_dependencies')
+local add_to_list = request('!.concepts.list.add_item')
 
-local GetModuleDependencies = request('get_module_dependencies')
-
-return
+local get_modules_dependencies =
   function(Modules)
     assert_table(Modules)
 
-    local UnitedSet = {}
-    local UnitedList = {}
+    local Result = { }
+    local UsedModules_Map = { }
 
-    for _, ModuleName in ipairs(Modules) do
-      local ModuleDependencies = {}
+    for _, root_module_name in ipairs(Modules) do
+      local ModuleDependencies = get_module_dependencies(root_module_name)
 
-      ModuleDependencies = GetModuleDependencies(ModuleName)
+      for _, module_name in ipairs(ModuleDependencies) do
+        if not UsedModules_Map[module_name] then
+          add_to_list(Result, module_name)
 
-      for _, DependencyName in ipairs(ModuleDependencies) do
-        if UnitedSet[DependencyName] then
-          goto Continue
+          UsedModules_Map[module_name] = true
         end
-
-        UnitedSet[DependencyName] = true
-        table.insert(UnitedList, DependencyName)
-
-        ::Continue::
       end
     end
 
-    return UnitedList
+    return Result
   end
 
+-- Export:
+return get_modules_dependencies
+
 --[[
-  2018-02-05
-  2024-03-02
+  2018 #
+  2024 #
+  2026-05-29
 ]]
