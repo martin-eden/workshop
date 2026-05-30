b27d396fc57442d2fad829214373ee5a7cddf5b4
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-30 22:10:40 +0200

    [^~] [i2c.assert_device_id] Code revisit. Using [get_bits].

--- concepts/i2c/assert_device_id.lua
+++ concepts/i2c/assert_device_id.lua
@@ -1,10 +1,31 @@
-return
+-- Assert that given argument is 10-bit integer (device id)
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-30
+]]
+
+-- Imports:
+local get_bits = request('!.number.get_bits')
+
+local assert_device_id =
   function(device_id)
     assert_integer(device_id)
-    if (device_id ~= (device_id & 0x3FF)) then
+
+    if (device_id ~= get_bits(device_id, 0, 9)) then
       local msg =
-        ('Given device id 0x%X does not fit in 10 bits (0x00/0x3FF'):
-        format(device_id)
+        string.format(
+          'Given device id 0x%X is not fitting in 10 bits.',
+          device_id
+        )
       error(msg, 2)
     end
   end
+
+-- Export:
+return assert_device_id
+
+--[[
+  2019
+  2026-05-30
+]]
