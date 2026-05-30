ffb4b4de7dc8b2697fa7cfd59077429fd1f2e4ef
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-30 22:05:37 +0200

    [^~] [firmata.parser.reply.digital_report] Revisit. Using [get_bit].

--- concepts/firmata/parser/reply/digital_report.lua
+++ concepts/firmata/parser/reply/digital_report.lua
@@ -1,25 +1,52 @@
-local signatures = request('!.concepts.firmata.protocol.signatures')
+-- Parse reply with port pins digital values
 
-return
-  function(self, chunk, type_id)
-    if (#chunk < 2) then
-      return
-    end
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-30
+]]
 
-    local port_index = signatures.digital_report[type_id]
-    local value = self:to_word(chunk:byte(1, 2))
+--[[
+  Output
 
-    local result =
+    [t]
+      [i] port_index
+      [t] pins -- boolean map of pin values
+
+  Example:
+
+    { port_index = 1, pins = { [8] = false, .. , [15] = true } }
+]]
+
+-- Imports:
+local Signatures = request('!.concepts.firmata.protocol.signatures')
+local get_bit = request('!.number.get_bit')
+
+local parse_digital_report =
+  function(Me, chunk, type_id)
+    if (#chunk < 2) then return end
+
+    local port_index = Signatures.digital_report[type_id]
+    local value = Me:to_word(chunk:byte(1, 2))
+
+    local Result =
       {
         port_index = port_index,
-        pins = {}
+        pins = { },
       }
 
     local base_offset = port_index * 8
+
     for pin_offset = 0, 7 do
-      result.pins[base_offset + pin_offset] =
-        (value & (1 << pin_offset) ~= 0)
+      Result.pins[base_offset + pin_offset] = get_bit(value, pin_offset)
     end
 
-    return result
+    return Result
   end
+
+-- Export:
+return parse_digital_report
+
+--[[
+  2019
+  2026-05-30
+]]
