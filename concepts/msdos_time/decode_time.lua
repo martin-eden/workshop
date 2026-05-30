aa72abdc3c9169518f4e9e058a7dd11008c29758
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-30 21:55:01 +0200

    [^~] [msdos_time.decode_time] Code revisit. Using [get_bits].

--- concepts/msdos_time/decode_time.lua
+++ concepts/msdos_time/decode_time.lua
@@ -1,18 +1,38 @@
+-- Convert time in MS-DOS format (16 bits) to string with ISO 8601 time
+
 --[[
-  Decode time given in MS-DOS FAT format (16 bits).
-  Return string with ISO 8601 time.
+  Author: Martin Eden
+  Last mod.: 2026-05-30
 ]]
 
-return
+--[[
+  Input:
+    [s] raw_date -- data bytes as string
+  Output:
+    [s] -- ISO date
+]]
+
+-- Imports:
+local get_bits = request('!.number.get_bits')
+
+local decode_time =
   function(raw_time)
     assert_string(raw_time)
     assert(#raw_time == 2)
-    local data = ('< I2'):unpack(raw_time)
-    local hour = (data >> 11) & ((1 << 5) - 1)
-    hour = hour
-    local minute = (data >> 5) & ((1 << 6) - 1)
-    local second = data & ((1 << 5) - 1)
-    second = second * 2
-    local result = ('%02d:%02d:%02d'):format(hour, minute, second)
-    return result
+
+    local data = string.unpack('< I2', raw_time)
+
+    local hour = get_bits(data, 11, 15)
+    local minute = get_bits(data, 5, 10)
+    local second = get_bits(data, 0, 4) * 2
+
+    return string.format('%02d:%02d:%02d', hour, minute, second)
   end
+
+-- Export:
+return decode_time
+
+--[[
+  2018
+  2026-05-30
+]]
