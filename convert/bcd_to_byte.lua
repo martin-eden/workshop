02d01180f57b13f42da59ff49cf8c7fa76586a2a
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-30 21:23:09 +0200

    [^~] [convert.bcd_to_byte] Using [get_bits].

--- convert/bcd_to_byte.lua
+++ convert/bcd_to_byte.lua
@@ -2,12 +2,15 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-13
+  Last mod.: 2026-05-30
 ]]
+
+-- Imports:
+local get_bits = request('!.number.get_bits')
 
 local bcd_to_byte =
   function(byte_bcd)
-    return (byte_bcd // 16) * 10 + (byte_bcd % 16)
+    return get_bits(byte_bcd, 4, 7) * 10 + get_bits(byte_bcd, 0, 3)
   end
 
 -- Export:
@@ -17,4 +20,5 @@ return bcd_to_byte
   2019 # #
   2026-05-05
   2026-05-13
+  2026-05-30
 ]]
