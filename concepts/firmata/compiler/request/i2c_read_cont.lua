19c638f61c4fc9d5f41a23f3d68e68e82845487b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-30 22:22:41 +0200

    [^~] [firmata.compiler.request.i2c_read_cont] Using [assert_byte].

--- concepts/firmata/compiler/request/i2c_read_cont.lua
+++ concepts/firmata/compiler/request/i2c_read_cont.lua
@@ -1,16 +1,31 @@
-local signatures = request('!.concepts.firmata.protocol.signatures')
-local assert_offset = request('!.concepts.i2c.assert_offset')
-local assert_num_bytes = request('!.concepts.i2c.assert_num_bytes')
+-- Compile I2C request for continuous reads
 
-return
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-30
+]]
+
+-- Imports:
+local assert_byte = request('!.number.assert_byte')
+local Signatures = request('!.concepts.firmata.protocol.signatures')
+
+local compile_i2c_read_cont =
   function(self, device_id, offset, num_bytes)
-    assert_offset(offset)
-    assert_num_bytes(num_bytes)
+    assert_byte(offset)
+    assert_byte(num_bytes)
 
     local restart_reads = true
 
-    self:emit(signatures.sysex_start)
+    self:emit(Signatures.sysex_start)
     self:i2c_emit_request('read_continiously', device_id, restart_reads)
     self:emit_bytes(offset, num_bytes)
-    self:emit(signatures.sysex_end)
+    self:emit(Signatures.sysex_end)
   end
+
+-- Export:
+return compile_i2c_read_cont
+
+--[[
+  2019
+  2026-05-30
+]]
