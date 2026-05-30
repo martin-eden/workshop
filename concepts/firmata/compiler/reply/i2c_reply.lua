7eaa338c97f1212db8b31f4270cf79f241e47ce7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-05-30 22:30:09 +0200

    [^~] [firmata.compiler.reply.i2c_reply] Uses [assert_byte], [get_bits].

--- concepts/firmata/compiler/reply/i2c_reply.lua
+++ concepts/firmata/compiler/reply/i2c_reply.lua
@@ -1,18 +1,43 @@
-local signatures = request('!.concepts.firmata.protocol.signatures')
+-- Compile I2C reply message
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-05-30
+]]
+
+--[[
+  Input:
+    [t] Me
+    [i] device_id
+    [i] offset
+    [...] -- sequence of data bytes (integers)
+]]
+
+-- Imports:
 local assert_device_id = request('!.concepts.i2c.assert_device_id')
-local assert_offset = request('!.concepts.i2c.assert_offset')
+local assert_byte = request('!.number.assert_byte')
+local get_bits = request('!.number.get_bits')
+local Signatures = request('!.concepts.firmata.protocol.signatures')
 
-return
-  function(self, device_id, offset, ...)
+local compile_i2c_reply =
+  function(Me, device_id, offset, ...)
     assert_device_id(device_id)
-    assert_offset(offset)
+    assert_byte(offset)
 
-    self:emit(
-      signatures.sysex_start,
-      signatures.i2c_reply,
-      device_id & 0x7F,
-      device_id >> 7
+    Me:emit(
+      Signatures.sysex_start,
+      Signatures.i2c_reply,
+      get_bits(device_id, 0, 6),
+      get_bits(device_id, 7, 13)
     )
-    self:emit_bytes(offset, ...)
-    self:emit(signatures.sysex_end)
+    Me:emit_bytes(offset, ...)
+    Me:emit(Signatures.sysex_end)
   end
+
+-- Export:
+return compile_i2c_reply
+
+--[[
+  2019 # #
+  2026-05-30
+]]
