d10b4ab1812e9425f82d4105d37d821d1dd02226
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-01 14:38:27 +0200

    [>] [system.get_loaded_modules_files] Resurrected ..
    Previous name [get_loaded_module_files].
    
    Restored code and revisited.

---
+++ system/get_loaded_modules_files.lua
@@ -0,0 +1,38 @@
+-- Returns a list of filenames for used modules
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-01
+]]
+
+--[[
+  Uses _G.package.loaded to get list of module names.
+
+  We'll search in that list and return found files.
+]]
+
+-- Imports:
+local get_table_keys = request('!.table.get_keys')
+local get_module_pathname = request('get_module_pathname')
+local add_to_list = request('!.concepts.list.add_item')
+
+local get_loaded_modules_files =
+  function()
+    local ModulesPathnames = { }
+
+    local ModulesNames = get_table_keys(_G.package.loaded)
+
+    for _, module_name in ipairs(ModulesNames) do
+      add_to_list(ModulesPathnames, get_module_pathname(module_name))
+    end
+
+    return ModulesPathnames
+  end
+
+-- Export:
+return get_loaded_modules_files
+
+--[[
+  2017
+  2026-06-01
+]]
