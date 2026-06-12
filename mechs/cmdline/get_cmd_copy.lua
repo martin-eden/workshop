8df65c31e544fef4d14cc4e47ef5d689f8ad113a
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-12 06:40:09 +0200

    [^+] [cmdline.get_cmd_copy] Smarter: sometimes produces shorter cmds ..
    Case:
    
    "cp /usr/local/share/lua/5.3/tek/lib/debug.lua deploy/tek/lib/debug.lua"
    
    We can reduce it to
    
    "cp /usr/local/share/lua/5.3/tek/lib/debug.lua deploy/tek/lib/"

--- mechs/cmdline/get_cmd_copy.lua
+++ mechs/cmdline/get_cmd_copy.lua
@@ -2,10 +2,11 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-04-28
+  Last mod.: 2026-06-12
 ]]
 
 -- Imports:
+local parse_pathname = request('!.concepts.path_name.parse')
 local normalize = request('!.file_system.file.normalize_name')
 local quote = request('!.concepts.shell.quote')
 local glue_words = request('!.concepts.words.to_string')
@@ -13,6 +14,13 @@ local glue_words = request('!.concepts.words.to_string')
 -- Export:
 return
   function(src_name, dest_name)
+    local src_file_name = parse_pathname(src_name).Name
+    local dest_file_name = parse_pathname(dest_name).Name
+
+    if (src_file_name == dest_file_name) then
+      dest_name = parse_pathname(dest_name).HostDir
+    end
+
     local Command =
       {
         'cp',
@@ -26,7 +34,7 @@ return
 --[[
   2018 #
   2024 #
-  2026-01-12
-  2026-04-17
-  2026-04-28
+  2026-01 #
+  2026-04 # #
+  2026-06-12
 ]]
