54470863ca7a7a054a8a261bb121d6e57713e380
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-12 09:10:54 +0200

    [+] [path_name.compile]

---
+++ concepts/path_name/compile.lua
@@ -0,0 +1,23 @@
+-- Compile list of names to directory path
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-12
+]]
+
+-- Imports:
+local list_to_str = request('!.concepts.list.to_string')
+
+local names_sep = '/'
+
+local compile =
+  function(Pathname)
+    return list_to_str(Pathname, names_sep)
+  end
+
+-- Export:
+return compile
+
+--[[
+  2026-06-12
+]]
