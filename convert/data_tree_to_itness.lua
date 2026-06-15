e51525f1c2a8aa98852d895a337b531aa607266b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-15 18:29:01 +0200

    [/] [convert.data_tree_to_itness] Relaxed accepted data types ..
    Originally only values that are strings or tables were converted.
    
    So you wasn't able to convert boolean value "false" to string "false".
    
    Now you can do it.
    
    That's fucking "[feat]"!
    
    You can't convert string "false" to boolean value "false" because
    we don't know type of original value.
    
    But it's practical, I'm serializing table with error details for meat
    (for myself).

--- convert/data_tree_to_itness.lua
+++ convert/data_tree_to_itness.lua
@@ -2,22 +2,32 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-08
+  Last mod.: 2026-06-15
 ]]
 
 --[[
   Input structure
 
-    <node> [t]:
-      <key> [s] = [s] -- terminal string node
-      or
-      <key> [s] = <node> [t] -- child node
+  Map. Typical Lua table with named keys.
+
+
+  Restrictions
+
+  Key names must be strings or integers.
+  Values must be strings or integers or booleans or tables.
+
+  ( It's injective transformation written for practical needs
+
+    Integers are mapped to strings. So it's impossible to
+    figure out original type of value. F.e. "1" was '1' or 1 ?
+  )
+
 
   Example:
 
-    { ['a'] = 'A', ['b'] = { ['bb'] = 'B' } }
+    { a = 'A', b = { bb = 0 } }
                      ->
-    { { 'a', 'A' }, { 'b', { 'bb', 'B' } } }
+    { { 'a', 'A' }, { 'b', { 'bb', '0' } } }
 ]]
 
 -- Imports:
@@ -30,14 +40,28 @@ data_tree_to_itness =
     local ResultIs = { }
 
     for key, val in ordered_pass(DataTree) do
-      local NodeIs = { }
-      add_to_list(NodeIs, key)
-      if is_string(val) then
-        add_to_list(NodeIs, val)
-      elseif is_table(val) then
-        add_to_list(NodeIs, data_tree_to_itness(val))
+      if
+        not (
+          (is_string(key) or is_integer(key)) and
+          (is_string(val) or is_integer(val) or is_boolean(val) or is_table(val))
+        )
+      then
+        goto next
       end
+
+      local NodeIs = { }
+
+      NodeIs[1] = tostring(key)
+
+      if is_table(val) then
+        NodeIs[2] = data_tree_to_itness(val)
+      else
+        NodeIs[2] = tostring(val)
+      end
+
       add_to_list(ResultIs, NodeIs)
+
+      :: next ::
     end
 
     return ResultIs
@@ -48,4 +72,5 @@ return data_tree_to_itness
 
 --[[
   2026-05-05
+  2026-06-15
 ]]
