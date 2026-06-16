5a93f9d76d196419d3c53306e0129bacca952a58
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-16 16:25:03 +0200

    [.^] [lua.data_mathtypes] Code revisit.

--- lua/data_mathtypes.lua
+++ lua/data_mathtypes.lua
@@ -1,26 +1,20 @@
 -- Return list of numeric type names
 
 --[[
-  Output
-
-    table
-
-      List with number type names as they are returned
-      by math.type() function.
-
-  Note
-
-    Used in code generation.
+  Author: Martin Eden
+  Last mod.: 2026-06-16
 ]]
 
--- Last mod.: 2024-08-06
-
-return
+local MathTypeNames =
   {
     'integer',
     'float',
   }
 
+-- Export:
+return MathTypeNames
+
 --[[
   2024-03-02
+  2026-06-16
 ]]
