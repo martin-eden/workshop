09259d2b1f8a0faade5a23853b1032827f0e580b
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-16 12:45:51 +0200

    [.^] [string.file_name.add_dir_postfix] Code revisit.

--- string/file_name/add_dir_postfix.lua
+++ string/file_name/add_dir_postfix.lua
@@ -1,7 +1,25 @@
-return
-  function(s)
-    if (s:sub(-1) ~= '/') then
-      s = s .. '/'
-    end
-    return s
+-- Add "/" to end of string if needed
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-16
+]]
+
+-- Imports:
+local ends_with = request('!.string.ends_with')
+
+local add_dir_postfix =
+  function(str)
+    local dir_sep = '/'
+
+    if ends_with(str, dir_sep) then return str end
+
+    return str .. dir_sep
   end
+
+return add_dir_postfix
+
+--[[
+  2018-02-05
+  2026-06-16
+]]
