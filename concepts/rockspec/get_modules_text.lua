d2d28a5be87eeb4891be33adea53e7c5c18c832f
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-16 21:06:39 +0200

    [^~] [rockspec.get_modules_text] Revisit. Using [convert.table_to_str].

--- concepts/rockspec/get_modules_text.lua
+++ concepts/rockspec/get_modules_text.lua
@@ -1,14 +1,28 @@
-local table_to_string = request('!.concepts.lua_table.save')
+-- Return string with Lua table describing Modules arg
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-16
+]]
+
+-- Imports:
+local table_to_str = request('!.convert.table_to_str')
 local trim_head = request('!.string.trim_head')
 
-return
-  function(modules)
-    local result
-    result =
-      table_to_string(
-        modules,
-        {c_text_block = {next_line_indent = 1}}
+local get_modules_text =
+  function(Modules)
+    local result =
+      table_to_str(
+        Modules, { c_text_block = { next_line_indent = 1 } }
       )
-    result = trim_head(result)
-    return result
+
+    return trim_head(result)
   end
+
+-- Export:
+return get_modules_text
+
+--[[
+  2018
+  2026-06-16
+]]
