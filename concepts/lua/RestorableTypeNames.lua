ccd432b8303b9a709bc14df0ffcf7b64e83218a1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-17 11:51:13 +0200

    [+] [concepts.lua.RestorableTypeNames]

---
+++ concepts/lua/RestorableTypeNames.lua
@@ -0,0 +1,16 @@
+-- List of value types that can be restored from string
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-17
+]]
+
+local RestorableTypeNames =
+  { 'boolean', 'number', 'string', 'table' }
+
+-- Export:
+return RestorableTypeNames
+
+--[[
+  2026-06-17
+]]
