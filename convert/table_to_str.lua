c4a16c5b99547145d9849c68a5c41173668f9344
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-17 11:36:32 +0200

    [^+] [convert.table_to_str] Trying to serialize as tree and as graph.

--- convert/table_to_str.lua
+++ convert/table_to_str.lua
@@ -1,13 +1,28 @@
--- Save table to string with Lua table definition
+-- Save table to string with Lua code for table definition
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-05
+  Last mod.: 2026-06-17
 ]]
 
+-- Imports:
+local graph_is_tree = request('!.table.is_tree')
+local tree_to_str = request('!.concepts.lua_table.save')
+local graph_to_str = request('!.concepts.lua_table_code.save')
+
+local table_to_str =
+  function(Graph)
+    if graph_is_tree(Graph) then
+      return tree_to_str(Graph)
+    end
+
+    return graph_to_str(Graph)
+  end
+
 -- Export:
-return request('!.concepts.lua_table.save')
+return table_to_str
 
 --[[
   2026-05-05
+  2026-06-17
 ]]
