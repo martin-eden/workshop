5bdab3afa9bebb0be4d496707bf1f8e44ad0ae61
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-17 11:37:03 +0200

    [^+] [convert.table_from_str] Trying to load as tree and as graph.

--- convert/table_from_str.lua
+++ convert/table_from_str.lua
@@ -2,12 +2,28 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-05
+  Last mod.: 2026-06-17
 ]]
 
+-- Imports:
+local tree_from_str = request('!.concepts.lua_table.load')
+local graph_from_str = request('!.concepts.lua_table_code.load')
+
+local table_from_str =
+  function(str)
+    local Graph
+
+    Graph = tree_from_str(str)
+    if Graph then return Graph end
+
+    Graph = graph_from_str(str)
+    if Graph then return Graph end
+  end
+
 -- Export:
-return request('!.concepts.lua_table.load')
+return table_from_str
 
 --[[
   2026-05-05
+  2026-06-17
 ]]
