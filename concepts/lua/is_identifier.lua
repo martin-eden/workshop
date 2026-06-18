2f85b9e0d2760fee660c2ab199b3244f2d20dc2e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-18 18:31:02 +0200

    [.^] [lua.is_identifier] Code revisit.

--- concepts/lua/is_identifier.lua
+++ concepts/lua/is_identifier.lua
@@ -1,9 +1,25 @@
-local keywords = request('keywords')
+-- Check that string is valid Lua name and is not a keyword
 
-return
-  function(s)
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-18
+]]
+
+-- Imports:
+local Keywords_Map = request('keywords')
+
+local is_identifier =
+  function(str)
     return
-      is_string(s) and
-      s:match('^[%a_][%w_]*$') and
-      not keywords[s]
+      is_string(str) and
+      string.match(str, '^[%a_][%w_]*$') and
+      not Keywords_Map[str]
   end
+
+-- Export:
+return is_identifier
+
+--[[
+  2016
+  2026-06-18
+]]
