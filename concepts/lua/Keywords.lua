f59169574fb7293bca6d2b15c74a3aabf71fe03c
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-18 18:38:56 +0200

    [/] [lua.keywords] Renamed to "Keywords". Data structure change ..
    Now it returns list of strings, not strings map to booleans.
    
    I want all base data be serializable.

---
+++ concepts/lua/Keywords.lua
@@ -0,0 +1,51 @@
+-- List of Lua keywords
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-06-18
+]]
+
+local Keywords =
+  {
+    'nil',
+
+    'true',
+    'false',
+
+    'not',
+    'and',
+    'or',
+
+    'local',
+
+    'do',
+    'end',
+
+    'goto',
+
+    'if',
+    'then',
+    'elseif',
+    'else',
+
+    'while',
+
+    'repeat',
+    'until',
+
+    'for',
+    'in',
+
+    'break',
+
+    'function',
+    'return',
+  }
+
+-- Export:
+return Keywords
+
+--[[
+  2016
+  2026-06-18
+]]
