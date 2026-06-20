eb775c777f52e2deefc5405f92dd171b4a0d4439
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-06-20 15:15:03 +0200

    [<~] [convert.value_to_str] Using [serialize_terminal_value].

--- convert/value_to_str.lua
+++ convert/value_to_str.lua
@@ -2,7 +2,7 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-06-18
+  Last mod.: 2026-06-20
 ]]
 
 --[[
@@ -27,92 +27,41 @@
     [∀] Value -- any Lua value
     [?t] Encoders -- map of type_name-function
       Function will receive value of given type and
-      must return ( true <str> ) in case of success.
+      must return string in case of success.
 ]]
 
 -- Imports:
-local is_nan = request('!.number.is_nan')
-local is_pos_inf = request('!.number.is_pos_inf')
-local is_neg_inf = request('!.number.is_neg_inf')
-local stock_value_to_str = _G.tostring
-local lua_quote_str = request('!.concepts.lua.quote_string')
+local serialize_terminal_value = request('!.concepts.lua.serialize_terminal_value')
 local table_to_str = request('table_to_str')
 
-local encode_nil =
-  function()
-    return true, 'nil'
-  end
-
-local encode_bool =
+local represent_unserializable =
   function(val)
-    if (val == false) then return true, 'false' end
-    if (val == true) then return true, 'true' end
-  end
-
-local encode_number =
-  function(val)
-    if is_nan(val) then return true, '0/0' end
-    if is_pos_inf(val) then return true, '1/0' end
-    if is_neg_inf(val) then return true, '-1/0' end
-
-    return true, stock_value_to_str(val)
-  end
-
-local encode_string =
-  function(val)
-    return true, lua_quote_str(val)
-  end
-
-local encode_table =
-  function(val)
-    return true, table_to_str(val)
-  end
-
-local encode_function =
-  function(val)
-    return true, lua_quote_str(stock_value_to_str(val))
-  end
-
-local encode_userdata =
-  function(val)
-    return true, lua_quote_str(stock_value_to_str(val))
-  end
-
-local encode_thread =
-  function(val)
-    return true, lua_quote_str(stock_value_to_str(val))
+    return serialize_terminal_value(_G.tostring(val))
   end
 
 local DefaultEncoders =
   {
-    ['nil'] = encode_nil,
-    ['boolean'] = encode_bool,
-    ['number'] = encode_number,
-    ['string'] = encode_string,
-    ['table'] = encode_table,
-    ['function'] = encode_function,
-    ['userdata'] = encode_userdata,
-    ['thread'] = encode_thread,
+    ['nil'] = serialize_terminal_value,
+    ['boolean'] = serialize_terminal_value,
+    ['number'] = serialize_terminal_value,
+    ['string'] = serialize_terminal_value,
+    ['table'] = table_to_str,
+    ['function'] = represent_unserializable,
+    ['userdata'] = represent_unserializable,
+    ['thread'] = represent_unserializable,
   }
 
 local value_to_str =
   function(Value, ArgEncoders)
-    local Encoders = DefaultEncoders
-    if is_table(ArgEncoders) then
-      Encoders = new(Encoders, ArgEncoders)
-    end
+    local Encoders = new(DefaultEncoders, ArgEncoders)
 
     local encoder = Encoders[type(Value)]
 
-    -- No codec?
-    if not encoder then return end
+    if not encoder then
+      error('No encoder for given type of value.')
+    end
 
-    local is_ok, as_str = encoder(Value)
-
-    -- Encoder failed?
-    if not is_ok then return end
-
-    return as_str
+    return encoder(Value)
   end
 
 -- Export:
@@ -121,4 +70,5 @@ return value_to_str
 --[[
   2026-06-17
   2026-06-18
+  2026-06-20
 ]]
