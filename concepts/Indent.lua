62c7c608f1a45e4a734f4a0e314ab8d4a78108f0
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-05 22:31:46 +0200

    [/] [Indent] Indent chunk is now unnamed field. Added accessors.

--- concepts/Indent.lua
+++ concepts/Indent.lua
@@ -2,7 +2,7 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-11
+  Last mod.: 2026-07-05
 ]]
 
 -- Imports:
@@ -18,16 +18,36 @@ RangePoint.value = 0
 
 Core =
   {
-    indent_chunk = '  ',
+    '  ',
     RangePoint = RangePoint,
   }
 
 local Interface
 Interface =
   {
+    GetIndentChunk =
+      function(Me)
+        return Me[1]
+      end,
+
+    SetIndentChunk =
+      function(Me, str)
+        assert_string(str)
+
+        Me[1] = str
+      end,
+
+    GetRangePoint =
+      function(Me)
+        return Me.RangePoint
+      end,
+
     ToString =
       function(Me)
-        return string.rep(Me.indent_chunk, Me:GetRangePoint():GetValue())
+        local indent_chunk = Me:GetIndentChunk()
+        local indent_level = Me:GetRangePoint():GetValue()
+
+        return string.rep(indent_chunk, indent_level)
       end,
 
     Inc =
@@ -43,11 +63,6 @@ Interface =
       function(OptCore)
         return create_instance(OptCore or Core, Interface)
       end,
-
-    GetRangePoint =
-      function(Me)
-        return Me.RangePoint
-      end,
   }
 
 -- Export:
@@ -55,4 +70,5 @@ return Interface
 
 --[[
   2026-05-11
+  2026-07-05
 ]]
