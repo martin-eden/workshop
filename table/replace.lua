6db57ddb3c2a3e216f2fa6c11acb8fa7b7a3c32f
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-12 06:57:54 +0200

    [^~] [table.replace] Now uses [apply_table].

--- table/replace.lua
+++ table/replace.lua
@@ -1,12 +1,29 @@
-return
-  function(dest, src)
-    assert_table(dest)
-    assert_table(src)
-    assert(src ~= dest)
-    for k in pairs(dest) do
-      dest[k] = nil
-    end
-    for k, v in pairs(src) do
-      dest[k] = v
-    end
+-- Replace contents of table
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-07-12
+]]
+
+-- Imports:
+local apply_table = request('apply_table')
+
+local Rules =
+  {
+    { has_a = true, has_b = true, action = 'replace' },
+    { has_a = false, has_b = true, action = 'replace' },
+    { has_a = true, has_b = false, action = 'remove' },
+  }
+
+local replace_table =
+  function(Dest, Src)
+    apply_table(Dest, Src, Rules)
   end
+
+-- Export:
+return replace_table
+
+--[[
+  2018
+  2026-07-12
+]]
