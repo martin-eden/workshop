dcb1e78221fbffd887011ecd48d1214a573de67f
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-14 04:37:09 +0200

    [-] [concepts.tsv] Removed "old" TSV codec..
    Use "new" [concepts.codec_tsv].
    
    This code was written 2017-09. Originally it was simple as it should be.
    But lacked documentation and clear interface.
    
    Somewhy I consider simple code as target for design experiments.
    So I wrote CSV codec and based it's interface on this.
    
    And then I added check/fix pattern (check() to verify data and fix()
    to validate it). There was no real need for this but I was testing
    concept. Module remained unused and now with murky code.
    
    So 2026-07 I saw this and just rewrote code.

--- concepts/tsv/parse/specific/parse.lua
+++
@@ -1,9 +0,0 @@
-return
-  function(self)
-    local result = {}
-    repeat
-      local rec = self:get_next_rec()
-      result[#result + 1] = rec
-    until not rec
-    return result
-  end
--- concepts/tsv/save.lua
+++
@@ -1,8 +0,0 @@
-local saver_class = request('compile.specific')
-
-return
-  function(t)
-    local saver = new(saver_class)
-    saver:init(t)
-    return saver:serialize()
-  end
