70df967eab1f3104b70b75b9d2737aa2fbaf150e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-17 17:43:00 +0200

    [^] [lua_bytecode_decompiler.bytecode_from_source] Removes shebang.

--- concepts/lua_bytecode_decompiler/bytecode_from_source.lua
+++ concepts/lua_bytecode_decompiler/bytecode_from_source.lua
@@ -2,10 +2,11 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-07-15
+  Last mod.: 2026-07-17
 ]]
 
 -- Imports:
+local split_shebang = request('!.concepts.shell.split_shebang')
 local bytecode_from_function = request('bytecode_from_function')
 
 local compile_source = load
@@ -13,6 +14,8 @@ local compile_source = load
 local bytecode_from_source =
   function(source_code_str)
     assert_string(source_code_str)
+
+    local shebang_str, source_code_str = split_shebang(source_code_str)
 
     local lua_func = compile_source(source_code_str)
 
@@ -26,4 +29,5 @@ return bytecode_from_source
 
 --[[
   2026-07-15
+  2026-07-17
 ]]
