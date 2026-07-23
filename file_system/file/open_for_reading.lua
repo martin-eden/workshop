722fde6c7fc427124e938a91f39d03aca5ad078f
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-23 19:07:18 +0200

    [/] [file.open_for_reading] Now explodes in case of errors..
    Caller should check surroundings before call.

--- file_system/file/open_for_reading.lua
+++ file_system/file/open_for_reading.lua
@@ -2,17 +2,19 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-27
+  Last mod.: 2026-07-23
 ]]
 
 --[[
-  If there is error opening file -- returns nil.
-  Else returns file object.
+  If there is error opening file -- explodes. Else returns file object.
 ]]
+
+-- Imports:
+local open_file = request('open')
 
 local open_for_reading =
   function(pathname)
-    return ( io.open(pathname, 'rb') )
+    return open_file(pathname, 'rb')
   end
 
 -- Export:
@@ -21,4 +23,5 @@ return open_for_reading
 --[[
   2024-08-09
   2026-05-27
+  2026-07-23
 ]]
