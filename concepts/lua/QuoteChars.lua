5ef68ea98ebb82942987267d676262af32996eb1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-01 22:57:08 +0200

    [~] [lua.QuoteChars] Using [Ascii].

--- concepts/lua/QuoteChars.lua
+++ concepts/lua/QuoteChars.lua
@@ -2,28 +2,25 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-07-12
+  Last mod.: 2026-08-01
 ]]
 
-local str_char = string.char
+local QuoteChars
+do
+  local Ascii = request('!.concepts.Ascii')
+  QuoteChars =
+    {
+      newline_code = Ascii.Codes.newline,
+      single_quote_code = Ascii.Codes.single_quote,
+      double_quote_code = Ascii.Codes.double_quote,
+      backslash_code = Ascii.Codes.backslash,
 
-local newline_code = 10
-local single_quote_code = 39
-local double_quote_code = 34
-local backslash_code = 92
-
-local QuoteChars =
-  {
-    newline_code = newline_code,
-    single_quote_code = single_quote_code,
-    double_quote_code = double_quote_code,
-    backslash_code = backslash_code,
-
-    newline = str_char(newline_code),
-    single_quote = str_char(single_quote_code),
-    double_quote = str_char(double_quote_code),
-    backslash = str_char(backslash_code),
-  }
+      newline = Ascii.Chars.newline,
+      single_quote = Ascii.Chars.single_quote,
+      double_quote = Ascii.Chars.double_quote,
+      backslash = Ascii.Chars.backslash,
+    }
+end
 
 -- Export:
 return QuoteChars
