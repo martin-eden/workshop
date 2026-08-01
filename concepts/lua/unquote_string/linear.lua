e25d4a5970a25e526b7d5af32fa789b4c6a1d6e6
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-02 01:06:54 +0200

    [^] [lua.unquote_string.linear] Code composition.

--- concepts/lua/unquote_string/linear.lua
+++ concepts/lua/unquote_string/linear.lua
@@ -1,109 +1,98 @@
--- Unquote Lua string quoted using backslashes
+-- Unquote Lua string that is quoted with backslashes
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-04-24
+  Last mod.: 2026-08-01
 ]]
 
-local one_char_seq_subst =
-  {
-    [ [[\a]] ] = '\007',
-    [ [[\b]] ] = '\008',
-    [ [[\f]] ] = '\x0c',
-    [ [[\n]] ] = '\x0a',
-    [ [[\r]] ] = '\x0d',
-    [ [[\t]] ] = '\x09',
-    [ [[\v]] ] = '\x0b',
-    [ [[\"]] ] = '"',
-    [ [[\']] ] = "'",
-    [ [[\]] .. '\n'] = '\n',
-  }
+-- Imports:
+local Ascii = request('!.concepts.Ascii')
 
-local space_char = '[\x00-\x20]'
-local space_chars_seq = ([[\z%s*]]):format(space_char)
+local str_to_int = tonumber
+local str_char = string.char
+local utf8_char = utf8.char
+local str_gsub = string.gsub
 
-local dec_code_seq = [[\(%d%d?%d?)]]
-local decode_dec_code =
-  function(code_str)
-    local dec_code = tonumber(code_str)
-    local result
-    -- dec_code is 0..999 by pattern design
-    if (dec_code <= 255) then
-      result = string.char(dec_code)
-    else
-      -- Hmm, it's invalid escape sequence. Do not change in this case.
+local unescape
+do
+  local one_char_seq_subst =
+    {
+      [ [[\a]] ] = Ascii.Chars.bell,
+      [ [[\b]] ] = Ascii.Chars.backspace,
+      [ [[\f]] ] = Ascii.Chars.form_feed,
+      [ [[\n]] ] = Ascii.Chars.newline,
+      [ [[\r]] ] = Ascii.Chars.carriage_return,
+      [ [[\t]] ] = Ascii.Chars.tab,
+      [ [[\v]] ] = Ascii.Chars.vertical_tab,
+      [ [[\"]] ] = Ascii.Chars.double_quote,
+      [ [[\']] ] = Ascii.Chars.single_quote,
+      [ [[\]] .. Ascii.Chars.newline] = Ascii.Chars.newline,
+    }
+
+  local space_chars_seq = [[\z[\000-\032]*]]
+
+  local dec_code_seq = [[\(%d%d?%d?)]]
+  local decode_dec_code =
+    function(code_str)
+      return str_char(str_to_int(code_str, 10))
     end
-    return result
-  end
 
-local hex_code_seq = [[\x(%x%x)]]
-local decode_hex_code =
-  function(code_str)
-    local dec_code = tonumber(code_str, 16)
-    -- dec_code is 0..255 by pattern design as it's from 2 hex digits
-    return string.char(dec_code)
-  end
-
-local utf_code_seq = [[\u{(%x+)}]]
-local decode_utf_code =
-  function(code_str)
-    local result
-    if (#code_str > 6) then
-      -- bad code sequence
-    else
-      local dec_code = tonumber(code_str, 16)
-      if (dec_code > 0x1FFFFF) then
-        -- bad code sequence also
-      else
-        result = utf8.char(dec_code)
-      end
+  local hex_code_seq = [[\x(%x%x)]]
+  local decode_hex_code =
+    function(code_str)
+      return str_char(str_to_int(code_str, 16))
     end
-    return result
-  end
 
-local split = request('!.string.split')
+  local utf_code_seq = [[\u{(%x+)}]]
+  local decode_utf_code =
+    function(code_str)
+      return utf8_char(str_to_int(code_str, 16))
+    end
 
-local unescape =
-  function(s)
-    s = s:gsub([[\.]], one_char_seq_subst)
-    s = s:gsub(space_chars_seq, '')
-    s = s:gsub(dec_code_seq, decode_dec_code)
-    s = s:gsub(hex_code_seq, decode_hex_code)
-    s = s:gsub(utf_code_seq, decode_utf_code)
-    return s
-  end
+  unescape =
+    function(str)
+      str = str_gsub(str, [[\.]], one_char_seq_subst)
+      str = str_gsub(str, space_chars_seq, '')
+      str = str_gsub(str, dec_code_seq, decode_dec_code)
+      str = str_gsub(str, hex_code_seq, decode_hex_code)
+      str = str_gsub(str, utf_code_seq, decode_utf_code)
 
-local backslash = [[\]]
-local double_backslash = backslash .. backslash
+      return str
+    end
+end
 
 -- Unquote Lua string quoted in linear encoding (backslashes)
-local unqote_string =
-  function(s)
-    if s:find(double_backslash) then
-      --[[
-        I believe there was tricky case with unquoting quoted slash
+local unquote_string
+do
+  local backslash = Ascii.Chars.backslash
+  local double_backslash = backslash .. backslash
 
-        When it's present we will split string into list of
-        chunks separated by quoted slash. Then unquote chunks.
-        Then catenate list to string inserting slash between chunks.
-      ]]
-      s = s .. double_backslash
-      local parts = split(s, double_backslash)
-      for i = 1, #parts do
-        parts[i] = unescape(parts[i])
+  local str_find = string.find
+  local str_split = request('!.string.split')
+  local list_to_str = request('!.concepts.list.to_string')
+
+  unquote_string =
+    function(str)
+      if str_find(str, double_backslash) then
+        -- Always correct
+        local Parts = str_split(str, double_backslash)
+        for index, part_str in ipairs(Parts) do
+          Parts[index] = unescape(part_str)
+        end
+        str = list_to_str(Parts, backslash)
+      else
+        -- Fast
+        str = unescape(str)
       end
-      s = table.concat(parts, backslash)
-    else
-      s = unescape(s)
+      return str
     end
-    return s
-  end
+end
 
 -- Export:
-return unqote_string
+return unquote_string
 
 --[[
   2017 # #
-  2026-04-22
-  2026-04-24
+  2026-04 # #
+  2026-08-01
 ]]
