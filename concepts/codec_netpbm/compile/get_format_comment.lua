e9da7560adee7fa5e0b2a27fc2dbe09ddf668cfc
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-08 16:41:04 +0200

    [.^] [codec_netpbm.get_format_comment] Code composition.

--- concepts/codec_netpbm/compile/get_format_comment.lua
+++ concepts/codec_netpbm/compile/get_format_comment.lua
@@ -5,24 +5,35 @@
   Last mod.: 2026-08-08
 ]]
 
-local FormatComments
+local get_format_comment
 do
-  local Syntels = request('^.Syntels')
+  local FormatComments
+  do
+    local monochrome_label
+    local grayscale_label
+    local color_label
+    do
+      local Syntels = request('^.Syntels')
+      monochrome_label = Syntels.monochrome_label
+      grayscale_label = Syntels.grayscale_label
+      color_label = Syntels.color_label
+    end
 
-  FormatComments =
-    {
-      [Syntels.monochrome_label] = 'Monochrome image, text format',
-      [Syntels.grayscale_label] = 'Grayscale image, text format',
-      [Syntels.color_label] = 'Color image, text format',
-    }
-end
-
-local get_format_comment =
-  function(format_label)
-    local comment = FormatComments[format_label]
-    assert(comment, 'Format label is not found.')
-    return comment
+    FormatComments =
+      {
+        [monochrome_label] = 'Monochrome image, text format',
+        [grayscale_label] = 'Grayscale image, text format',
+        [color_label] = 'Color image, text format',
+      }
   end
+
+  get_format_comment =
+    function(format_label)
+      local comment = FormatComments[format_label]
+      assert(comment, 'Format label is not found.')
+      return comment
+    end
+end
 
 -- Export:
 return get_format_comment
