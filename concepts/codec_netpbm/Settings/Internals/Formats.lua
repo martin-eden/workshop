82c3b49a375a1613e957a136121e3ea42cf8bc32
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-08 16:33:54 +0200

    [~] [codec_netpbm.Formats] Using [Syntels].

--- concepts/codec_netpbm/Settings/Internals/Formats.lua
+++ concepts/codec_netpbm/Settings/Internals/Formats.lua
@@ -2,24 +2,37 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-06-15
+  Last mod.: 2026-08-08
 ]]
 
-local Formats =
-  {
+local Formats
+do
+  local monochrome_label
+  local grayscale_label
+  local color_label
+  do
+    local Syntels = request('^.^.Syntels')
+    monochrome_label = Syntels.monochrome_label
+    grayscale_label = Syntels.grayscale_label
+    color_label = Syntels.color_label
+  end
+
+  Formats =
     {
-      label = 'P1',
-      Settings = { num_channels = 1, num_channel_values = 2 },
-    },
-    {
-      label = 'P2',
-      Settings = { num_channels = 1 },
-    },
-    {
-      label = 'P3',
-      Settings = { num_channels = 3 },
-    },
-  }
+      {
+        label = monochrome_label,
+        Settings = { num_channels = 1, num_channel_values = 2 },
+      },
+      {
+        label = grayscale_label,
+        Settings = { num_channels = 1 },
+      },
+      {
+        label = color_label,
+        Settings = { num_channels = 3 },
+      },
+    }
+end
 
 -- Export:
 return Formats
