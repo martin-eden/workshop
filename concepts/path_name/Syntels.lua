6e0b4ae72d340543ad0fe8264a66f7a9a0e731d9
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-08 14:31:47 +0200

    [+] [path_name.Syntels]

---
+++ concepts/path_name/Syntels.lua
@@ -0,0 +1,21 @@
+-- Special pathname syntax elements
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-08-08
+]]
+
+local Syntels =
+  {
+    separator = '/',
+    empty = '',
+    self_dir = '.',
+    upper_dir = '..',
+  }
+
+-- Export:
+return Syntels
+
+--[[
+  2026-08-08
+]]
