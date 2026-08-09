9ce6cf5da54f115c24beef906756a1c748f6d2f5
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-09 15:38:00 +0200

    [^~] [json.load.via_parser] Code revisit. Fixed broken data contract..
    Somewhere in past parser started to return data tree with all
    data, not only data tree with annotated nodes.
    
    Analyzed and updated code to handle this.

--- concepts/json/load/via_parser/struc_to_lua.lua
+++ concepts/json/load/via_parser/struc_to_lua.lua
@@ -1,46 +1,133 @@
-local unquote_string = request('^.unquote_string')
-local to_array = request('!.mechs.array.from_table')
+-- Convert data tree from parser to Lua structure
 
-return
-  function(data_struc)
-    assert_table(data_struc)
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-08-09
+]]
 
-    local struc_to_lua
-    struc_to_lua =
-      function(node)
-        local result
-        if (node.type == 'object') then
-          result = {}
-          local i = 1
-          while (i + 1 <= #node) do
-            local key = struc_to_lua(node[i])
-            local value = struc_to_lua(node[i + 1])
-            result[key] = value
-            i = i + 2
-          end
-        elseif (node.type == 'array') then
-          result = {}
-          for i = 1, #node do
-            result[i] = struc_to_lua(node[i])
-          end
-          to_array(result, #node)
-        elseif (node.type == 'string') then
-          result = unquote_string(node.value)
-        elseif (node.type == 'number') then
-          result = tonumber(node.value)
-        elseif (node.type == 'boolean') then
-          local value = node.value
-          if (value == 'true') then
-            result = true
-          elseif (value == 'false') then
-            result = false
-          end
-        elseif (node.type == 'null') then
-          result = nil
+--[=[
+  Job of this code is convert input data from parser (tree)
+  to Lua structure.
+
+  JSON is similar to Lua, so JSON objects and arrays become
+  Lua tables and other types are native to Lua too.
+
+  Implementation caveat is input data tree.
+
+  For JSON [[{ "list": ]] tree will start like
+
+    {
+      [1] = '{ ',
+      [2] = {
+        [1] = '"list"',
+        type = 'string',
+      },
+      [3] = ': ',
+    }
+
+  So some data nodes are annotated in syntax (and we need them),
+  some nodes are plain strings (we don't need them, they will
+  be recreated by formatter).
+]=]
+
+local struc_to_lua
+do
+  local unquote_string = request('^.unquote_string')
+  local to_array = request('!.mechs.array.from_table')
+
+  -- Get next names node and return it and next unchecked index
+  local get_next_node =
+    function(data_struc, pos)
+      while true do
+        if is_nil(data_struc[pos]) then
+          return
         end
 
-        return result
-      end
+        if is_string(data_struc[pos].type) then
+          return data_struc[pos], pos + 1
+        end
 
-    return struc_to_lua(data_struc)
-  end
+        pos = pos + 1
+      end
+    end
+
+  struc_to_lua =
+    function(data_struc)
+      assert_table(data_struc)
+
+      local struc_to_lua
+      struc_to_lua =
+        function(Node)
+          assert_table(Node)
+
+          local Result
+
+          if (Node.type == 'object') then
+            Result = { }
+
+            local KeyNode
+            local ValueNode
+            local pos = 1
+
+            while true do
+              KeyNode, pos = get_next_node(Node, pos)
+              ValueNode, pos = get_next_node(Node, pos)
+
+              if not ValueNode then break end
+
+              local key = struc_to_lua(KeyNode)
+              local value = struc_to_lua(ValueNode)
+
+              Result[key] = value
+            end
+          elseif (Node.type == 'array') then
+            Result = { }
+
+            local ValueNode
+            local pos = 1
+            local num_items = 0
+
+            while true do
+              ValueNode, pos = get_next_node(Node, pos)
+
+              if not ValueNode then break end
+
+              local value = struc_to_lua(ValueNode)
+
+              num_items = num_items + 1
+              Result[num_items] = value
+            end
+            to_array(Result, num_items)
+          elseif (Node.type == 'string') then
+            Result = unquote_string(Node[1])
+          elseif (Node.type == 'number') then
+            Result = tonumber(Node[1])
+          elseif (Node.type == 'boolean') then
+            local value = Node[1]
+            if (value == 'true') then
+              Result = true
+            elseif (value == 'false') then
+              Result = false
+            end
+          elseif (Node.type == 'null') then
+            Result = nil
+          else
+            error()
+          end
+
+          return Result
+        end
+
+      return struc_to_lua(data_struc)
+    end
+end
+
+-- Export:
+return struc_to_lua
+
+--[[
+  2016
+  2017
+  2018
+  2026-08-09
+]]
