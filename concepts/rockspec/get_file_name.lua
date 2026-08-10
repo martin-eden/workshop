fc972b3eddccb3e9829e0314c2569f218a2f9227
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-10 16:47:25 +0200

    [<] [rockspec.get_file_name] Moved into [fill_modules].

--- concepts/rockspec/get_file_name.lua
+++
@@ -1,7 +0,0 @@
-local strip_updirs = request('!.string.file_name.strip_updirs')
-local strip_self_prefix = request('!.string.file_name.strip_self_prefix')
-
-return
-  function(file_name)
-    return strip_self_prefix(strip_updirs(file_name))
-  end
