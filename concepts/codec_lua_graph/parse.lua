651f53c3a1610a8390421d434543af3902e8303d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-11 16:06:32 +0200

    [+] [codec_lua_graph.parse] Support for loading strings with expressions.

--- concepts/codec_lua_graph/parse.lua
+++ concepts/codec_lua_graph/parse.lua
@@ -2,12 +2,32 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-06-17
+  Last mod.: 2026-08-11
+]]
+
+--[[
+  Input string can contain Lua expression or Lua statements
+
+    * String with Lua statements is produced by [compile_graph].
+      F.e. "return { }".
+
+    * String with Lua expression is produced by [compile_tree].
+      F.e. "{ }"
+
+  Instead of asking caller to specify which kind he is giving us,
+  we're trying first to load string as chunk and if it fails,
+  retrying to load it as expression.
+
+  This moves cost of distinguishing them from caller to us.
 ]]
 
 local parse =
   function(str)
     local f = load(str)
+
+    if not f then
+      f = load('return ' .. str)
+    end
 
     if not f then return end
 
@@ -20,4 +40,5 @@ return parse
 --[[
   2016
   2026-06-17
+  2026-08-11
 ]]
