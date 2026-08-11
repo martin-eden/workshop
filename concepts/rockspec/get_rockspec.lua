788edd50b9f3abcd669133ec8eb7c2b4fe0ce09a
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-11 17:56:15 +0200

    [.] [rockspec.get_rockspec]

--- concepts/rockspec/get_rockspec.lua
+++ concepts/rockspec/get_rockspec.lua
@@ -1,3 +1,11 @@
+-- Return string with .rockspec file contents
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-08-11
+]]
+
+-- Imports:
 local fill_modules = request('fill_modules')
 local get_modules_text = request('get_modules_text')
 local get_wrappers_text = request('get_wrappers_text')
@@ -5,28 +13,37 @@ local rockspec_template = request('rockspec_template')
 local fill_template = request('fill_template')
 local quote = request('!.string.quote')
 
-return
-  function(cfg)
-    local modules = fill_modules(cfg)
-    local wrappers
-    if cfg.commands then
-      wrappers = {}
-      for _, rec in ipairs(cfg.commands) do
-        wrappers[rec.command] = rec.wrapper
+local get_rockspec =
+  function(Config)
+    local Modules = fill_modules(Config)
+
+    local Wrappers = { }
+    if Config.commands then
+      for _, Rec in ipairs(Config.commands) do
+        Wrappers[Rec.command] = Rec.wrapper
       end
     end
-    local substitutions =
+
+    local Substitutions =
       {
-        package = quote(cfg.project_name),
-        repository_url = quote(cfg.repository.url),
-        repository_branch = quote(cfg.repository.branch),
-        short_desc = quote(cfg.short_desc),
-        description = quote(cfg.description),
-        license = quote(cfg.license),
-        wrappers = get_wrappers_text(wrappers),
-        modules = get_modules_text(modules),
+        package = quote(Config.project_name),
+        repository_url = quote(Config.repository.url),
+        repository_branch = quote(Config.repository.branch),
+        short_desc = quote(Config.short_desc),
+        description = quote(Config.description),
+        license = quote(Config.license),
+        wrappers = get_wrappers_text(Wrappers),
+        modules = get_modules_text(Modules),
       }
-    local result = fill_template(rockspec_template, substitutions)
+
+    local result = fill_template(rockspec_template, Substitutions)
+
     return result
   end
 
+-- Export:
+return get_rockspec
+
+--[[
+  2017 # # # #
+]]
