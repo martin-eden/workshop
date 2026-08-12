c864526db03e3a0b42049a1d8f312bc8705511bc
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-12 15:58:05 +0200

    [/] [get_program_output_lines] Renamed to [get_command_output_lines].

--- system/get_command_output_lines.lua
+++ system/get_command_output_lines.lua
@@ -8,7 +8,7 @@
 -- Imports:
 local lines_from_str = request('!.convert.lines_from_str')
 
-local get_program_output_lines =
+local get_command_output_lines =
   function(Command)
     local is_ok, ExecResult = Command:Execute()
 
@@ -16,11 +16,10 @@ local get_program_output_lines =
   end
 
 -- Export:
-return get_program_output_lines
+return get_command_output_lines
 
 --[[
   2017 #
   2024 # #
-  2026 #
-  2026-08-12
+  2026 # #
 ]]
