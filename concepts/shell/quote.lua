93cfb38eb1bb41ccaf117ea1826318ef135d1b09
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-08-13 19:16:05 +0200

    [.^] [shell.quote] Code revisit.

--- concepts/shell/quote.lua
+++ concepts/shell/quote.lua
@@ -1,8 +1,8 @@
--- Encode string to shell
+-- Encode string to shell syntax
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-06-09
+  Last mod.: 2026-08-13
 ]]
 
 --[[
@@ -15,78 +15,77 @@
     a b -> 'a b'
 ]]
 
--- Imports:
+local empty = ''
+local single_quote
+local backslash
+do
+  local Ascii = request('!.concepts.Ascii.Chars')
+  single_quote = Ascii.single_quote
+  backslash = Ascii.backslash
+end
+
 local list_to_str = request('!.concepts.list.to_string')
+local str_find = string.find
+
+local needs_quoting
+do
+  local special_chars_regexp
+  local starts_with_comment_regexp
+  do
+    local quote_regexp = request('!.lua.regexp.quote')
+    do
+      local SpecialChars = request('quote.SpecialChars')
+      local special_chars_str = list_to_str(SpecialChars)
+      special_chars_regexp = '[' .. quote_regexp(special_chars_str) .. ']'
+    end
+    do
+      local SpaceChars = request('quote.SpaceChars')
+      local space_chars_str = list_to_str(SpaceChars)
+      local space_chars_regexp = '[' .. quote_regexp(space_chars_str) .. ']'
+      local comment_char = '#'
+      starts_with_comment_regexp = '^' .. space_chars_regexp .. '+' .. comment_char
+    end
+  end
+  needs_quoting =
+    function(str)
+      return
+        (str == empty) or
+        not is_nil(str_find(str, special_chars_regexp)) or
+        not is_nil(str_find(str, starts_with_comment_regexp))
+    end
+end
+
 local split_string = request('!.string.split')
 local add_to_list = request('!.concepts.list.add_item')
 
-local special_chars_regexp
-local starts_with_comment_regexp
-do
-  -- Imports:
-  local quote_regexp = request('!.lua.regexp.quote')
+--[[
+  * We're quoting only when it's needed
 
-  do
-    -- Imports:
-    local SpecialChars = request('quote.SpecialChars')
+    F.e. [a;b] is quoted. But [a=b] is not.
 
-    local special_chars_str = list_to_str(SpecialChars)
+    Or [a#] is not quoted but [#a] can be treated as comment and
+    so it is quoted.
 
-    special_chars_regexp = '[' .. quote_regexp(special_chars_str) .. ']'
-  end
+  * We're using single-quotes
 
-  do
-    -- Imports:
-    local SpaceChars = request('quote.SpaceChars')
+    Happy case is when string does not contain ['].
+    Then we just surround it with ['].
 
-    local space_chars_str = list_to_str(SpaceChars)
+    If string contains ['] we'll split it by ['],
+    quote parts, and glue with [\'].
 
-    local space_chars_regexp = '[' .. quote_regexp(space_chars_str) .. ']'
+  * ['] can be represented as [\'] or as ["'"] or as [\039] or as ...
 
-    local comment_char = '#'
-
-    starts_with_comment_regexp = '^' .. space_chars_regexp .. '+' .. comment_char
-  end
-end
-
-local needs_quoting =
-  function(str)
-    return
-      (str == '') or
-      not is_nil(string.find(str, special_chars_regexp)) or
-      not is_nil(string.find(str, starts_with_comment_regexp))
-  end
-
+    We'll use [\'].
+]]
 local quote
 quote =
   function(str)
-    --[[
-      We're quoting only when it's needed
-
-      F.e. "a;b" is quoted. But "a=b" is not.
-
-
-      We're using single-quotes
-
-      Happy case is when string does not contain "'".
-      Then we just surround string with "'".
-
-      If string contains "'" we split it by "'",
-      quote parts, and glue with escaped "'".
-
-
-      "'" can be represented as [\'] or as ["'"] or as [\039] or as ...
-
-      We'll use [\'].
-    ]]
-
     assert_string(str)
 
     if not needs_quoting(str) then return str end
 
-    local single_quote = "'"
-
-    if not string.find(str, single_quote) then
+    if not str_find(str, single_quote) then
       return single_quote .. str .. single_quote
     end
 
@@ -97,9 +96,9 @@ quote =
 
     for _, item in ipairs(RawItems) do
       local quoted_item
-      if (item == '') then
+      if (item == empty) then
         -- Empty string is part here. No need to quote it
-        quoted_item = ''
+        quoted_item = empty
       else
         quoted_item = quote(item)
       end
@@ -107,7 +106,7 @@ quote =
       add_to_list(Items, quoted_item)
     end
 
-    return list_to_str(Items, [[\]] .. single_quote)
+    return list_to_str(Items, backslash .. single_quote)
   end
 
 return quote
