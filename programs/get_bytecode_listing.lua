be03822b3f5e6aff72f3595e6d46e9a7d1100a81
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-01 17:06:41 +0200

    [~] [programs.get_bytecode_listing]

--- programs/get_bytecode_listing.lua
+++ programs/get_bytecode_listing.lua
@@ -8,11 +8,13 @@
 --[[
   Input
 
-    [s] -- name of Lua source file
+    [t] -- arguments
+      1 [s] -- name of Lua source file
+    [t] -- output stream
 
   Output
 
-    [s] -- bytecode listing in Itness (strings tree) format
+    Bytecode listing in Itness (strings tree) format
 ]]
 
 local file_to_str = request('!.convert.file_to_str')
@@ -20,15 +22,14 @@ local get_bytecode =
   request('!.concepts.lua_bytecode_decompiler.bytecode_from_source')
 local get_listing =
   request('!.concepts.lua_bytecode_decompiler.listing_from_bytecode')
-local StdoutStream = request('!.concepts.StreamIo.Output.Pipe')
 local itness_to_stream = request('!.concepts.codec_itness.compile')
 
 -- Export:
 return
-  function(source_file_name)
+  function(Args, OutputStream)
     itness_to_stream(
-      get_listing(get_bytecode(file_to_str(source_file_name))),
-      StdoutStream
+      get_listing(get_bytecode(file_to_str(Args[1]))),
+      OutputStream
     )
   end
 
