04ce55d1b19acdc4f25d70e0c60b86cec4ee0b06
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-08 16:42:18 +0200

    [+] [path_name.pathname_to_str:] Now removes possible tail slashes.

--- concepts/path_name/pathname_to_str.lua
+++ concepts/path_name/pathname_to_str.lua
@@ -2,18 +2,46 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-08-29
+  Last mod.: 2026-09-08
 ]]
 
 local sep = request('Syntels').separator
+
+local clean_pathname
+do
+  local clean_name
+  do
+    local ends_with = request('!.string.ends_with')
+    local str_sub = string.sub
+    clean_name =
+      function(name)
+        local sep_len = #sep
+        while ends_with(name, sep) do
+          name = str_sub(name, 1, -(sep_len + 1))
+        end
+        return name
+      end
+  end
+  local add_to_list = request('!.concepts.list.add_item')
+  clean_pathname =
+    function(Pathname)
+      local Result = { }
+      for _, name in ipairs(Pathname) do
+        add_to_list(Result, clean_name(name))
+      end
+      return Result
+    end
+end
+
 local list_to_str = request('!.concepts.list.to_string')
 
 -- Export:
 return
   function(Pathname)
-    return list_to_str(Pathname, sep)
+    return list_to_str(clean_pathname(Pathname), sep)
   end
 
 --[[
   2026 #
+  2026-09-08
 ]]
