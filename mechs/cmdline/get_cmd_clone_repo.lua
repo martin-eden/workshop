bc10ce7e8e17ed2cd29b32bfb25ed8602f0432a0
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-08 15:56:15 +0200

    [.] [get_cmd_clone_repo]

--- mechs/cmdline/get_cmd_clone_repo.lua
+++ mechs/cmdline/get_cmd_clone_repo.lua
@@ -2,7 +2,7 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-08-12
+  Last mod.: 2026-09-08
 ]]
 
 -- Imports:
@@ -10,32 +10,21 @@ local add_separator = request('!.concepts.path_name.add_separator')
 local normalize = request('!.concepts.path_name.normalize')
 local ShellCommand = request('!.concepts.ShellCommand')
 
-local get_cmd_clone_repo =
+-- Export:
+return
   function(url, dest_dir)
     assert_string(url)
     assert_string(dest_dir)
 
     assert(dest_dir ~= '')
 
-    dest_dir = add_separator(dest_dir)
-    dest_dir = normalize(dest_dir)
+    dest_dir = normalize(add_separator(dest_dir))
 
     local Command =
-      {
-        'git',
-        {
-          'clone',
-          url,
-          dest_dir,
-          '--quiet',
-        }
-      }
+      { 'git', { 'clone', url, dest_dir, '--quiet' } }
 
     return ShellCommand.create(Command)
   end
-
--- Export:
-return get_cmd_clone_repo
 
 --[[
   2026 # #
