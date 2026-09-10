20eef178c92f40aaae6f089446eb0ff784532fb7
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-10 22:50:59 +0200

    [/] [path_name.get_host_dir] Interface change. Returning list, not str.

--- concepts/path_name/get_host_dir.lua
+++ concepts/path_name/get_host_dir.lua
@@ -1,51 +1,50 @@
--- Return string with host directory for pathname list
+-- Return list with host directory for pathname list
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-08-29
+  Last mod.: 2026-09-10
 ]]
 
+local self_dir = request('Syntels').self_dir
 local is_directory = request('is_directory')
-local tbl_concat = table.concat
 local is_absolute = request('is_absolute')
-
-local sep
-local self_dir
-do
-  local Syntels = request('Syntels')
-  sep = Syntels.separator
-  self_dir = Syntels.self_dir
-end
+local select_seg = request('!.concepts.list.select_seg')
+local add_to_list = request('!.concepts.list.add_item')
 
 -- Export:
 return
   function(Pathname)
-    local host_dir_name
-    do
-      local host_dir_end_idx
-      if is_directory(Pathname) then
-        host_dir_end_idx = #Pathname - 2
-      else
-        host_dir_end_idx = #Pathname - 1
-      end
-      host_dir_name = tbl_concat(Pathname, sep, 1, host_dir_end_idx)
-      if (host_dir_name == '') then
-        --[[
-          Cases when we can come here: ".", "..", "/", "abc".
-          We need to provide host dir for them.
-          Zen question: what is the name of "parent dir" for ".."?
-          That's why we're using name "host", not "parent".
-        ]]
-        if not is_absolute(Pathname) then
-          host_dir_name = self_dir
-        end
-      end
-      host_dir_name = host_dir_name .. sep
+    local HostDir
+
+    local host_dir_end_idx
+    if is_directory(Pathname) then
+      host_dir_end_idx = #Pathname - 2
+    else
+      host_dir_end_idx = #Pathname - 1
     end
 
-    return host_dir_name
+    if (host_dir_end_idx <= 0) then
+      --[[
+        Cases when we can come here: ".", "..", "/", "abc".
+        We need to provide host dir for them.
+        Zen question: what is the name of "parent dir" for ".."?
+        That's why we're using name "host", not "parent".
+      ]]
+      if is_absolute(Pathname) then
+        HostDir = { '' }
+      else
+        HostDir = { self_dir }
+      end
+    else
+      HostDir = select_seg(Pathname, 1, host_dir_end_idx)
+    end
+
+    add_to_list(HostDir, '')
+
+    return HostDir
   end
 
 --[[
   2026 #
+  2026-09-10
 ]]
