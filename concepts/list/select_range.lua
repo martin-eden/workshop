5d4c9cc0ecc91ae1e8368e5b99d044d1243cd360
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-10 21:53:39 +0200

    [.] [list.select_range] Code revisit.

--- concepts/list/select_range.lua
+++ concepts/list/select_range.lua
@@ -2,27 +2,24 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-05-02
+  Last mod.: 2026-09-10
 ]]
 
--- Imports:
 local is_natural = request('!.number.is_natural')
+local tbl_move = table.move
 
-local select_range =
+-- Export:
+return
   function(List, start_idx, stop_idx)
     assert(is_natural(start_idx))
     assert(is_natural(stop_idx))
     assert(start_idx <= stop_idx)
 
     local Result = { }
-
-    table.move(List, start_idx, stop_idx, 1, Result)
+    tbl_move(List, start_idx, stop_idx, 1, Result)
 
     return Result
   end
-
--- Export:
-return select_range
 
 --[[
   2026-05-02
