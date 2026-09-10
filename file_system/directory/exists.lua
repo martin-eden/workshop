2a5118752274ae67348ba880e21cfa246446b96d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-10 19:39:42 +0200

    [~] [directory.exists] Using [get_cmd_dir_exists].

--- file_system/directory/exists.lua
+++ file_system/directory/exists.lua
@@ -2,46 +2,18 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-06-12
+  Last mod.: 2026-09-10
 ]]
 
--- Imports:
-local normalize_name = request('!.concepts.path_name.normalize')
-
-local is_directory =
-  function(dir_name)
-    --[[
-      Directory is opened as file
-
-      But when we reading from it io.read() returns error.
-    ]]
-
-    assert_string(dir_name)
-
-    dir_name = normalize_name(dir_name)
-
-    local file = io.open(dir_name, 'rb')
-
-    -- No such thing with this name
-    if is_nil(file) then
-      return false
-    end
-
-    -- Trying to read
-    local _, err_str, err_num = file:read(1)
-
-    local is_dir =
-      (err_num == 21) and (err_str == 'Is a directory')
-
-    file:close()
-
-    return is_dir
-  end
+local get_cmd_dir_exists = request('!.mechs.cmdline.get_cmd_dir_exists')
 
 -- Export:
-return is_directory
+return
+  function(dir_name)
+    return (get_cmd_dir_exists(dir_name):Execute())
+  end
 
 --[[
-  2024
-  2026-05-04
+  2024 #
+  2026 # #
 ]]
