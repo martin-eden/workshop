ed1c3b9e837bad875672a49df1809ae49a8c0007
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-23 15:22:56 +0200

    [!/] [file_system.directory.get_directories_list] Output change..
    Again, now each entry will NOT contain base directory prefix.

--- file_system/directory/get_directories_list.lua
+++ file_system/directory/get_directories_list.lua
@@ -2,29 +2,24 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-09-14
+  Last mod.: 2026-09-23
 ]]
 
 local get_cmd_listdirs = request('!.mechs.cmdline.get_cmd_listdirs')
 local get_command_output_lines = request('!.system.get_command_output_lines')
+local to_clean_filelist = request('to_clean_filelist')
 
---[[
-  Return directory names in base directory as list of strings
-
-  Each list entry is string with directory name with
-  following properties:
-
-    * Starts with base directory prefix
-    * Does not contain "/"
-]]
-
--- Export:
 return
   function(base_dir)
-    return get_command_output_lines(get_cmd_listdirs(base_dir))
+    return
+      to_clean_filelist(
+        get_command_output_lines(get_cmd_listdirs(base_dir)),
+        base_dir
+      )
   end
 
 --[[
   2017 #
   2026 # #
+  2026-09-23
 ]]
