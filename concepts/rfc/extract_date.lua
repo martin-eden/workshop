eacb03aa6947d5c1c7978245a59ec3ca3ea937d2
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-24 11:34:24 +0200

    [-] [concepts.rfc] It was custom and unused info extractor..
    We can write another one if needed.

--- concepts/rfc/extract_date.lua
+++
@@ -1,8 +0,0 @@
-local extract_date_re = '.+  +(.*)$'
-
-return
-  function(block)
-    local date_str = block[#block]
-    date_str = date_str:match(extract_date_re)
-    return date_str
-  end
--- concepts/rfc/print_block.lua
+++
@@ -1,9 +0,0 @@
-return
-  function(block, block_name)
-    assert_table(block)
-    assert_string(block_name)
-    print(('%s:'):format(block_name))
-    for i = 1, #block do
-      print(('[%02d] "%s"'):format(i, block[i]))
-    end
-  end
