1f5816090334381f6e295428dbbccebc2993fa07
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-24 17:43:43 +0200

    [.] [base] Code styling.

--- base.lua
+++ base.lua
@@ -54,146 +54,148 @@
   and use it for relative require() modules in that directory.
 ]]
 
--- External functions used by implementation
-local str_match = string.match
-local str_find = string.find
-local str_sub = string.sub
-local tbl_pack = table.pack
-local tbl_unpack = table.unpack
-local require = require
-
-local empty = ''
-
+local set_base_prefix
+local split_name
+local get_base_prefix
+local request
 local stack_init
-local stack_get
 local stack_add
 local stack_remove
 do
-  local Names
-  local depth
+  local str_match = string.match
+  local str_find = string.find
+  local str_sub = string.sub
+  local tbl_pack = table.pack
+  local tbl_unpack = table.unpack
+  local require = require
 
-  stack_init =
-    function()
-      Names = { }
-      depth = 1
-    end
+  local empty = ''
 
-  stack_get =
-    function()
-      return Names[depth]
-    end
+  local stack_get
+  do
+    local Names
+    local depth
 
-  stack_add =
-    function(prefix, name)
-      depth = depth + 1
-      Names[depth] = { prefix = prefix, name = name }
-    end
+    stack_init =
+      function()
+        Names = { }
+        depth = 1
+      end
 
-  stack_remove =
-    function()
-      depth = depth - 1
-    end
-end
+    stack_get =
+      function()
+        return Names[depth]
+      end
 
-local get_caller_prefix =
-  function()
-    local NameRec = stack_get()
+    stack_add =
+      function(prefix, name)
+        depth = depth + 1
+        Names[depth] = { prefix = prefix, name = name }
+      end
 
-    if not NameRec then return empty end
-
-    return NameRec.prefix
+    stack_remove =
+      function()
+        depth = depth - 1
+      end
   end
 
-local split_name
-do
-  -- a.b.c -> ( a.b. c )
-  local prefix_name_capture = '^(.+%.)([^%.]+)$'
+  local get_caller_prefix =
+    function()
+      local NameRec = stack_get()
 
-  split_name =
+      if not NameRec then return empty end
+
+      return NameRec.prefix
+    end
+
+  do
+    -- a.b.c -> ( a.b. c )
+    local prefix_name_capture = '^(.+%.)([^%.]+)$'
+
+    split_name =
+      function(qualified_name)
+        local prefix, name =
+          str_match(qualified_name, prefix_name_capture)
+
+        if not prefix then
+          prefix = empty
+          if str_find(qualified_name, '%.') then
+            name = empty
+          else
+            name = qualified_name
+          end
+        end
+
+        return prefix, name
+      end
+  end
+
+  -- Apply relative path prefix
+  local apply_rel_prefix
+  do
+    -- a.b.c. -> a.b.
+    local uplevel_capture = '(.+%.)[^%.]-%.$'
+
+    apply_rel_prefix =
+      function(base_prefix, rel_prefix)
+        while (str_sub(rel_prefix, 1, 2) == '^.') do
+          if (base_prefix == empty) then
+            error("Link is outside of caller's prefix.")
+          end
+          base_prefix = str_match(base_prefix, uplevel_capture) or empty
+          rel_prefix = str_sub(rel_prefix, 3)
+        end
+
+        return base_prefix .. rel_prefix
+      end
+  end
+
+  do
+    local base_prefix
+
+    set_base_prefix =
+      function(arg_base_prefix)
+        base_prefix = arg_base_prefix
+      end
+
+    get_base_prefix =
+      function()
+        return base_prefix
+      end
+  end
+
+  local get_require_name =
     function(qualified_name)
-      local prefix, name =
-        str_match(qualified_name, prefix_name_capture)
+      local caller_prefix
 
-      if not prefix then
-        prefix = empty
-        if str_find(qualified_name, '%.') then
-          name = empty
-        else
-          name = qualified_name
-        end
+      local is_absolute_name = (str_sub(qualified_name, 1, 2) == '!.')
+
+      if is_absolute_name then
+        qualified_name = str_sub(qualified_name, 3)
+        caller_prefix = get_base_prefix()
+      else
+        caller_prefix = get_caller_prefix()
       end
 
-      return prefix, name
+      local prefix, name = split_name(qualified_name)
+      prefix = apply_rel_prefix(caller_prefix, prefix)
+
+      return prefix .. name
+    end
+
+  request =
+    function(qualified_name)
+      local require_name = get_require_name(qualified_name)
+
+      stack_add(split_name(require_name))
+
+      local Results = tbl_pack(require(require_name))
+
+      stack_remove()
+
+      return tbl_unpack(Results)
     end
 end
-
--- Apply relative path prefix
-local apply_rel_prefix
-do
-  -- a.b.c. -> a.b.
-  local uplevel_capture = '(.+%.)[^%.]-%.$'
-
-  apply_rel_prefix =
-    function(base_prefix, rel_prefix)
-      while (str_sub(rel_prefix, 1, 2) == '^.') do
-        if (base_prefix == empty) then
-          error("Link is outside of caller's prefix.")
-        end
-        base_prefix = str_match(base_prefix, uplevel_capture) or empty
-        rel_prefix = str_sub(rel_prefix, 3)
-      end
-
-      return base_prefix .. rel_prefix
-    end
-end
-
-local set_base_prefix
-local get_base_prefix
-do
-  local base_prefix
-
-  set_base_prefix =
-    function(arg_base_prefix)
-      base_prefix = arg_base_prefix
-    end
-
-  get_base_prefix =
-    function()
-      return base_prefix
-    end
-end
-
-local get_require_name =
-  function(qualified_name)
-    local caller_prefix
-
-    local is_absolute_name = (str_sub(qualified_name, 1, 2) == '!.')
-
-    if is_absolute_name then
-      qualified_name = str_sub(qualified_name, 3)
-      caller_prefix = get_base_prefix()
-    else
-      caller_prefix = get_caller_prefix()
-    end
-
-    local prefix, name = split_name(qualified_name)
-    prefix = apply_rel_prefix(caller_prefix, prefix)
-
-    return prefix .. name
-  end
-
-local request =
-  function(qualified_name)
-    local require_name = get_require_name(qualified_name)
-
-    stack_add(split_name(require_name))
-
-    local Results = tbl_pack(require(require_name))
-
-    stack_remove()
-
-    return tbl_unpack(Results)
-  end
 
 -- Setup and export globals
 do
