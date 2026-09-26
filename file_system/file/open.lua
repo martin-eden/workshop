ec8894d5d5fa0e667f20fa5cd8269856bd26f432
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-26 13:38:10 +0200

    [.^] [file_system.file.open] Code revisit..
    Code no longer requires [path_name.normalize].
    
      It's not really needed and current implementation of normalize
      limits code to POSIX file systems.

--- file_system/file/open.lua
+++ file_system/file/open.lua
@@ -2,29 +2,19 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-09-14
+  Last mod.: 2026-09-26
 ]]
 
---[[
-  Contract is that this function returns file handle or explodes
-
-  No pesky ( nil error_msg ) from stock io.open().
-]]
-
-local normalize_name = request('!.concepts.path_name.normalize')
 local default_mode = 'rb'
 local io_open = io.open
 
--- Export:
 return
   function(pathname, mode)
     assert_string(pathname)
-    assert(is_nil(mode) or is_string(mode))
 
-    pathname = normalize_name(pathname)
     mode = mode or default_mode
 
-    local file, err_msg = io.open(pathname, mode)
+    local file, err_msg = io_open(pathname, mode)
 
     if not file then
       error(err_msg, 2)
@@ -37,4 +27,5 @@ return
   2016 #
   2017 #
   2026 #
+  2026-09-26
 ]]
