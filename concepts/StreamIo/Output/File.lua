eca0cb0b765f3dac4990fe99afc3efb27366f772
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-26 15:48:11 +0200

    [/] [StreamIo.Output.File] New implementation style..
    Applied my current module style.
    
    File must be opened after create().
    File must be explicitly closed. No more auto-close on garbage collection.

--- concepts/StreamIo/Output/File.lua
+++ concepts/StreamIo/Output/File.lua
@@ -5,44 +5,69 @@
   Last mod.: 2026-08-30
 ]]
 
--- Imports:
-local open_file_for_writing = request('!.file_system.file.open_for_writing')
-local close_file = request('!.file_system.file.close')
+--[[
+  Storage format
 
-local Interface =
+    1 [s] pathname
+    2 [u] file instance
+]]
+
+local open
+do
+  local open_for_writing = request('!.file_system.file.open_for_writing')
+  open =
+    function(Me)
+      Me[2] = open_for_writing(Me[1])
+    end
+end
+
+local close
+do
+  local close_file = request('!.file_system.file.close')
+  close =
+    function(Me)
+      close_file(Me[2])
+    end
+end
+
+local write =
+  function(Me, data_str)
+    assert_string(data_str)
+
+    Me[2]:write(data_str)
+  end
+
+local Interface
+
+local create
+do
+  local attach_methods = request('!.table.attach_methods')
+  create =
+    function(pathname)
+      assert_string(pathname)
+
+      local Me = { pathname, 0 }
+      attach_methods(Me, Interface)
+
+      return Me
+    end
+end
+
+Interface =
   {
-    -- [Required extension]
-    Open =
-      function(Me, pathname)
-        Me.File = open_file_for_writing(pathname)
-      end,
-
-    -- [Required extension]
-    Close =
-      function(Me)
-        close_file(Me.File)
-      end,
-
-    -- [Main]
-    Write =
-      function(Me, data_str)
-        assert_string(data_str)
-
-        Me.File:write(data_str)
-      end,
-
-    -- [Internal]
-    File = 0,
+    create = create,
+    -- Extensions:
+    Open = open,
+    Close = close,
+    -- Core:
+    Write = write,
   }
-
--- Close file at garbage collection
-setmetatable(Interface, { __gc = function(Me) Me:Close() end } )
 
 -- Export:
 return Interface
 
 --[[
   2024 # # # # #
-  2026-05-27
-  2026-08-30
+  2026 # #
+  2026-09-26
 ]]
